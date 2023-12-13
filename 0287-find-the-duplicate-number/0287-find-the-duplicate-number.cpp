05d75c8fe4b42775f89b1ec9e6368a7d46692141
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-13 18:05:02 +0530

    Time: 77 ms (82.03%), Space: 61.8 MB (24.41%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -40,7 +40,7 @@ public:
 
         while(nums[0] != nums[nums[0]])
         {
-            swap(nums[0], nums[nums[0]])
+            swap(nums[0], nums[nums[0]]);
         }
         return nums[0];
     }
