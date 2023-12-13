01998cab4e7157e253d25dd13b596d5162baf422
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-13 14:28:54 +0530

    Time: 15 ms (80.73%), Space: 32.7 MB (8.2%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,37 +1,29 @@
+#include <vector>
+
 class Solution {
 public:
-    int pivotIndex(vector<int>& nums) {
-        int leftsum=0;
-        int rightsum=0;
-        int pivot=-1;
-        int flag=0;
-        for(int i=0;i<nums.size();i++) //pivot index
-        {
-            if(i>0)
-                leftsum+=nums[i-1];
-            for(int j=i+1;j<nums.size();j++)
-            {
-                rightsum+=nums[j];
-            }
-            if(leftsum==rightsum)
-            {
-                pivot=i;
-                flag=1;
-                break;
-            }
-            else
-            {
-                rightsum=0;
-            }
+    int pivotIndex(std::vector<int>& nums) {
+        int n = nums.size();
+        std::vector<int> lsum(n, 0);
+        std::vector<int> rsum(n, 0);
 
+        lsum[0] = 0;
+        rsum[n - 1] = 0;
+        int flag = 0;
+
+        for (int i = 1; i < n; i++) {
+            lsum[i] = lsum[i - 1] + nums[i - 1];
         }
-        if (flag == 1)
-        {
-            return pivot;
+
+        for (int i = n - 2; i >= 0; i--) {
+            rsum[i] = rsum[i + 1] + nums[i + 1];
         }
-        else
-        {
-            return -1;
+
+        for (int i = 0; i < n; i++) {
+            if (lsum[i] == rsum[i]) {
+                return i;
+            }
         }
+        return -1;
     }
 };
