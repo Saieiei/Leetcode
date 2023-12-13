ffa38c020c8c17addca19b0c51210be9f9717383
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-13 12:53:48 +0530

    Time: 20 ms (34.47%), Space: 13.3 MB (25.79%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -1,46 +1,30 @@
 class Solution {
 public:
     int numSpecial(vector<vector<int>>& mat) {
-        int flag=0;
-        int row=0;
-        int col=0;
-        int ans=0;
-        for(int i=0;i<mat.size();i++) //iterate through ech row
-        {
-            flag=0;
-            for(int j=0;j<mat[i].size();j++) //iterating through each col to find 1
-            {
-                if(mat[i][j]==1)
-                {
-                    flag=1;
-                    row=i;
-                    col=j;
+        int sum = 0;
+        vector<int> rows(mat.size());
+        vector<int> columns(mat[0].size());
 
-                    int sum=0;
-                    if(flag==1)
-                    {
-                        //we have to go through each col to find if there is more 1s
-                        for(int k=0;k<mat[i].size();k++)
-                        {
-                            sum=sum+mat[row][k];
-                        }
-                        //we have to go through each row to find if there is more 1s
-                        for(int k=0;k<mat.size();k++)
-                        {
-                            sum=sum+mat[k][col];
-                        }
-                        if(sum==2)
-                        {
-                            ans++;
-                            break;
-                        }
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[i].size(); j++) {
+                if (mat[i][j] == 1) {
+                    rows[i]++;
+                    columns[j]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < mat.size(); i++) {
+            for (int j = 0; j < mat[i].size(); j++) {
+                if (mat[i][j] == 1) {
+                    if (rows[i] == 1 && columns[j] == 1) {
+                        sum++;
                     }
                 }
-
             }
-
         }
-        return ans;
-        
+
+        return sum;
+
     }
 };
