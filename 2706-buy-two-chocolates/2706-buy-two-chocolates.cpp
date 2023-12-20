c0abc4ff1414b99e4ce78183d24db3fa5ecb40a4
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-20 20:59:47 +0530

    Time: 15 ms (45.23%), Space: 47.2 MB (13.41%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,16 +1,7 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(), prices.end());
-        if(prices[0]+prices[1]<=money)
-        {
-            return(money-(prices[0]+prices[1]));
-        }
-        else
-        {
-            return money;
-        }
-        
-        return money;
+        sort(prices.begin(),prices.end());
+        return prices[0]+prices[1]<=money?money-prices[0]-prices[1]:money;
     }
 };
