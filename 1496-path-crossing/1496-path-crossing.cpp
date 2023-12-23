25a88f6a08e1aff340693e4d0d0b7459a188deb6
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-23 13:42:56 +0530

    Time: 1941 ms (19.89%), Space: 9.4 MB (7.18%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -1,41 +1,71 @@
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        //string s;
-        //s = "NESW";
-        int x = 0, y = 0;
-        pair<int, int> currentPos = make_pair(x, y);
-        list<pair<int, int>> visitedPositions;
-        visitedPositions.push_back(currentPos);
-        int flag = 0;
-
-        for (int i = 0; i < path.length(); i++) {
-            if (path[i] == 'N') {
+        int x=0,y=0;
+        char c1, c2;
+        string s1;
+        s1="0,0";
+        set<string> set1;
+        set1.insert(s1);
+        int flag=0;
+        //printing the set
+        //cout<<"set1: ";
+        // for(auto i:set1)
+        //{
+        //        cout<<i<<" ";
+        //}
+        //cout<<endl;
+        for(int i=0;i<path.length();i++)
+        {
+            //printing the set
+            //cout<<"set1: ";
+            //for(auto j:set1)
+            //{
+            //        cout<<j<<" ";
+            //}
+            //cout<<endl;
+            if(path[i]=='N')
+            {
                 y++;
-            } else if (path[i] == 'S') {
+            }
+            else if(path[i]=='S')
+            {
                 y--;
-            } else if (path[i] == 'E') {
+            }
+            else if(path[i]=='E')
+            {
                 x++;
-            } else if (path[i] == 'W') {
+            }
+            else if(path[i]=='W')
+            {
                 x--;
             }
-
-            currentPos = make_pair(x, y);
-
-            auto it = find(visitedPositions.begin(), visitedPositions.end(), currentPos);
-            if (it != visitedPositions.end()) {
-                flag = 1;
-                break;
-            } else {
-                visitedPositions.push_back(currentPos);
+            c1 = char(x);
+            c2 = char(y);
+            s1 = to_string(x) + "," + to_string(y);  // Convert x and y to strings
+            //cout << s1 << endl;
+            //checking if the string is present in the set
+            for(auto i:set1)
+            {
+                if(i==s1)
+                {
+                    flag=1;
+                    break;
+                }
             }
-        }
 
-        if (flag == 1) {
+            set1.insert(s1);
+        }
+        if(flag==1)
+        {
+            //cout<<"true";
             return 1;
-        } else {
+        }
+        else{
+            //cout<<"false";
             return 0;
         }
+
 
         return 0;
     }
