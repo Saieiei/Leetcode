a18aab02143d00c3f4e0557df07cebb6f4ea12a9
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-25 22:39:25 +0530

    Time: 30 ms (68.82%), Space: 32.2 MB (51.44%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,24 +2,27 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
 
-        //this is a custom compartor sorting operator, this will sort(rearrange) the elements based on the differences with x in ascending order
-        //int a is the 1st element and int b is the second element 
-        //so if a is smaller than b wrt to the difference then we will keep a element 1st then b element 2nd and so on
-        auto cmp = [&](int a, int b) {
-            int diffA = abs(a - x);
-            int diffB = abs(b - x);
-            return diffA < diffB || (diffA == diffB && a < b);
-        };
-        
-        // Sorting the array based on the custom comparator
-        sort(arr.begin(), arr.end(), cmp);
-        
-        // Extracting the first k elements
-        vector<int> result(arr.begin(), arr.begin() + k);
-        
-        // Sorting the result to maintain ascending order
-        sort(result.begin(), result.end());
-        
-        return result;
+        //method 2: using 2 pointers, l(low) and h(high)
+
+        int l=0, h=arr.size()-1;
+        while(h-l>=k) //this gives u the required elements range,  that is k 
+        {
+            if(x-arr[l]>arr[h]-x) 
+//the diffence made by lower elemnt of the sorted array is more, this means the element on the right side(higher element) has a low difference, which means we should not touch the higher element of the array as we need only lower differences element, so we should move the lower element to get more low diference
+            {
+                l++;
+            }
+            else
+            {
+                h--;
+            }
+        }
+        vector<int> ans;
+        for(int i=l;i<=h;i++)
+        {
+            ans.push_back(arr[i]);
+        }
+
+        return ans;
     }
 };
