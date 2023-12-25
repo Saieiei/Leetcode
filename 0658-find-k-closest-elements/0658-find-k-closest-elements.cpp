52b66797f3acb16e5f25f397f6a0ec27e47dafc7
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-25 21:51:06 +0530

    Time: 53 ms (28.76%), Space: 31.4 MB (98.56%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -5,7 +5,7 @@ public:
         //this is a custom compartor sorting operator, this will sort(rearrange) the elements based on the differences with x in ascending order
         //int a is the 1st element and int b is the second element 
         //so if a is smaller than b wrt to the difference then we will keep a element 1st then b element 2nd and so on
-        auto cmp = (int& a, int& b) {
+        auto cmp = [&](int a, int b) {
             int diffA = abs(a - x);
             int diffB = abs(b - x);
             return diffA < diffB || (diffA == diffB && a < b);
