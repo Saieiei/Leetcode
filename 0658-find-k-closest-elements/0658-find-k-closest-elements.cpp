308c922604ce87f0fd89d5c233d47f23af89c540
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-25 22:41:18 +0530

    Time: 30 ms (68.82%), Space: 32.1 MB (62.33%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
 
-        //method 2: using 2 pointers, l(low) and h(high)
+        //method 2: using 2 pointers, l(low) and h(high), since it is sorted
 
         int l=0, h=arr.size()-1;
         while(h-l>=k) //this gives u the required elements range,  that is k 
         {
-            if(x-arr[l]>arr[h]-x) 
+            if(x-arr[l]>arr[h]-x) // dont put >= sign
 //the diffence made by lower elemnt of the sorted array is more, this means the element on the right side(higher element) has a low difference, which means we should not touch the higher element of the array as we need only lower differences element, so we should move the lower element to get more low diference
             {
                 l++;
