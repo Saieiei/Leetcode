9d24dd80b9094855e8b33f14fd2b4b4fb75594d1
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-29 23:58:38 +0530

    Time: 11 ms (42%), Space: 7.7 MB (78.63%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,33 @@
+//week5_ass_1
+//we will use freq table, main aim is to check the number of then letters r same or not
+//the vector freqTable will go upto 256(random)and will staore all as 0
+//we will parse through each element of s, we will access the element in the freqTable(by converting it to int) and inc the value
+//we will parse through each element of s, we will access the element in the freqTable(by converting it to int) and dec the value
+//in the end if all the elements in freqTable ==0 then it is anagram else it is not
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int freqTable[256]={0}; //calles as hashing table also
+
+        //access each charcter in string s
+        for(int i=0;i<s.length();i++)
+        {
+            freqTable[s[i]]++; //automatic convertion of char to int
+        }
+        //access each charcter in string t
+        for(int i=0;i<t.length();i++)
+        {
+            freqTable[t[i]]--;//automatic convertion of char to int
+        }
+        //checking all th elements in freqTable == 0 or not
+        for(int i=0;i<256;i++)
+        {
+            if(freqTable[i]!=0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
