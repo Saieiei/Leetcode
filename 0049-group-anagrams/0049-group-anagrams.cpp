926db030103cdb39f635cac9c7885db52f674fd4
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-30 23:19:08 +0530

    Time: 84 ms (7.92%), Space: 20.3 MB (71.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,33 @@
+//week5_ass_6
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //we have to use map
+            //if u do not know map then map is like, there are 2 things in map(fisrt, second), u will be able to assign multiple values in second and   map it to 1 value to first
+                //first         second
+                //key           values
+                ////map<string, vector<string>> mp      mp[str].push_back(st)   
+                //for(auto it=mp.begin(); it!=mp.end(); i++)
+                //{
+                //  ans.push_back(it->second) //basically i am pushing back the vales of a mapped strings in a new vector called ans
+                //}
+        
+        vector<vector<string>> ans;
+        map<string, vector<string>> mp;
+
+        for(auto word:strs)
+        {
+            string value=word;
+            sort(word.begin(), word.end()); //key
+            mp[word].push_back(value);
+        }
+
+        for(auto it=mp.begin();it!=mp.end();it++) //iterator, to travel through each i of map
+        {
+            ans.push_back(it->second);
+        }
+        return ans;
+        
+    }
+
+};
