e857ef88ff2efa663ea910aa19bac62784333056
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-31 13:04:32 +0530

    Time: 119 ms (5.31%), Space: 41.2 MB (5.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,27 +1,42 @@
 //week5_ass_6
+//method 2 map(hash)
 class Solution {
 public:
+
+    std::array<int, 256>hash(string str) 
+    {
+        std::array<int, 256>hash={0}; //the function name and the variable name both r hash
+        for(char i:str)
+        {
+            hash[i]++;
+        }
+        return hash;
+        //everytime u will return a new hash
+
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //we have to use map(sorting->key)
+        //we have to use map(sorting-hashing(mapping))
             //if u do not know map then map is like, there are 2 things in map(fisrt, second), u will be able to assign multiple values in second(groups) and   map it to 1 value to first
                 //first         second
                 //key           values
-                //"aet"         {"eat","tea","ate"}
-                ////map<string, vector<string>> mp      mp[str].push_back(st)   
+                //e=1
+                //a=1
+                //t=1         {"eat","tea","ate"}
+                //map<std::array<int, 256>, vector<string>> mp      mp[hash(str)].push_back(st), over here    std::array<int, 256> is same as int array[256]={0}, and hash is a function 
                 //for(auto it=mp.begin(); it!=mp.end(); i++)
                 //{
                 //  ans.push_back(it->second) //basically i am pushing back the values of a mapped strings in a new vector called ans
                 //}
         
         vector<vector<string>> ans;
-        map<string, vector<string>> mp;
+        map<std::array<int, 256>, vector<string>> mp;
 
+        //everytime u will ask to hash the word
         for(auto word:strs)
         {
-            string value=word;
-            sort(word.begin(), word.end()); //key
-            string key=word;
-            mp[key].push_back(value);
+            mp[hash(word)].push_back(word);
+            //if that hash table is already present it will get mapped accordingly
         }
 
         for(auto it=mp.begin();it!=mp.end();it++) //iterator, to travel through each i of map
