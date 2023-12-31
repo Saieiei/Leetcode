2e38af552953fd46ffd94de6bedabc04d3548893
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-31 12:44:56 +0530

    Time: 83 ms (8.47%), Space: 20.3 MB (69.23%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -2,14 +2,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //we have to use map
+        //we have to use map(sorting->key)
             //if u do not know map then map is like, there are 2 things in map(fisrt, second), u will be able to assign multiple values in second(groups) and   map it to 1 value to first
                 //first         second
                 //key           values
+                //"aet"         {"eat","tea","ate"}
                 ////map<string, vector<string>> mp      mp[str].push_back(st)   
                 //for(auto it=mp.begin(); it!=mp.end(); i++)
                 //{
-                //  ans.push_back(it->second) //basically i am pushing back the vales of a mapped strings in a new vector called ans
+                //  ans.push_back(it->second) //basically i am pushing back the values of a mapped strings in a new vector called ans
                 //}
         
         vector<vector<string>> ans;
