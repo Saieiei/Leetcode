98e603d370c840268b7300deae4513dabfd65d7f
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2023-12-31 12:26:45 +0530

    Time: 81 ms (8.69%), Space: 20.2 MB (72.02%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,7 +3,7 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         //we have to use map
-            //if u do not know map then map is like, there are 2 things in map(fisrt, second), u will be able to assign multiple values in second and   map it to 1 value to first
+            //if u do not know map then map is like, there are 2 things in map(fisrt, second), u will be able to assign multiple values in second(groups) and   map it to 1 value to first
                 //first         second
                 //key           values
                 ////map<string, vector<string>> mp      mp[str].push_back(st)   
