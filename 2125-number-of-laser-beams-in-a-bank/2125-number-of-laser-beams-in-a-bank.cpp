8d1d37db6b61664b6a9bc2970f04841f9dc7815c
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2024-01-05 11:44:50 +0530

    Time: 96 ms (81.23%), Space: 29.6 MB (5.97%) - LeetHub

--- 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -1,26 +1,53 @@
 class Solution {
 public:
-    int numberOfBeams(std::vector<std::string>& bank) {
-        int prevRowCount = 0;
-        int total = 0;
 
-        for (const std::string& row : bank) {
-            int curRowCount = calc(row);
-            if (curRowCount == 0)
-                continue;
-
-            total += curRowCount * prevRowCount;
-            prevRowCount = curRowCount;
+    int total_laser_present_in_row(string& str)
+    {
+        int total_lasers=0;
+        for(int i=0;i<str.size();i++)
+        {
+            if (str[i] == '1')
+            {
+                total_lasers++;
+            }
         }
-        return total;
+        return total_lasers;
+        
     }
 
-private:
-    int calc(const std::string& s) {
-        int count = 0;
-        for (char c : s)
-            count += c - '0';
 
-        return count;
+    int numberOfBeams(vector<string>& bank) {
+        //1st we will calculate how many lazers rr present in each row
+            //we will take the 1st string and take each char and see if its 1, if so add it to rows and return the value
+        //2nd we will use 2 pointers approach i at 0, and j at i+1; 
+            //we will simply multiply the the values (i and j) if j is 0 we will move j ahead
+    
+        vector<int>rows;
+        for(string row:bank) 
+        {
+            rows.push_back(total_laser_present_in_row(row));
+        }// after u have found the no of lasers in each rows its time to calculate the no of beams
+
+        int ans=0;
+        for(int i=0;i<rows.size();i++)
+        {
+            int j=i+1;
+            while(j<rows.size())
+            {
+                ans=ans+rows[i]*rows[j];
+                if(rows[j]==0)
+                {
+                    j++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+        return ans;
+
+
     }
+
 };
