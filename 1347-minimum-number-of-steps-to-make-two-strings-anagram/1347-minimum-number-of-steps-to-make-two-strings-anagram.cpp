4a175585619ebb578200521b9f04b75c9a1392c3
Sairudra More <88966749+Saieiei@users.noreply.github.com>
2024-01-13 16:16:42 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -1,32 +1,47 @@
 class Solution {
 public:
     int minSteps(string s, string t) {
-        // Check if the lengths of both strings are the same
-        if (s.length() != t.length()) {
-            return -1; // Invalid input, strings should be of the same length
-        }
-
-        // Count the frequency of characters in both strings
+        // we will use unordered_map to to ocount the values of each character
+        // in string s and t
         unordered_map<char, int> sFreq, tFreq;
-        for (char c : s) {
-            sFreq[c]++;
+        // if th elengths r not same its just not possible to mach every freq of
+        // the char in string s with string t
+        if (s.length() != t.length()) {
+            return -1;
         }
-        for (char c : t) {
-            tFreq[c]++;
+        // now lets try to find out the freq of each char in string s and t
+        for (auto c : s) {
+            sFreq[c]++; // go to that index and increase the freq
+            // for(auto pair:sFreq)
+            //{
+            //    cout<<"{"<<pair.first<<": "<<pair.second<<"}";
+            //}
+            // cout<<endl;
         }
 
-        // Loop over characters and calculate the minimum steps
-        int steps = 0;
-        for (auto& entry : sFreq) {
-            char c = entry.first;
-            int sCount = entry.second;
-            int tCount = tFreq[c];
-
-            if (tCount < sCount) {
-                steps += sCount - tCount;
+        for (auto c : t) {
+            tFreq[c]++; // go to that index and increase the freq
+        }
+        // cout<<sFreq<<endl<<tFreq<<endl; u can not print a map like this
+        // unforctunatly to print a unordered_map u need to use forloop for(auto
+        // pair:sFreq)
+        //{
+        // cout<<"{"<<pair.first<<": "<<pair.second<<"}";
+        //}
+        // now Loop over all characters if the frequency of a character in t is
+        // less than the frequency of the same character in s then add the
+        // difference between the frequencies to the answer.
+        int steps=0;
+        for(auto pair:sFreq)
+        {
+            char ch=sFreq.first;
+            int sCount=sFreq.second;
+            int tCount=tFreq[ch];
+            if(tCount>cCount)
+            {
+                steps=steps+tCount-sCount;
             }
         }
-
         return steps;
     }
 };
