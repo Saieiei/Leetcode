52aa405722d5717cd48f1f772cf3ada38613c784
Sairudra More <sairudra60@gmail.com>
2024-02-29 00:09:59 +0530

    Time: 4 ms (90.18%), Space: 20.2 MB (76.07%) - LeetHub

---
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        int leftmost_value;
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            leftmost_value = node->val;
+
+            if (node->right) {
+                q.push(node->right);
+            }
+            if (node->left) {
+                q.push(node->left);
+            }
+        }
+
+        return leftmost_value;
+    }
+};
