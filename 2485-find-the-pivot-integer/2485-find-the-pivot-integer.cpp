1afc163aea4ebab588e6ec686044fc2c7dbe349e
Sairudra More <sairudra60@gmail.com>
2024-03-13 19:00:49 +0530

    Time: 4 ms (35.26%), Space: 7.1 MB (73.82%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.cpp
@@ -3,7 +3,7 @@ public:
     int pivotInteger(int n) {
         int sum = ((n * (n + 1)) / 2), sum2 = 0;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 0; i <= n; i++) {
             sum2 += i;
             if (sum - sum2 + i == sum2) {
                 return i;
