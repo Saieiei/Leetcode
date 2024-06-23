003574e8dd44e72bda004e53ff33dfc473eda4c0
Sairudra More <sairudra60@gmail.com>
2024-06-23 20:04:05 +0530

    Time: 0 ms (100%), Space: 11.6 MB (36.12%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,14 +11,22 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* prev = nullptr;
+
+        ListNode* prev = NULL;
         ListNode* curr = head;
-        while (curr != nullptr) {
+        //case where we need to stop
+        while (curr != NULL) 
+        {
+            //we  will mark a node that is always ahead of current, as when we assign current->next = prev we will lose the rest of the LL
             ListNode* nextTemp = curr->next;
+
+            //point it in the reverse direction
             curr->next = prev;
+            
+            //move ur prev and current pointers 1 step ahead
             prev = curr;
             curr = nextTemp;
         }
-        return prev;
+        return prev; //which is now our starting point of the LL and the LL will go in the reverse order
     }
 };
