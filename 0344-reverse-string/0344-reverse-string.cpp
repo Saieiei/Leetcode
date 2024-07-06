4743f8f2a3fd05eec9f754ea0768d633491fb604
Sairudra More <sairudra60@gmail.com>
2024-07-06 13:35:26 +0530

    Time: 17 ms (56.89%), Space: 27.6 MB (21.25%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -5,17 +5,36 @@ public:
        //reverse(s.begin(), s.end());
        //return;
 
-       //using 2 pointers apprach
-       int p1 = 0;
-       int p2 = s.size() -1;
-       while(p1<p2)
-       {
-            char temp = s[p1];
-            s[p1] = s[p2];
-            s[p2] = temp;
-            p1++;
-            p2--;
-       }
-       return;
+       //using 2 pointers approach
+       //int p1 = 0;
+       //int p2 = s.size() -1;
+       //while(p1<p2)
+       //{
+       //     char temp = s[p1];
+       //     s[p1] = s[p2];
+       //     s[p2] = temp;
+       //     p1++;
+       //     p2--;
+       //}
+       //return;
+
+        //using stacks
+        stack<char> st;
+ 
+        for(int i=0; i<s.size(); i++)
+        {
+             char ch = s[i];
+             st.push(ch);
+        }
+
+        int i=0; 
+        while(!st.empty())
+        {
+            char ch = st.top();
+            s[i] = ch;
+            i++;
+            st.pop();
+        }
+        return;
     }
 };
