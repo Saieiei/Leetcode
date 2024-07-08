cce93ad4de32e1c08633c503bcaeb45d32970f27
Sairudra More <sairudra60@gmail.com>
2024-07-08 22:56:48 +0530

    Time: 298 ms (16.31%), Space: 783 MB (7.57%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,27 +1,49 @@
 class Solution {
 public:
+//week 11
     string removeDuplicates(string s) {
-        //we will solve this by creating a new string method
-        string s2="";
-        int len=s.length();
-        for(int i=0;i<len;i++)
+        ////we will solve this by creating a new string method
+        //string s2="";
+        //int len=s.length();
+        //for(int i=0;i<len;i++)
+        //{
+        //    char ch=s[i];
+        //    if(s2.empty())
+        //    {
+        //        s2.push_back(ch);
+        //        continue;
+        //    }
+        //    if(s2.back()!=ch)
+        //    {
+        //        s2.push_back(ch);
+        //    }
+        //    else
+        //    {
+        //        s2.pop_back();
+        //    }
+        //}
+        //return s2;
+
+        //now we will try to do it using stacks
+        stack<char> st;
+        for(char ch : s)
         {
-            char ch=s[i];
-            if(s2.empty())
-            {
-                s2.push_back(ch);
-                continue;
-            }
-            if(s2.back()!=ch)
-            {
-                s2.push_back(ch);
-            }
+            if(st.empty()) st.push(ch);
             else
             {
-                s2.pop_back();
+                if(!st.empty() && st.top() == ch) //same, paired, hence pop 
+                    st.pop();
+                else st.push(ch);
             }
         }
-        return s2;
-    }
+
+        string ans = "";
+        while(!st.empty())
+        {
+            ans = st.top() + ans; st.pop(); 
+        }
+        return ans;
+    }   
+
    
 };
