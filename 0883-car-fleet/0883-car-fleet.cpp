927e8da590fc81b07aa9a2facfc61a33d0fb5c13
Sairudra More <sairudra60@gmail.com>
2024-07-10 16:12:55 +0530

    Time: 133 ms (57.44%), Space: 89.5 MB (26.6%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.cpp
+++ 0883-car-fleet/0883-car-fleet.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
+//week 11
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int n = position.size();
+    int n = position.size();
     vector<pair<int, double>> cars; // pair of position and time to reach target
 
     // Calculate the time each car takes to reach the target
@@ -10,22 +11,22 @@ public:
         cars.push_back({position[i], time});
     }
 
-    // Sort cars based on their starting positions in decreasing order
-    sort(cars.rbegin(), cars.rend());
+    // Sort cars based on their starting positions in increasing order
+    sort(cars.begin(), cars.end());
 
     stack<double> st;
 
-    // Iterate through the sorted cars
-    for (int i = 0; i < n; ++i) {
+    // Iterate through the sorted cars from the farthest to the nearest
+    for (int i = n - 1; i >= 0; --i) {
         double time = cars[i].second;
-        // If current car's time is greater than the time at the top of the stack,
-        // it means current car cannot catch up to the car fleet ahead, thus it forms a new fleet.
+        // If the current car's time is greater than the time at the top of the stack,
+        // it means the current car cannot catch up to the car fleet ahead, thus it forms a new fleet.
         if (st.empty() || time > st.top()) {
             st.push(time);
         }
     }
 
     return st.size();
-    }
+}
 
 };
