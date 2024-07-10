69484ce7f708d31e9947349eeb0466294624e06f
Sairudra More <sairudra60@gmail.com>
2024-07-10 18:46:55 +0530

    Time: 4 ms (67.13%), Space: 13.9 MB (61.11%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -4,6 +4,8 @@ public:
         int level = 0;
         for(string log : logs)
         {
+            cout<<"Level = "<< level<<endl;
+            cout<<"log = "<<log<<endl;
             if(log == "../") //which means go behind
             {
                 if(level != 0) level --; //u cannot go behind the root level
@@ -12,6 +14,7 @@ public:
             else if (log == "./") continue; //do nothing
             else level++;
         }
+        cout<<"Level = "<< level<<endl;
         return level;
     }
 };
