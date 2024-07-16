65d997578c2105a1abd07d4fc225e1ca62d0a265
Sairudra More <sairudra60@gmail.com>
2024-07-16 11:07:06 +0530

    Time: 3 ms (92.62%), Space: 17.9 MB (8.14%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -13,14 +13,46 @@ class Solution {
 public:
 //week 13
     int maxDepth(TreeNode* root) {
-        //idk how this works but it works
+        ////idk how this works but it works
+        ////we will solve this using recurssion
+        //if(root == NULL) return 0;
+        //else
+        //{
+        //    int leftHeight = maxDepth(root->left);
+        //    int rightHeight = maxDepth(root->right);
+        //    return max(leftHeight+1, rightHeight+1); //+1 because of the root
+        //}
 
+        //we will solve this with the NULL pointer approach
+
+        //base case
         if(root == NULL) return 0;
-        else
+        int count = 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+        count++;
+
+        while(!q.empty())
         {
-            int leftHeight = maxDepth(root->left);
-            int rightHeight = maxDepth(root->right);
-            return max(leftHeight+1, rightHeight+1);
+            TreeNode* frontNode = q.front();
+            q.pop();
+
+            if(frontNode == NULL) //finished 1 row
+            {
+                if(!q.empty()) //this is imp, dont forget this
+                {
+                    q.push(NULL);
+                    count++;
+                }
+            }
+            else
+            {
+                if(frontNode->left != NULL) q.push(frontNode->left);
+                if(frontNode->right != NULL) q.push(frontNode->right);
+            }
         }
+        return count;
     }
 };
