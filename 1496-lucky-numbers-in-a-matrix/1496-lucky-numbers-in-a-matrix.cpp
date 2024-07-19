30ac551657d3f9af05b4962783753f47fc43235d
Sairudra More <sairudra60@gmail.com>
2024-07-19 21:53:07 +0530

    Time: 20 ms (23.27%), Space: 14.7 MB (9.04%) - LeetHub

--- 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
+++ 1496-lucky-numbers-in-a-matrix/1496-lucky-numbers-in-a-matrix.cpp
@@ -12,6 +12,7 @@ public:
         int maxEle = 0;
         int rowSize = matrix.size();
         int colSize = matrix[0].size();
+        vector<int> ans;
 
         //now lets do this
         //we will save the min element of every rown in 1 set
@@ -28,7 +29,7 @@ public:
         }
 
         //now lets do this
-        //we will save the max element of every col in 1 set
+        //we will save the max element of every col in 1 set, but instead of saving them and checking later, we can directly check if that element is present in the minRow or not
         for(int j=0; j<colSize; j++)
         {
             //just take the 1st element of every col as maxEle
@@ -37,20 +38,21 @@ public:
             {
                 maxEle = max(maxEle, matrix[i][j]);
             }
-            //for that col, push the max element iin the set
-            maxCol.insert(maxEle);
+            ////for that col, push the max element iin the set
+            //maxCol.insert(maxEle);
+            if(minRow.count(maxEle)) ans.push_back(maxEle);
         }
 
-        vector<int> ans;
+        
         //now we will try to find the common element, if found push it in the ans vector
-        for(int ele: minRow) if(maxCol.count(ele)) ans.push_back(ele);    
-        //for(int i=0; i<rowSize; i++)
-        //{
-        //    for(int j=0; j<colSize; j++)
-        //    {
-        //        if(minRow.count(matrix[i][j]) && maxCol.count(matrix[i][j])) ans.push_back(matrix[i][j]);
-        //    }
-        //}
+        //for(int ele: minRow) if(maxCol.count(ele)) ans.push_back(ele);    
+        ////for(int i=0; i<rowSize; i++)
+        ////{
+        ////    for(int j=0; j<colSize; j++)
+        ////    {
+        ////        if(minRow.count(matrix[i][j]) && maxCol.count(matrix[i][j])) ans.push_back(matrix[i][j]);
+        ////    }
+        ////}
         return ans;
     }
 };
