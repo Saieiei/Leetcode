a09d1241eea4a3a61de6d955dc4058c49c1d4786
Sairudra More <sairudra60@gmail.com>
2024-07-23 22:26:30 +0530

    Time: 7 ms (77.07%), Space: 21.9 MB (17.05%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,42 +9,68 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//class Solution {
+//public:
+////week 13
+//    int height(TreeNode* root)
+//    {
+//        if(root == NULL) return 0;
+//        else
+//        {
+//            int leftHeight = height(root->left);
+//            int rightHeight = height(root->right);
+//            int finalAns = max(leftHeight, rightHeight) +1; //because of the root node
+//            return finalAns;
+//        }
+//    }
+//    bool isBalanced(TreeNode* root) {
+//        //for this to work, u should know how to find the height of a node
+//        //for a node to be balanced, its right height and left height should not differ more tham 1
+//        //for the rest of the nodes, we will do recursion
+//        //this is very bad approach but
+//        //base case
+//        if(root == NULL) return true;
+//
+//        //for the currentNode
+//        int leftHeight = height(root->left);
+//        int rightHeight = height(root->right);
+//        int diff = abs(leftHeight - rightHeight);
+//        bool currentNode = diff<=1;
+//
+//        //now we will for the rest of the nodes
+//        bool left = isBalanced(root->left);
+//        bool right = isBalanced(root->right);
+//
+//        //check condition 
+//        if(currentNode && left && right) return true;
+//        else return false;
+//    }
+//};
+
 class Solution {
 public:
-//week 13
+//week13
+    bool ans = true;
     int height(TreeNode* root)
     {
-        if(root == NULL) return 0;
-        else
-        {
-            int leftHeight = height(root->left);
-            int rightHeight = height(root->right);
-            int finalAns = max(leftHeight, rightHeight) +1; //because of the root node
-            return finalAns;
-        }
-    }
-    bool isBalanced(TreeNode* root) {
-        //for this to work, u should know how to find the height of a node
-        //for a node to be balanced, its right height and left height should not differ more tham 1
-        //for the rest of the nodes, we will do recursion
-
-        //base case
-        if(root == NULL) return true;
-
-        //for the currentNode
+        if(!root) return 0;
+        //the height fucntion will go to each and everynode and return its height
+        //it works on bottom up approach
+        //do for the 1st node
         int leftHeight = height(root->left);
         int rightHeight = height(root->right);
-        int diff = abs(leftHeight - rightHeight);
-        bool currentNode = diff<=1;
 
-        //now we will for the rest of the nodes
-        bool left = isBalanced(root->left);
-        bool right = isBalanced(root->right);
+        //calculate its height
+        int balanced = abs(leftHeight - rightHeight);
+        if( ans && balanced>1) ans = false;
 
-        //check condition 
-        if(currentNode && left && right) return true;
-        else return false;
-
-
+        return (max(leftHeight, rightHeight) + 1);
+    }
+    bool isBalanced(TreeNode* root)
+    {
+        //for this u have to know the concept of height
+        //the height fucntion will go to each and everynode and return its height
+        height(root);
+        return ans;
     }
 };
