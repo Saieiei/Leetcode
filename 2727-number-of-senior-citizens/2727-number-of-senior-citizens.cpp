9d4dbd7a06ca627214c48b7e66ae348064740328
Sairudra More <sairudra60@gmail.com>
2024-08-01 11:36:20 +0530

    Time: 9 ms (23.77%), Space: 17.6 MB (27.35%) - LeetHub

--- 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
+++ 2727-number-of-senior-citizens/2727-number-of-senior-citizens.cpp
@@ -4,7 +4,8 @@ public:
         int ans = 0;
         for(string& s: details)
         {
-            int age  = stoi(s.substr(11, 2));
+            //int age  = stoi(s.substr(11, 2)); convertion from string to char makes it sloe
+            int age = ((s[11]-'0')*10) + (s[12]-'0');
             if (age > 60) ans++;
         }
         return ans;
