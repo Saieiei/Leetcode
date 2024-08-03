e7064c53d4309b2631f5ae5978a16ed0dc0094b1
Sairudra More <sairudra60@gmail.com>
2024-08-03 12:49:30 +0530

    Time: 10 ms (52.84%), Space: 21 MB (6.79%) - LeetHub

--- 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        sort(target.begin(), target.end());
-        sort(arr.begin(), arr.end());
+        //sort(target.begin(), target.end());
+        //sort(arr.begin(), arr.end());
+        //for(int i=0;i<target.size();i++)
+        //{
+        //    if(target[i] != arr[i]) return false;
+        //}
+        //return true;
+
+        //u can also solve it based on freq
+        unordered_map<int, int> targetFreq;
+        unordered_map<int, int> arrFreq;
+
+        //finding freq
         for(int i=0;i<target.size();i++)
         {
-            if(target[i] != arr[i]) return false;
+            targetFreq[target[i]]++;
+        }
+        for(int i=0;i<arr.size();i++)
+        {
+            arrFreq[arr[i]]++;
+        }
+
+        //now eliminating
+        if(targetFreq.size() != arrFreq.size()) return false;
+
+        for(auto pair: targetFreq)
+        {
+            if(arrFreq[pair.first] != pair.second) return false;
         }
         return true;
     }
