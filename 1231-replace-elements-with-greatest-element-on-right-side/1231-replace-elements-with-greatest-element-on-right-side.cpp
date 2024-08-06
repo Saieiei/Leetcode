5efc16be0a5c8db11fc04efae6734f4344640203
Sairudra More <sairudra60@gmail.com>
2024-08-06 12:44:02 +0530

    Time: 57 ms (96.7%), Space: 71.1 MB (43.7%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
+//O(n2)
+    //vector<int> replaceElements(vector<int>& arr) {
+    //    vector<int> ans;
+//
+    //    for(int i=0;i<arr.size();i++)
+    //    {
+    //        if(arr.size()-1 == i) ans.push_back(-1);  //last element is alsoways -1
+    //        else
+    //        {
+    //            auto ele = (max_element(arr.begin()+i+1, arr.end())); //O(n2)
+    //            ans.push_back(*ele);
+    //        }
+    //    }
+    //    //ans[arr.size()-1] = -1;
+    //    return ans;
+    //}
     vector<int> replaceElements(vector<int>& arr) {
-        vector<int> ans;
-
-        for(int i=0;i<arr.size();i++)
+        //O(n)
+        vector<int> ans(arr.size(), 0);
+        int maxi=-100000;
+        //reverse order
+        ans[ans.size()-1] = -1;
+        for(int i=ans.size()-2; i>=0; i--)
         {
-            if(arr.size()-1 == i) ans.push_back(-1);
-            else
-            {
-                auto ele = (max_element(arr.begin()+i+1, arr.end()));
-                ans.push_back(*ele);
-            }
+            maxi = max(maxi, arr[i+1]);
+            ans[i] = maxi;
         }
-        //ans[arr.size()-1] = -1;
         return ans;
     }
 };
