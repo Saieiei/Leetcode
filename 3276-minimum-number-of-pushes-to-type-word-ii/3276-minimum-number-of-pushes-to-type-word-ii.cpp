a4af0fc9f43b97c0b877d5431f4a97499f70dc6a
Sairudra More <sairudra60@gmail.com>
2024-08-07 00:12:04 +0530

    Time: 69 ms (75.25%), Space: 24.9 MB (62.15%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    static int minimumPushes(string& word) {
-        int freq[26]={0};
-        for(char c: word) 
-            freq[c-'a']++;
-        sort(freq, freq+26);
-    
-        int sz=25, ans=0;
-        for(; sz>=0 && freq[sz]!=0; sz--){
-            ans+=freq[sz]*((25-sz)/8+1);    
+//https://www.youtube.com/watch?v=j2zyVaWqF20
+    int minimumPushes(string word) {
+        int ans=0;
+        vector<int> freq(26,0); // frequenct calculated
+        for(auto it: word){
+            freq[it-'a']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>()); // sort frequency in decreasing order
+        for(int i=0;i<26;i++){
+            if(i<8){
+                ans+= freq[i]; // map at first positon of all keys
+            }
+            else if(i<16){
+                ans+= freq[i]*2; // map with second position of all keys
+            }else if(i<24){
+                ans+= freq[i]*3; // map with third position all keys
+            }
+            else ans+= freq[i]*4; // map with fourth position of all keys
+        
         }
         return ans;
     }
