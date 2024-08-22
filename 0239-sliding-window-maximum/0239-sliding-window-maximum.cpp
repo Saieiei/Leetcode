34db72ea11a94a6a4313771cba16462b75fe66f8
Sairudra More <sairudra60@gmail.com>
2024-08-22 21:45:32 +0530

    Time: 535 ms (5.09%), Space: 214.8 MB (5.35%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
+//week 12 
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-       deque<int> dq;
-        vector<int> result;
+        //this deque method requires O(n)
+    //   deque<int> dq;
+    //   //in the deque, we will store the indeex of the greatest element
+    //    vector<int> result;
+    //    //in case of sliding window, just rememebr this technique
+    //        //while (condition)->pop_back()
+    //        //push_back()
+    //        //sliding window limit exceeded pop_front()
+    //        //put the 1st elemet into the ans, after size of the sliding window, front()
+//
+    //    for (int i = 0; i < nums.size(); ++i) {
+    //        // Remove elements from the back if they are smaller than the current element
+    //        while (!dq.empty() && nums[dq.back()] < nums[i]) {
+    //            dq.pop_back();
+    //        }
+//
+    //        // Add the current element's index to the deque
+    //        dq.push_back(i);
+//
+    //        // Remove the element from the front if it's outside the current window
+    //        if (dq.front() == i - k) {
+    //            dq.pop_front();
+    //        }
+//
+    //        // Start adding results to the output list after the first k elements
+    //        if (i >= k - 1) {
+    //            result.push_back(nums[dq.front()]);
+    //        }
+    //    }
+//
+    //    return result; 
 
-        for (int i = 0; i < nums.size(); ++i) {
-            // Remove elements from the back if they are smaller than the current element
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
-                dq.pop_back();
-            }
+        //we can do the same using sets, which can do it O(nlogn)
+        vector<int> ans;
+        set<pair<int, int>, greater<pair<int, int>>> st;
 
-            // Add the current element's index to the deque
-            dq.push_back(i);
-
-            // Remove the element from the front if it's outside the current window
-            if (dq.front() == i - k) {
-                dq.pop_front();
-            }
-
-            // Start adding results to the output list after the first k elements
-            if (i >= k - 1) {
-                result.push_back(nums[dq.front()]);
-            }
+        for(int i=0; i<k; i++)
+        {
+            st.insert({nums[i], i});
         }
+        ans.push_back(st.begin()->first);
 
-        return result; 
+        for(int i=k; i<nums.size(); i++)
+        {
+            st.erase({nums[i-k], i-k});
+            st.insert({nums[i], i});
+            ans.push_back(st.begin()->first);
+        }
+        return ans;
     }
 };
