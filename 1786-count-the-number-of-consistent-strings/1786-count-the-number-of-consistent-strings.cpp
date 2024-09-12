292597f10a378fcdf95ca4674cea5cea1625eb21
Sairudra More <sairudra60@gmail.com>
2024-09-12 22:11:09 +0530

    Time: 49 ms (46.08%), Space: 34 MB (27.48%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -1,28 +1,48 @@
 class Solution {
 public:
-    void checker(map<int, int>& alloChar, int& ans, string word)
-    {
-        int flag = 1; //innitally allowed
-        for(char ch: word)
-        {
-            auto it = alloChar.find(ch);
-            if(it != alloChar.end()) continue; //the element is there 
-            else 
-            {flag = 0; break;} //the element is not there
-        }
-        if(flag == 1) ans = ans+1;
-    }
+    //void checker(map<int, int>& alloChar, int& ans, string word)
+    //{
+    //    int flag = 1; //innitally allowed
+    //    for(char ch: word)
+    //    {
+    //        auto it = alloChar.find(ch);
+    //        if(it != alloChar.end()) continue; //the element is there 
+    //        else 
+    //        {flag = 0; break;} //the element is not there
+    //    }
+    //    if(flag == 1) ans = ans+1;
+    //}
 
     int countConsistentStrings(string allowed, vector<string>& words) {
-        int ans  = 0;
+        //int ans  = 0;
+//
+        ////1st we will create a hash map,  that will store all the elemenets (allowed)
+        //map<int, int>alloChar;
+//
+        //for(char ch: allowed) alloChar[ch] = 1;
+//
+        ////now lets traverse through the words
+        //for(string word: words) checker(alloChar, ans, word);
+//
+        //return ans;
+                int ans = 0;
 
-        //1st we will create a hash map,  that will store all the elemenets (allowed)
-        map<int, int>alloChar;
+        // Use unordered_set for faster lookups
+        unordered_set<char> allowedSet(allowed.begin(), allowed.end());
 
-        for(char ch: allowed) alloChar[ch] = 1;
-
-        //now lets traverse through the words
-        for(string word: words) checker(alloChar, ans, word);
+        // Check each word
+        for (const string& word : words) {
+            bool isConsistent = true;
+            for (char ch : word) {
+                if (allowedSet.find(ch) == allowedSet.end()) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+            if (isConsistent) {
+                ++ans;
+            }
+        }
 
         return ans;
     }   
