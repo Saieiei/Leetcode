237e64c852b7c0418ba82608900a5e09536a042d
Sairudra More <sairudra60@gmail.com>
2024-09-12 21:47:42 +0530

    Time: 76 ms (5.48%), Space: 34 MB (27.48%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.cpp
@@ -6,30 +6,23 @@ public:
         for(char ch: word)
         {
             auto it = alloChar.find(ch);
-            if(it != alloChar.end()) //the element is there
-            {
-                continue;
-            }
+            if(it != alloChar.end()) continue; //the element is there 
             else flag = 0; //the element is not there
         }
-        if(flag == 1)ans = ans+1;
+        if(flag == 1) ans = ans+1;
     }
+
     int countConsistentStrings(string allowed, vector<string>& words) {
         int ans  = 0;
 
         //1st we will create a hash map,  that will store all the elemenets (allowed)
         map<int, int>alloChar;
 
-        for(char ch: allowed)
-        {
-            alloChar[ch] = 1;
-        }
+        for(char ch: allowed) alloChar[ch] = 1;
 
         //now lets traverse through the words
-        for(string word: words)
-        {
-            checker(alloChar, ans, word);
-        }
+        for(string word: words) checker(alloChar, ans, word);
+
         return ans;
     }   
 
