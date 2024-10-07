e2c29082864c3332c025adc378584458db5897bb
Sairudra More <sairudra60@gmail.com>
2024-10-07 20:37:57 +0530

    Time: 8 ms (42.58%), Space: 11.5 MB (72.37%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -1,28 +1,40 @@
 class Solution {
 public:
     int minLength(string s) {
-        //bruteforce, using find, erase
-        string s1 = "AB";
-        string s2 = "CD";
-    
-        while(true)
+        ////bruteforce, using find, erase
+        //string s1 = "AB";
+        //string s2 = "CD";
+    //
+        //while(true)
+        //{
+        //    if(s.find(s1) != string::npos)
+        //    {
+        //        int index = s.find(s1);
+        //        s = s.erase(index, 2);
+        //    }
+        //    else if(s.find(s2) != string::npos)
+        //    {
+        //        int index = s.find(s2);
+        //        s = s.erase(index, 2);
+        //    }
+        //    else
+        //    {
+        //        //no more possible
+        //        break;
+        //    }
+        //}
+        //return s.length();
+
+        //now lets try with stack
+        stack<char> st;
+
+        for(char ch: s)
         {
-            if(s.find(s1) != string::npos)
-            {
-                int index = s.find(s1);
-                s = s.erase(index, 2);
-            }
-            else if(s.find(s2) != string::npos)
-            {
-                int index = s.find(s2);
-                s = s.erase(index, 2);
-            }
-            else
-            {
-                //no more possible
-                break;
-            }
+            if(st.empty()) st.push(ch);
+            else if(st.top() == 'A' && ch == 'B') st.pop();
+            else if(st.top() == 'C' && ch == 'D') st.pop();
+            else st.push(ch);
         }
-        return s.length();
+        return st.size();
     }
 };
