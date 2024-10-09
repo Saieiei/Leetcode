f7dfbda9b572910ed3a264a85a7d77b726dd58ed
Sairudra More <sairudra60@gmail.com>
2024-10-09 20:01:24 +0530

    Time: 3 ms (42.94%), Space: 7.8 MB (81.46%) - LeetHub

--- 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
+++ 0957-minimum-add-to-make-parentheses-valid/0957-minimum-add-to-make-parentheses-valid.cpp
@@ -2,18 +2,35 @@ class Solution {
 public:
 //week 11
     int minAddToMakeValid(string s) {
-        //we will solve this problem using stack
-        stack<char> st;
+        ////we will solve this problem using stack
+        //stack<char> st;
+//
+        //for(char ch: s)
+        //{
+        //    if(ch == '(') st.push(ch); //simply push the ( into the stack
+        //    else //if u get )
+        //    {
+        //        if(!st.empty() && st.top() == '(') st.pop();//if the stack is not empty and the top of the stack is a opening bracket, then we can cancel
+        //        else st.push(ch);// then simply push,
+        //    }
+        //}
+        //return st.size();
+
+
+        //u can also do it without stack
+        //this is another method of solving without the use of a stack
+        int open = 0;
+        int close = 0;
 
         for(char ch: s)
         {
-            if(ch == '(') st.push(ch); //simply push the ( into the stack
-            else //if u get )
+            if(ch == '(') open++;
+            else
             {
-                if(!st.empty() && st.top() == '(') st.pop();//if the stack is not empty and the top of the stack is a opening bracket, then we can cancel
-                else st.push(ch);// then simply push,
+                if(open>0) open--;
+                else close++;
             }
         }
-        return st.size();
+        return (open + close);
     }
 };
