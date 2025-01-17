5460b10c87b64ea9518f0e50ebbb106a131bcf56
Sairudra More <sairudra60@gmail.com>
2025-01-17 19:19:19 +0530

    Time: 6 ms (31.78%), Space: 263.9 MB (74.77%) - LeetHub

--- 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
+++ 2792-neighboring-bitwise-xor/2792-neighboring-bitwise-xor.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
+////easy only
+////we will use thge proprty of xor
+////if a+b = c, then a+c = b and b+c = a
     bool doesValidArrayExist(vector<int>& derived) {
-        bool ans = 0;
-        int len = derived.size();
-        vector<int> original (len, 0);
-        for(int i = 1; i<len; i++) //indexing from 0 only
-        {
-            original[i] = derived[i-1]^original[i-1];
-        }
-        //checker
-        if(derived[len-1] == (original[len-1]^original[0])) return true;
+//        bool ans = 0;
+//        int len = derived.size();
+//        vector<int> original (len, 0);
+//        for(int i = 1; i<len; i++) //indexing from 0 only
+//        {
+//            original[i] = derived[i-1]^original[i-1];
+//        }
+//        //checker
+//        if(derived[len-1] == (original[len-1]^original[0])) return true;
+//
+//        original.assign(len, 1);
+//        for(int i = 1; i<len; i++) //indexing from 0 only, 
+//        {
+//            original[i] = derived[i-1]^original[i-1];
+//        }
+//        //checker
+//        if(derived[len-1] == (original[len-1]^original[0])) return true;
+//
+//        return false;
 
-        original.assign(len, 1);
-        for(int i = 1; i<len; i++) //indexing from 0 only, but we do not wish to check the last index
-        {
-            original[i] = derived[i-1]^original[i-1];
-        }
-        //checker
-        if(derived[len-1] == (original[len-1]^original[0])) return true;
+//Understand that from the original element, we are using each element twice to construct the derived array
+//The xor-sum of the derived array should be 0 since there is always a duplicate occurrence of each element.
 
-        return false;
+        int ans = 0;
+        for(int element: derived)
+        {
+            ans = ans^element;
+        }
+        if(ans == 0) return 1; else return 0;
     }
 };
