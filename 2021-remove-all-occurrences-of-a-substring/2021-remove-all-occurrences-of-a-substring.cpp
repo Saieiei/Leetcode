8df015ca2cd18725fe1124dfe01fa9245d9b70be
Sairudra More <sairudra60@gmail.com>
2025-02-11 21:49:26 +0530

    Time: 0 ms (100%), Space: 9.4 MB (34.81%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.cpp
@@ -1,42 +1,42 @@
 class Solution {
 public:
-    //string removeOccurrences(string s, string part) {
-    //    while(s.find(part)!=string::npos) //which means found
-    //    {
-    //        s.erase(s.find(part), part.length());
-    //    }
-    //    return s;
-    //}
+ /*
+   We want to remove all occurrences of 'part' from string 's'.
+   Instead of repeatedly searching for "part" across the entire string,
+   we can simulate this removal with a stack-like approach:
 
-    //we will try to solve this using recurssion
-    //week7
+   1. Traverse each character of s.
+   2. Append the character to an output string (think of it as pushing to a stack).
+   3. After each append, check if the output string's tail matches 'part'.
+      - If it does, remove the occurrence of 'part' from the end
+        (like popping from the stack).
 
-    //1st we will find 
+   This works because every time we insert a new character, the only
+   new possible occurrence of 'part' could be at the end of the new string.
+*/
+string removeOccurrences(string s, string part) {
+    // This will act as our "stack" or output builder
+    string result;
+    
+    // Pre-calculate the length of 'part' for convenience
+    int partLen = part.size();
+    
+    // Iterate through every character in s
+    for (char c : s) {
+        // "Push" the current character to our result
+        result.push_back(c);
 
-    void removeOccurRF(string &s, string part)
-    {
-        //1st we will find the index of the part in the string
-        //calclulaions
-        int FoundIndex=s.find(part);
-        if(FoundIndex!=string::npos) //which means that it is actually there in the string, and did not exceed the string s
-        {
-            //now we will focus on removing the part from the string
-            s.erase(FoundIndex, part.length()); //s.earse(erase from which index, length to be erased)
-            //if u dont wish to use erase, then u can split it from left part and right part, then later on combine it
-                //string left_part=s.substr(0, FoundIndex);
-                //string right_part=s.substr(FoundIndex+part.length(), s.length());
-                //s=left_part+right_part;      
-                
-            //recurssion for the rest
-            removeOccurRF(s, part);
+        // Check if the current tail of 'result' matches 'part'
+        if (result.size() >= partLen) {
+            // Compare the substring at the end of 'result' with 'part'
+            if (result.compare(result.size() - partLen, partLen, part) == 0) {
+                // If they match, remove this occurrence
+                // (like popping from stack)
+                result.erase(result.size() - partLen, partLen);
+            }
         }
-        //base case
-        return; //if the part is not founnd
     }
 
-    string removeOccurrences(string s, string part) {
-        //we will do manupulation on the string itself, so need of any return datatype
-       removeOccurRF(s, part);
-       return s;
-    }
+    return result;
+}
 };
