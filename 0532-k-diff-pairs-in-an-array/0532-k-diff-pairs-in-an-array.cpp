a996318b586ba189ceb98be87ca72e2530784f0b
Sairudra More <sairudra60@gmail.com>
2025-07-03 19:50:43 +0530

    Time: 4 ms (78.36%), Space: 17.2 MB (40.45%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
-    int binarysearch(vector<int>& nums,  int start, int target)
-    {
-        int end=nums.size()-1;
-        int mid=start+(end-start)/2;
-        while(end>=start)
-        {
-            mid=start+(end-start)/2;
-            if(nums[mid]==target)
-            {
-                return 1; //found
-            }
-            else if(nums[mid]<target)
-            {
-                start=mid+1;
-            }
-            else //nums[mid]>target
-            {
-                end=mid-1;
-            }
-        }
-        return -1;
-    }
     int findPairs(vector<int>& nums, int k) {
-        //method 2:
-        //we will sort it and then use binary search 
-        //we know that nums[j]-nums[i]=k, hence, we can say that the target = k+nums[i]
-        //so we will go through each element (nums[i]) and try to find out the target
-        //if we r able to find out the target then we will store the pair ain the set as i!=j
+        int ans = 0;
+        //this is like two sums 
+        //its simple only
+        //frequency, not needed, we just need the 1st part
+        unordered_map<int, int>mp;
+        for(int num: nums) mp[num]++; 
 
-        sort(nums.begin(), nums.end());
-        set<pair<int, int>> ans;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            if(binarysearch(nums, i+1, nums[i]+k)!=-1) //TARGET FOUND
-            {
-                ans.insert({nums[i], nums[i]+k});
+        //but if k = 0, then basically it will start counting itself even though its wrong
+        //to avoid this will we check on its frequency, if > 1 then ans++
+        if (k == 0) {
+            for (auto& [num, frequency] : mp) {
+                if (frequency > 1) {
+                    ans++;
+                }
             }
         }
-        return ans.size();
-        
+        //just find its complement, dw, big ones always win
+        else
+        {
+            for(auto&  [num, _]: mp)
+            {
+                if(mp.find(num-k) != mp.end()) ans++;
+            }
+        }
+        return ans;
     }
 };
