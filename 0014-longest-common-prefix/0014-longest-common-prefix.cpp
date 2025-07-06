45213e7cbeb10a443ea835817eeac3ba56e7f0ff
Sairudra More <sairudra60@gmail.com>
2025-07-07 00:31:05 +0530

    Time: 0 ms (100%), Space: 12.1 MB (19.68%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string longestCommonPrefix(const vector<string>& strs) {
+        //this can be done using substr and prefix
+        //we will take the 1st string as complete prefix it self
+        //then in each of the string we will find if its there, if not, then decrease the prefix by 1 (substr())
+        if (strs.empty()) return "";
+
+        string prefix = strs[0];
+
+        for (int i = 1; i < strs.size(); i++) {
+            while (strs[i].find(prefix) != 0) {
+                prefix = prefix.substr(0, prefix.length() - 1);
+                
+                if (prefix.empty()) return "";
+            }
+        }
+        return prefix;
+    }
+};
