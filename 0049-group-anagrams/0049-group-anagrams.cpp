e1cd7047bf6ac476b0c489468b5740a526f79981
Sairudra More <sairudra60@gmail.com>
2025-07-07 23:43:05 +0530

    Time: 14 ms (77.3%), Space: 24.9 MB (85.2%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,50 +1,30 @@
-//week5_ass_6
-//method 2 map(hash)
 class Solution {
 public:
+    vector<vector<string>> groupAnagrams(const vector<string>& strs) {
+        //its simple only
+        //we will use hash to grp them together
+        //then we will sort those grps and push it into vector<vector<string>>
 
-    std::array<int, 256>hash(string str) 
-    {
-        std::array<int, 256>hash={0}; //the function name and the variable name both r hash
-        for(char i:str)
+        unordered_map<string, vector<string>> grouper;
+        //group anagrams
+        for(string str: strs)
         {
-            hash[i]++;
+            string sort_str = str;
+            sort(sort_str.begin(), sort_str.end());
+            grouper[sort_str].push_back(str);
         }
-        return hash;
-        //everytime u will return a new hash
 
-    }
-
-    vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        //we have to use map(sorting-hashing(mapping))
-            //if u do not know map then map is like, there are 2 things in map(fisrt, second), u will be able to assign multiple values in second(groups) and   map it to 1 value to first
-                //first         second
-                //key           values
-                //e=1
-                //a=1
-                //t=1         {"eat","tea","ate"}
-                //map<std::array<int, 256>, vector<string>> mp      mp[hash(str)].push_back(st), over here    std::array<int, 256> is same as int array[256]={0}, and hash is a function 
-                //for(auto it=mp.begin(); it!=mp.end(); i++)
-                //{
-                //  ans.push_back(it->second) //basically i am pushing back the values of a mapped strings in a new vector called ans
-                //}
-        
         vector<vector<string>> ans;
-        map<std::array<int, 256>, vector<string>> mp;
 
-        //everytime u will ask to hash the word
-        for(auto word:strs)
+        //we will sort the vector<string> and push into ans to return it
+        for(auto& [key, group]: grouper)
         {
-            mp[hash(word)].push_back(word);
-            //if that hash table is already present it will get mapped accordingly
+            //sort(group.begin(), group.end());
+            ans.push_back(group);
         }
 
-        for(auto it=mp.begin();it!=mp.end();it++) //iterator, to travel through each i of map
-        {
-            ans.push_back(it->second);
-        }
+        //finally sort ur ans
+        //sort(ans.begin(), ans.end());
         return ans;
-        
     }
-
 };
