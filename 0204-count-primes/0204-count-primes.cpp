a02c6d81a3cb23b5c15abe21aebf28297b9ab37c
Sairudra More <sairudra60@gmail.com>
2025-07-16 22:17:55 +0530

    Time: 355 ms (9.2%), Space: 132.7 MB (10.85%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,54 +1,31 @@
-// what we have to do is 1st create an array from 0 to n
-// we will makr all those values are true(which means all r prime)
-// then we will use the for loop from 2 to n
-// we will check if that value is prime or not
-// if its prime then we will increement the ans
-// then we will make all its multiples as false till we reach n
-// in the end return the ans
-
-//class Solution {
-//public:
-//   int countPrimes(int n) {
-//       vector<bool> prime(n, true);
-//       int ans=0;
-//       //prefdefine values
-//       prime[0]=false;
-//       prime[1]=false;
-//       if(n==1) return ans;
-//       for(int i=2;i<n;i++)
-//       {
-//           if(prime[i])
-//           {
-//               ans++;
-//               int j=2*i;
-//               while(j<n) //multiples of i making them as true
-//               {
-//                   prime[j]=false;
-//                   j=j+i;
-//               }
-//           }
-//       }
-//       return ans;
-//   }
-//};
 class Solution {
 public:
     int countPrimes(int n) {
-        if (n==0)
+
+        if(n < 2){
             return 0;
-        vector<bool> prime(n, true); // all.
-        prime[0] = prime[1] = false;
-        int ans = 0;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) {
-                ans++;
-                int j = 2 * i;
-                while (j < n) {  //multiples of i making them as true
-                    prime[j] = false;
-                    j += i;
-                }
+        }
+
+        vector<int> arr(n, 1);
+        
+        for(int i = 2; i*i < n ; i++)
+        {
+            for (int j = i*i; j < n; j+=i)
+            {
+                arr[j] = 0;
             }
         }
-        return ans;
+
+        int prime = 0;
+
+        for (int i = 2; i < n; i++) //we go till 1 number below
+        {
+            if(arr[i] == 1)
+            {
+                prime++;
+            }
+        }
+
+        return prime;
     }
 };
