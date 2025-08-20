c7590656420e134de930f6abeb0faa161f64df99
Sairudra More <sairudra60@gmail.com>
2025-08-20 18:08:25 +0530

    Time: 33 ms (83.95%), Space: 18.5 MB (82.06%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -8,44 +8,82 @@
  */
 class Solution {
 public:
-//week 10
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        //its simple only
+        //if the length of the LLs r the same, then Node a == Node b at some point of iterations
+        //if they are not the same then, obio some LL is bigger than the other
+        //we will have to figure out which LL is bigger than the other and by how much
+            //this will help us find out by how much we should keep our Head ahead such that Node a == Node b at some point of iterations
+        //there can also be a case where the LL doesnt intersect and they could be of the same length
+            //in that case, both the LL will be pointing to NULL and the nodes can never be the same, then return NULL;
+
         if (!headA || !headB) return nullptr;
 
-        ListNode *tempA = headA;
-        ListNode *tempB = headB;
-        
-        // Traverse both lists to find the lengths
-        int lenA = 0, lenB = 0;
-        while (tempA) {
-            lenA++;
-            tempA = tempA->next;
-        }
-        while (tempB) {
-            lenB++;
-            tempB = tempB->next;
-        }
+        //if the length of the LLs r the same, then Node a == Node b at some point of iterations
+        ListNode* a = headA;
+        ListNode* b = headB;
 
-        // Adjust starting points
-        tempA = headA;
-        tempB = headB;
-        if (lenA > lenB) {
-            for (int i = 0; i < lenA - lenB; i++) {
-                tempA = tempA->next;
+        while(a->next && b->next)
+        {
+            if(a == b)
+            {
+                return a;
             }
-        } else {
-            for (int i = 0; i < lenB - lenA; i++) {
-                tempB = tempB->next;
+            a= a->next;
+            b= b->next;
+        }
+
+        //there can also be a case where the LL doesnt intersect and they could be of the same length
+            //in that case, both the LL will be pointing to NULL and the nodes can never be the same, then return NULL;
+        if(!a->next && !b->next && a!=b) return NULL;
+
+        //if they are not the same then, obio some LL is bigger than the other
+        //we will have to figure out which LL is bigger than the other and by how much
+            //this will help us find out by how much we should keep our Head ahead such that Node a == Node b at some point of iterations
+        int count =0;
+        if(b->next == NULL)
+        {
+            while(a->next)
+            {
+                a=a->next;
+                count++;
+            }
+            while(count--)
+            {
+                headA = headA->next;
+            }
+            while(headA && headB)
+            {
+                if(headA == headB)
+                {
+                    return headA;
+                }
+                headA= headA->next;
+                headB= headB->next;
+            }
+            
+        }
+        else
+        {
+            while(b->next)
+            {
+                b=b->next;
+                count++;
+            }
+            while(count--)
+            {
+                headB = headB->next;
+            }
+            while(headA && headB)
+            {
+                if(headA == headB)
+                {
+                    return headA;
+                }
+                headA= headA->next;
+                headB= headB->next;
             }
         }
-
-        // Traverse together to find intersection
-        while (tempA && tempB) {
-            if (tempA == tempB) return tempA;
-            tempA = tempA->next;
-            tempB = tempB->next;
-        }
-
-        return nullptr; // No intersection
+        return nullptr;
     }
 };
