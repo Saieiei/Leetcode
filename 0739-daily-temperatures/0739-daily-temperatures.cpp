073f3ab3f0cd7a310a7c36502f56df521690224f
Sairudra More <sairudra60@gmail.com>
2025-08-29 19:02:36 +0530

    Time: 43 ms (6.11%), Space: 108.1 MB (16.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,50 +1,58 @@
+#include<algorithm>
 class Solution {
 public:
-//week 11
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        //we will use stack and a new vector concept
-        //the stack will be used to refer the index of the temperature vector
-        //the new vector (ans) will be used to store the difference of the index
-        //we will go from behind
-        //in the stack we will push the last element(index) of the vector and push 0 in the ans vector
-        //then we will see the 2nd last element of the vector and we will compare if the elment is greater than the 1 in the stack, 
-            //if greater then we will find out the difference of the indexes and put it in the ans vector,
-            //else then we pop the top element of the stack and then compare the element the top element of the stack with the tep
-        //this process will be going on until we find a number in the stack that is greater than the temp
-        //but if all the elements have been popped then we will have to push the element in the stack(index) and push 0 in the ans vector
-        //in the end reverse the ans vector
-        //https://www.youtube.com/watch?v=7MIXlgBfr_U
+        //this is simple only, but we start from back side
+        //stack<pair>, pair-> <temp, days>
+        //we also need ans which is a vector to keep the track of the final days that we get for each temp
+        //initially just push in the temp with 0 days and update the ans vector
+        //if u get a temp that is bigger than than top() then simply keep poping until u get a top()<temp and keep adding up the days
+        //while popping if u the stack is empty then just push in the temp with 0 days and update the ans vector
+        //but if the stack is not empty and u get a top()<temp, then simply push in the temp and 1+ the accumulated days and update the ans vector
+        //in the end reverse the ans vector and return it
 
-        stack<int>s;
         vector<int> ans;
-        int n=temperatures.size()-1; //last index
-        for(int i=n;i>=0;i--)
+        stack<pair<int, int>> st; //<temp, days>
+
+        //initially just push in the temp with 0 days
+        if(st.empty()) st.push({temperatures[temperatures.size()-1], 0});
+        ans.push_back(0);
+        //we will start from the back
+        for(int i=temperatures.size()-2; i>=0; i--)
         {
-            if(s.empty())
+            int temp = temperatures[i];
+            int accumulator = 0; //days
+            ////if the stack is empty simply push with 0
+            //if(st.empty()) st.push({temp, 0});
+            //ans.push_back(accumulator);
+
+            //temp >= st.top().first then simply pop and accumulate the days
+            if(!st.empty() && temp >= st.top().first)
             {
-                s.push(i);
-                ans.push_back(0);
-            }
-            else
-            {
-                while(!s.empty() && temperatures[i]>=temperatures[s.top()])
+                while(!st.empty() && temp >= st.top().first) //st is not empty
                 {
-                    s.pop();
+                    accumulator = accumulator + st.top().second;
+                    st.pop();
                 }
-                if(s.empty())
+                if(st.empty()) //after continous poping in case it goes empty
                 {
-                    s.push(i);
+                    st.push({temp, 0}); //temp remainded the bigesset during the popping
                     ans.push_back(0);
-                }
-                else
-                {
-                    ans.push_back(s.top()-i);
-                    s.push(i);
-                }
+                    accumulator = 0;
+                } 
             }
+            if(temp < st.top().first) //then simply push with the updated days
+            {
+                int days = 1+accumulator;
+                st.push({temp, days});
+                ans.push_back(days);
+                accumulator = 0;
+            }
+
         }
+        //reverse and return 
         reverse(ans.begin(), ans.end());
         return ans;
-        //u can also use use 2 pointers approach but that will be O(n^2)
+        
     }
 };
