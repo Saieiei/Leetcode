828c0415ed49e0d0af88bd23fde3c0c5575fb97d
Sairudra More <sairudra60@gmail.com>
2025-09-21 14:20:39 +0530

    Time: 0 ms (100%), Space: 23.1 MB (23.34%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -9,69 +9,39 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//class Solution {
-//public:
-////week 13
-//    int height(TreeNode* root)
-//    {
-//        if(root == NULL) return 0;
-//        else
-//        {
-//            int leftHeight = height(root->left);
-//            int rightHeight = height(root->right);
-//            int finalAns = max(leftHeight, rightHeight) +1; //because of the root node
-//            return finalAns;
-//        }
-//    }
-//    bool isBalanced(TreeNode* root) {
-//        //for this to work, u should know how to find the height of a node
-//        //for a node to be balanced, its right height and left height should not differ more tham 1
-//        //for the rest of the nodes, we will do recursion
-//        //this is very bad approach but
-//        //base case
-//        if(root == NULL) return true;
-//
-//        //for the currentNode
-//        int leftHeight = height(root->left);
-//        int rightHeight = height(root->right);
-//        int diff = abs(leftHeight - rightHeight);
-//        bool currentNode = diff<=1;
-//
-//        //now we will for the rest of the nodes
-//        bool left = isBalanced(root->left);
-//        bool right = isBalanced(root->right);
-//
-//        //check condition 
-//        if(currentNode && left && right) return true;
-//        else return false;
-//    }
-//};
-
 class Solution {
 public:
-//week13
-    bool ans = true;
-    int height(TreeNode* root)
+//for this q we will use the logic to calculate the height(recurssion)(maxdepth)
+//then to compute this, at each node we will find it its balanced, then check left , then check right (all use recursion again)
+//if node is balanced (abs(!>1)) then return true, else false
+//if check if left subtree and right subrtree are balanced
+
+    int maxDepth(TreeNode* root)
     {
-        if(!root) return 0;
-        //the height fucntion will go to each and everynode and return its height
-        //it works on bottom up approach
-        //do for the 1st node
-        int leftHeight = height(root->left);
-        int rightHeight = height(root->right);
+        //base case
+        if(root == NULL)return 0;
 
-        //check if balanced
-        int balanced = abs(leftHeight - rightHeight);
-        if( ans && balanced>1) ans = false;
-
-        //calculate its height
-        return (max(leftHeight, rightHeight) + 1);
+        int op1 = maxDepth(root->left)+1;
+        int op2 = maxDepth(root->right)+1;
+        return max(op1, op2);
     }
-    bool isBalanced(TreeNode* root)
-    {
-        //for this u have to know the concept of height
-        //the height fucntion will go to each and everynode and return its height
-        height(root);
-        return ans;
+    bool isBalanced(TreeNode* root) {
+        //base case
+        if(root==NULL)return true;
+        //node
+        int leftTreeHeight = maxDepth(root->left);
+        int rightTreeHeight = maxDepth(root->right);
+        int absDiff = abs(leftTreeHeight-rightTreeHeight);
+        if(absDiff>1) return false;
+        else 
+        {
+            //left
+            bool leftSubTree = isBalanced(root->left);
+            //right
+            bool rightSubTree = isBalanced(root->right);
+                //check if left and right are balanced
+                if(leftSubTree && rightSubTree) return true;
+                else return false;
+        }
     }
 };
