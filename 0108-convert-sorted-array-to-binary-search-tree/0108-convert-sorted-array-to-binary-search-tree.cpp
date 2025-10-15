2507c472641550af4913db0bf91a92b05a74a1cb
Sairudra More <sairudra60@gmail.com>
2025-10-15 22:06:05 +0530

    Time: 114 ms (5.08%), Space: 345.7 MB (5.59%) - LeetHub

---
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    TreeNode* sortedArrayToBSTHelper(vector<int> nums, int start, int end)
+    {
+        if(start>end)return NULL;
+
+        int mid = start + (end-start)/2;
+        TreeNode* root = new TreeNode(nums[mid]);
+
+        //recursion
+        root->left = sortedArrayToBSTHelper(nums, start, mid-1);
+        root->right = sortedArrayToBSTHelper(nums, mid+1, end);
+
+        //backtrack
+        return root;
+    }
+
+    //the intuition comes when u take the middle ele of the vector it makes sense
+    //the left of the middle ele is gonna be the LST
+    //and the right if the middle ele is gonna be the RST
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        
+        int start = 0; int end = nums.size()-1;
+        TreeNode* root = sortedArrayToBSTHelper(nums, start, end);
+        return root;
+    }
+};
