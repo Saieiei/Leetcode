1fe090dcc3e96f1dfc41f00f1de3bac6abfe5507
Sairudra More <sairudra60@gmail.com>
2025-11-16 13:45:45 +0530

    Time: 103 ms (42.07%), Space: 51.4 MB (31.45%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,41 +1,71 @@
+class info{
+    public:
+    int val;
+    int row;
+    int col;
+    info(int data, int rowIndex, int colIndex): val(data), row(rowIndex), col(colIndex) {}
+};
+
+class compare{
+    public:
+    bool operator()(info* a, info* b){
+        return a->val > b->val;
+    }
+};
+
+
+//to do this we will make use of min heap
+//so the idea is that we will have a custome data structure (info) which will have the value, the col and the row 
+//then we will push in the 1st element (info) of each vector into the pq
+//keep track of the minAns and maxAns
+//as we keep inserting and poping we need to keep track of maxi and mini
+//then we will compare it with the diff of maxAns-minAns and maxi-mini, which ever is small we will update the minAns and maxAns
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        // Min-Heap: stores (value, list index, element index)
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
-        int curMax = numeric_limits<int>::min();
+        priority_queue<info*, vector<info*>, compare> pq;
+        vector<int> ans;
+        int minAns = INT_MAX;
+        int maxAns = INT_MIN;
 
-        // Initialize the heap with the first element of each list
-        for (int i = 0; i < nums.size(); i++) {
-            minHeap.push({nums[i][0], i, 0});
-            curMax = max(curMax, nums[i][0]);
+        //so the idea is that we will have a custome data structure (info) which will have the value, the col and the row 
+        for(int i=0; i<nums.size(); i++){
+            int elementToPush = nums[i][0];
+            //before we push we will keep the track of minAns and maxAns as this could be the 1st ans 
+            minAns = min(minAns, elementToPush);
+            maxAns = max(maxAns, elementToPush);
+            info* toPush = new info(elementToPush, i, 0);
+            pq.push(toPush);
         }
-        // Track the smallest range
-        vector<int> smallRange = {0, numeric_limits<int>::max()};
 
-        while (!minHeap.empty()) {
-            // Get the minimum element from the heap
-            vector<int> curr = minHeap.top();
-            minHeap.pop();
-            int curMin = curr[0], listIdx = curr[1], elemIdx = curr[2];
+        //this is for the comparison
+        int mini = minAns;
+        int maxi = maxAns;
 
-            // Update the smallest range if a better one is found
-            if ((curMax - curMin < smallRange[1] - smallRange[0]) ||
-                (curMax - curMin == smallRange[1] - smallRange[0] && curMin < smallRange[0])) {
-                smallRange[0] = curMin;
-                smallRange[1] = curMax;
+        while(!pq.empty()){
+            info* front = pq.top(); pq.pop();
+
+            //now in min heap we can keep the track of the min element with the top (mini)
+            int frontElement = front->val;
+            mini = frontElement;
+
+            //compare for now (maxAns, minAns)
+            if(maxi-mini < maxAns-minAns){
+                maxAns=maxi;
+                minAns=mini;
             }
 
-            // Move to the next element in the same list
-            if (elemIdx + 1 < nums[listIdx].size()) {
-                int nextVal = nums[listIdx][elemIdx + 1];
-                minHeap.push({nextVal, listIdx, elemIdx + 1});
-                curMax = max(curMax, nextVal);
-            } else {
-                // If any list is exhausted, stop
-                break;
+            //now we check if we can insert and check if that can be maxi
+            if(front->col + 1 < nums[front->row].size()){
+                info* temp = new info(nums[front->row][front->col + 1], front->row, front->col + 1);
+                pq.push(temp);
+                maxi = max(maxi, nums[front->row][front->col + 1]);
             }
+            else  break; //we need eleemts from all the vectors, so we can just leave any1
         }
-        return smallRange;
+        ans.push_back(minAns);
+        ans.push_back(maxAns);
+        return ans;
+        //return {minAns, maxAns;
     }
 };
