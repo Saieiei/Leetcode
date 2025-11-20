cc6657db38ddaac9b62e6948e91685a080924cdb
Sairudra More <sairudra60@gmail.com>
2025-11-20 23:22:56 +0530

    Time: 57 ms (21.07%), Space: 153.3 MB (12.99%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,64 +1,29 @@
+//basically we will push in the 1st k elements in the heap and get the top
+//the next next what we have to do is play with index to remove them as we move forward
+//the formula is top->index <= i-k then keep poping
+//to keep the track of the index too with the eleemnt we will use pairs in the heap
+
 class Solution {
 public:
-//week 12 
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-  //this deque method requires O()
-   deque<int> dq;
-   //in the deque, we will store the indeex of the greatest element
-    vector<int> result;
-    //in case of sliding window, just rememebr this technique
-        //while (condition)->pop_back()
-        //push_back()
-        //sliding window limit exceeded pop_front()
-        //put the 1st elemet into the ans, after size of the sliding window, front()
+        priority_queue<pair<int, int>> pq;
+        vector<int> ans;
 
-    for (int i = 0; i < nums.size(); ++i) {
-        // Remove elements from the back if they are smaller than the current element
-        while (!dq.empty() && nums[dq.back()] < nums[i]) {
-            dq.pop_back();
+        //proceess teh 1st k elements
+        for(int i=0; i<k; i++){
+            pq.push({nums[i], i});
+        }
+        ans.push_back(pq.top().first);
+
+        //now we will process the rest of the elemets
+        for(int i=k; i<nums.size(); i++){
+            pq.push({nums[i], i});
+
+            while(pq.top().second <= i-k) pq.pop();
+
+            ans.push_back(pq.top().first);
         }
 
-        // Add the current element's index to the deque
-        dq.push_back(i);
-
-        // Remove the element from the front if it's outside the current window
-        if (dq.front() == i - k) {
-            dq.pop_front();
-        }
-
-        // Start adding results to the output list after the first k elements
-        if (i >= k - 1) {
-            result.push_back(nums[dq.front()]);
-        }
-    }
-
-    return result; 
-
-    //we can do the same using sets, which can do it O(nlogn)
-    //    vector<int> ans;
-    //    set<pair<int, int>, greater<pair<int,int>>> st;
-//
-    //    // Initialize the set with the first k elements
-    //    for (int i = 0; i < k; i++)
-    //        st.insert({ nums[i], i });
-//
-    //    // The maximum element in the first window
-    //    ans.push_back((st.begin())->first);
-//
-    //    // Process the remaining elements
-    //    for (int i = k; i < nums.size(); i++) {
-//
-    //        // Remove the (i-k)th element from the window
-    //        st.erase({ nums[i - k], i - k});
-//
-    //        // Add the current element to the set
-    //        st.insert({ nums[i], i});
-//
-    //        // The maximum element in the current window
-    //        ans.push_back(st.begin()->first);
-    //    }
-//
-    //    return ans;
-
+        return ans;
     }
 };
