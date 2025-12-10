966c0b72418d0885f81d62399294c47144f6f1ac
Sairudra More <sairudra60@gmail.com>
2025-12-10 09:17:06 +0530

    Time: 3 ms (64.69%), Space: 24.4 MB (5.84%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -1,47 +1,120 @@
+#include <vector>
+#include <string>
+#include <queue>
+#include <algorithm>
 
-class MyComparator{
-    public:
-    bool operator()(pair<int, string>& p1, pair<int, string>& p2){
-        //incase the freq is same then alphabetic order
-        if(p1.first == p2.first) return p1.second<p2.second;
-        //else return base on higher freq
-        else return p1.first>p2.first;
+//this is for pq (MinHeap)
+class comp {
+public:
+    bool operator()(pair<int, string>& a, pair<int, string>& b) {
+        return a.first == b.first ? a.second < b.second : a.first > b.first;
     }
 };
 
-//there are 2 ways to do this (hahsmaps + pq) or with tries
-//here we will go with prior 1
-//u can solve this with maxheap, but we will do it minheap with our custome comparator
+class TrieNode {
+public:
+    char data;
+    TrieNode* children[26];
+    bool isTerminal;
+    int freq;
+
+    TrieNode(char ch) : data(ch), isTerminal(false), freq(0) {
+        for (int i = 0; i < 26; ++i) {
+            children[i] = nullptr;
+        }
+    }
+};
+
+class Trie {
+    TrieNode* root;
+
+    void insertUtil(TrieNode* root, string& word, int i) {
+        //basecase
+        if (i >= word.size()) {
+            root->isTerminal = true;
+            root->freq++;
+            return;
+        }
+        
+        int index = word[i] - 'a';
+        if (!root->children[index])
+            root->children[index] = new TrieNode(word[i]);
+
+        //recursion
+        insertUtil(root->children[index], word, i + 1);
+    }
+
+    void traverseUtil(TrieNode* root, string& s, priority_queue<pair<int, string>, vector<pair<int, string>>, comp>& pq, int& k) {
+        //BC
+        if (!root) return;
+        if (root->isTerminal) {
+            if (pq.size() == k && root->freq > pq.top().first) {
+                pq.pop();
+                pq.push({root->freq, s});
+            } else if (pq.size() < k) {
+                pq.push({root->freq, s});
+            }
+        }
+        //we need to travel on each kid that exists
+        for (int i = 0; i < 26; ++i) {
+            if (root->children[i]) {
+                s.push_back(i + 'a');
+                //R
+                traverseUtil(root->children[i], s, pq, k);
+                //BT
+                s.pop_back();
+            }
+        }
+    }
+
+public:
+    Trie() : root(new TrieNode('\0')) {}
+
+    void insert(string word) {
+        insertUtil(root, word, 0);
+    }
+
+    void traverse(priority_queue<pair<int, string>, vector<pair<int, string>>, comp>& pq, int& k) {
+        string s;
+        traverseUtil(root, s, pq, k);
+    }
+};
+
+//reverse order 
+bool mysort(const pair<int, string>& a, const pair<int, string>& b) {
+    if (a.first == b.first) {
+        return a.second < b.second;
+    }
+    return a.first > b.first;
+}
+
+//using tries
 class Solution {
 public:
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> mp;
-
-        //frequency of the word
-        for(string word: words){
-            mp[word]++;
+        priority_queue<pair<int, string>, vector<pair<int, string>>, comp> pq;
+        Trie trie;
+        //insert the words into the trie (just like hashmaps)
+        for (auto& word : words) {
+            trie.insert(word);
         }
 
-        priority_queue<pair<int, string>, vector<pair<int, string>>, MyComparator>pq;
+        //to insert every work in the pq
+        trie.traverse(pq, k);
 
-        //push all of them from map to pq
-        for(auto it: mp){
-            pq.push({it.second, it.first});
-
-            //now the top elements of pq will be the least freq
-            //so if the size of pq > k then pop them
-            if(pq.size()>k) pq.pop();
-        }
-        
-        //now we have top k pairs but in decending order (min heap)
-        int index = k-1;
-        vector<string> ans(k);
-        while(!pq.empty()){
-            pair<int, string> front = pq.top(); pq.pop();
-            ans[index--] = front.second;
+        vector<pair<int, string>> temp;
+        while (!pq.empty()) {
+            temp.push_back(pq.top());
+            pq.pop();
         }
 
+        //the top elements will have less freq, so we have to reverse it
+        sort(temp.begin(), temp.end(), mysort);
+
+        vector<string> ans;
+        for (auto& val : temp) {
+            ans.push_back(val.second);
+        }
         return ans;
-
     }
 };
