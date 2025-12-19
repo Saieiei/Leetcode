a4d2122cf0833b436736b9ed716e0caab0f3c8db
Sairudra More <sairudra60@gmail.com>
2025-12-19 16:49:45 +0530

    Time: 2 ms (76.51%), Space: 7.9 MB (36.49%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,17 +1,26 @@
-//we will be doing it using recustion 1st
+//we will be doing it using recustion 1st and then DP(memoization)
 class Solution {
 public:
 
-    int recursionFib(int n){
+    int recursionFib(int n, vector<int>& dp){
         //basecase
         if(n==0 || n==1) return n;
+        //check if we have the result usinf dp
+        if(dp[n] != -1){
+            return dp[n];
+        }
 
         //recursion
-        return recursionFib(n-1) + recursionFib(n-2);
+        int ans = recursionFib(n-1, dp) + recursionFib(n-2, dp);
+        //store the ans in dp
+        dp[n] = ans;
+        return ans;
     }
 
     int fib(int n) {
-        int ans = recursionFib(n);
+        //for DP
+        vector<int> dp(100, -1);
+        int ans = recursionFib(n, dp);
         return ans;
     }
 };
