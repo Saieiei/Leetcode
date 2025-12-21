49d28a1de75738e6d60c435cd4e3a917cf56c2e4
Sairudra More <sairudra60@gmail.com>
2025-12-21 14:00:09 +0530

    Time: 19 ms (78.93%), Space: 17.8 MB (91.79%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,36 +1,35 @@
-//M2 recursion + Memoisation
+//M3 recursion + Tabulation
 //i still dont know how it all works
 class Solution {
 public:
-    int recursionMemo(vector<int>& coins, int amount, vector<int>& dp){
-        //basecase
-        //4. check if its there in dp
-        if(amount == 0) return 0;
-        if(dp[amount] != -1) return dp[amount];
+    int recursionTabu(vector<int>& coins, int amount){
+        //1. create dp
+        int n = amount;
+        vector<int>dp(n+1, INT_MAX); //for each block, it will take INT_MAX amount of ways(not ideal)
 
-        //only if there is a chnage we will take it, and less amount of coins 
-        int mini = INT_MAX; 
+        //2. Analyse BC and update the dp
+        dp[0] = 0;
 
-        for(int i=0; i<coins.size(); i++){
-            //1st check if the amount > coin, then sub
-            if(coins[i] <= amount){
-                int res = recursionMemo(coins, amount - coins[i], dp);
-                if(res != INT_MAX) mini = min(mini, 1 + res); //used up 1 coin
+        //3. for loop, reversed, copy-paste, replace functions, looping variable
+        for(int value=1; value<=amount; value++){
+            int mini = INT_MAX;
+            for(int i=0; i<coins.size(); i++){
+                //1st check if the amount > coin, then sub
+                if(coins[i] <= value){
+                    int res = dp[value - coins[i]];
+                    if(res != INT_MAX) mini = min(mini, 1 + res); //used up 1 coin
+                }
             }
+            dp[value] = mini;
         }
-        //3. update the dp
-        dp[amount] = mini;
-        return mini;     
+        //4. return accordingly
+        return dp[amount];     
     }
 
     int coinChange(vector<int>& coins, int amount){
 
-        //1. create the dp
-        int n = amount;
-        vector<int>dp(n+1, -1);
+        int ans = recursionTabu(coins, amount);
 
-        //2. pass and update the dp
-        int ans = recursionMemo(coins, amount, dp);
         if(ans == INT_MAX) return -1; //no possible outcomes
         else return ans;
 
