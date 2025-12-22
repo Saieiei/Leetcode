e0c244e35461ec6896914d8931dacaee080e74d0
Sairudra More <sairudra60@gmail.com>
2025-12-22 11:05:27 +0530

    Time: 23 ms (20.56%), Space: 9.6 MB (82.86%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,31 +1,39 @@
-//M2 Memoization
+//M4 SO-1
 //we will take 1 dice and use all its faces(values)
     //as we keep going we will reduce the target and the no of dices used
 class Solution {
 public:
     long long int mod = 1000000007; //we use this mod because the q is telling us to use it
 
-    int recursionMemo(int n, int k, int target, vector<vector<long long int>>& dp){
-        //3. basecase and check dp
-        if(n==0 && target == 0) return 1; //u have no dice left and u have to make 0 sum
-        if(n==0 && target != 0) return 0; //u have no dices left and u have to make target
-        if(n!=0 && target == 0) return 0; //u have deices left and u have to make sum 0
-        if(n<0 || target <0) return 0; 
-        if(dp[n][target] != -1) return dp[n][target];
+    int recursionSO1(int n, int k, int target){
+        //1. we just need 2 rows(curr, prev)
+        vector<int> curr(target+1, 0); vector<int> prev(target+1, 0);
+        //dp[0][0] = 1; is basically the 1st block, and the 1st block in our logic comes in prev[0]
+        prev[0] = 1;
 
-        int ans = 0;
-        for(int value=1; value<=k; value++){
-            ans = (ans + recursionMemo(n-1, k, target-value, dp)) %mod; //we use mod because the q is telling us
+        //2. for loop, reverse, copy-paste, fun-call, indexing
+        for( int i = 1; i<=n; i++){
+            //reset curr
+            fill(curr.begin(), curr.end(), 0);
+            for(int t = 1; t<=target; t++){
+                int ans = 0;
+                for(int value=1; value<=k; value++){
+                    //we r doing this because t-value could go in negatives
+                    if(t-value >= 0)
+                        curr[t] = (curr[t] + prev[t-value]) %mod; //we use mod because the q is telling us
+                }
+                
+            }
+            //shifting
+            prev = curr;
+
         }
 
-        //2. store the ans in dp
-        dp[n][target] = ans;
-        return ans;
+        //3. return accordingly (return anything its fine, (prev = curr))
+        return prev[target];
     }
 
     int numRollsToTarget(int n, int k, int target) {
-        //1. create dp
-        vector<vector<long long int>> dp(n+1, vector<long long int>(target+1, -1));
-        return recursionMemo(n, k, target, dp);
+        return recursionSO1(n, k, target);
     }
 };
