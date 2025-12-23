d00fb49d62e2c9d5f141210f43e08cc48d2e76c2
Sairudra More <sairudra60@gmail.com>
2025-12-23 19:07:03 +0530

    Time: 151 ms (34.29%), Space: 111.3 MB (14.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,33 @@
-//M2 Memoization
+//M3 Tabulation
 //we will solve this using include/exclude method
 //we will divide the sum in half(target) and include means sub from the target until we get 0
 class Solution {
 public:
 
-    bool recursionMemo(vector<int>& nums, int index, int target, vector<vector<int>>& dp){
+    bool recursionTabu(vector<int>& nums, int index, int target){
         int n = nums.size();
-        //basecase
-        if(index >= n) return 0;
-        if(target < 0) return 0;
-        if(target == 0) return 1;
-        //3. check if its there in dp
-        if(dp[index][target] != -1) return dp[index][target];
 
-        int include = recursionMemo(nums, index+1, target - nums[index], dp);
-        int exclude = recursionMemo(nums, index+1, target, dp);
+        //1.create and update the dp with BC
+        vector<vector<int>> dp(nums.size()+1, vector<int>(target+1, 0)); //nums.size() = row, target = col, 0 cuz of the BC (if(t==0)return 1)
+        for(int i=0; i<=n; i++){ //cuz of the BC (if(target==0)return 1)
+            dp[i][0] = 1;
+        }
 
-        //2. store an update the dp
-        dp[index][target] = (include || exclude);
-        return dp[index][target];
+        //2. forloop, reversed, rec-fun, indexing
+        for(int i=n-1; i>=0; i--){ //n-1 because of the bc (if(index >=n) return 0)
+            for(int t=1; t<=target; t++){ //t=1 because of the bc (if(t==0)return 1)
+
+                int include = 0;
+                if(t - nums[i] >=0) //because teh sub can cause negative indexing which can cause problems
+                    include = dp[i+1][t - nums[i]];
+                int exclude = dp[i+1][t];
+
+                dp[i][t] = (include || exclude);
+            }
+        }
+
+        //3.return accordingly
+        return dp[0][target];
     }
 
     bool canPartition(vector<int>& nums) {
@@ -27,9 +36,7 @@ public:
         if(sum & 1) return 0; //bitwise, if 1 then odd else 0
         else{
             int target = sum >> 1; //divide it by 2
-            //1. create and pass dp
-            vector<vector<int>> dp(nums.size()+1, vector<int>(target+1, -1)); //nums.size() = row, target = col
-            return recursionMemo(nums, index, target, dp);
+            return recursionTabu(nums, index, target);
         }
     }
 };
