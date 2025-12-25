d2eefb5f7423f6624a4db9780884cc7e4b36e88c
Sairudra More <sairudra60@gmail.com>
2025-12-25 17:49:26 +0530

    Time: 47 ms (11.36%), Space: 27.5 MB (27.75%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,31 +1,38 @@
+//M2 Memoization
+//the idea is that if they match then move th eindex ahead by 1 fot both
+//else we have to move either 1 cuz there is a posibility
+    //now we have to move the index which was not moved before
+//consider this test case string A = xyzabc, B = abcxyz, there r 2 possibolities, "abc" and "xyz"
+//u will get "abc" if u move the A string index and u will get "xyz" if u move the B string index
+//we will use recursion for the upcoming comparison
+//make sure to take the max of the recursions
 class Solution {
 public:
-    // Function to find the length of the longest common subsequence in two strings.
-    int longestCommonSubsequence(string text1, string text2) {
-        int text1Length = text1.size(), text2Length = text2.size();
-        // Create a 2D array to store lengths of common subsequence at each index.
-        int dp[text1Length + 1][text2Length + 1];
-      
-        // Initialize the 2D array with zero.
-        memset(dp, 0, sizeof dp);
-      
-        // Loop through both strings and fill the dp array.
-        for (int i = 1; i <= text1Length; ++i) {
-            for (int j = 1; j <= text2Length; ++j) {
-                // If current characters match, add 1 to the length of the sequence
-                // until the previous character from both strings.
-                if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    // If current characters do not match, take the maximum length
-                    // achieved by either skipping the current character of text1 or text2.
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
+
+    int recursion(string& text1, string& text2, int index1, int index2, vector<vector<int>>& dp){
+        //3.bc check if its there in dp
+        if(index1 >= text1.size()) return 0;
+        if(index2 >= text2.size()) return 0;
+        if(dp[index1][index2] != -1) return dp[index1][index2];
+        int ans = 0;
+
+        if(text1[index1] == text2[index2]){
+            ans = 1 + recursion(text1, text2, index1+1, index2+1, dp);
         }
-      
-        // Return the value in the bottom-right cell which contains the
-        // length of the longest common subsequence for the entire strings.
-        return dp[text1Length][text2Length];
+        else{
+            int pattern1 = recursion(text1, text2, index1, index2+1, dp);
+            int pattern2 = recursion(text1, text2, index1+1, index2, dp);
+            ans = 0 + max(pattern1, pattern2);
+        }
+        //2. store the ans in dp
+        dp[index1][index2] = ans;
+        return ans;
+    }
+
+    int longestCommonSubsequence(string text1, string text2) {
+        int index1=0, index2=0;
+        //1. create dp
+        vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, -1));
+        return recursion(text1, text2, index1, index2, dp);
     }
 };
