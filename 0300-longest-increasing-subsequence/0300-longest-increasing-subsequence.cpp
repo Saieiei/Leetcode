4b0071b6d0b7f524694059fbacc0c43205f49b3e
Sairudra More <sairudra60@gmail.com>
2025-12-26 20:58:29 +0530

    Time: 163 ms (34.55%), Space: 14.6 MB (38.37%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,34 +1,35 @@
-//M2 Recursion + Memoization
-//we will solve this using inclusion exclusion logic (take teh max out of the 2)
-//we will include only when the ele > subsequence
+//M3 TabulationSO
 class Solution {
 public:
 
-    int recursionMemo(vector<int>& nums, int prev, int curr, vector<vector<int>>& dp){
-        //3. basecase + check if value is already prent in dp
-        if(curr >= nums.size()) return 0; //dont add anythign
-        if(dp[curr][prev+1] != -1) return dp[curr][prev+1]; //+1 because prev can be -1
+    int recursionTabuSO(vector<int>& nums, int curr, int prev){
+        //1. create dp and update it too
+        int n = nums.size();
+        //vector<vector<int>>dp(n+1, vector<int>(n+1, 0)); //initialisizng all to 0 cuz of the basecase (return 0 when curr>=nums.size())
+        vector<int> currRow(n+1);
+        vector<int> nextRow(n+1);
 
-        int include = 0;
-        //we will only include if prev is -1 (just starting) or when the curr>prev
-        if(prev == -1 || nums[curr] > nums[prev]){
-            //we move 1 step ahead (curr and prev) and incease the include value by 1
-            include = 1 + recursionMemo(nums, curr, curr+1, dp); 
+        //2. loop-update, reversed, copy-paste, fun-decl, index
+        for(int curr = n-1; curr>=0; curr--){
+            for(int prev = curr-1; prev>=-1; prev--){
+                int include = 0;
+                //we will only include if prev is -1 (just starting) or when the curr>prev
+                if(prev == -1 || nums[curr] > nums[prev]){
+                    //we move 1 step ahead (curr and prev) and incease the include value by 1
+                    include = 1 + nextRow[curr+1];  //in col we have to do +1 because of prev
+                }
+                int exclude = 0 + nextRow[prev+1]; //in col we have to do +1 because of prev //we dont update prev, cuz nothign was added 
+                currRow[prev+1] = max(include, exclude);
+            }
+            //shifting
+            nextRow = currRow;
         }
-        int exclude = 0 + recursionMemo(nums, prev, curr+1, dp); //we dont update prev, cuz nothign was added
-
-        //2. store the ans in dp
-        int ans = max(include, exclude);
-        dp[curr][prev+1] = ans;
-        return ans;
+        //3. return accordingly 
+        return nextRow[-1+1]; //+1 because of the prev which is -1 and it will cause probs
     }
 
     int lengthOfLIS(vector<int>& nums) {
-        int curr = 0, prev = -1; //represent the index
-        //1. create dp
-        int n = nums.size();
-        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
-
-        return recursionMemo(nums, prev, curr, dp);
+        int curr = 0, prev = -1; //represent the index (curr = row, prev = col)
+        return recursionTabuSO(nums, curr, prev);
     }
 };
