965d369aacfeade2b1f0c0bb16023a9773e788e0
Sairudra More <sairudra60@gmail.com>
2026-01-02 13:14:12 +0530

    Time: 43 ms (68.39%), Space: 13.1 MB (80.45%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,4 +1,4 @@
-//M2 Memoization
+//M3 Tabulation
 //N^N
 //we will try to solve for 1 perfect sqaure that is 1
 //then we will try to find all the possible combinations using while loop
@@ -6,24 +6,29 @@
 //with the base case we will have to return 1, because of the recursion, so in the final ans we will sub 1
 class Solution {
 public:
-    int recursionMemo(int n, vector<int>& dp){
+    int recursionTabu(int n){
+        vector<int>dp(n+1, 0); //if(n < 0) return 0
         //bc
-        if(n == 0) return 1; //this is possible
-        if(n < 0) return 0; //we cannot accept such a solution
-        if(dp[n] != -1) return dp[n];
+        //if(n == 0) return 1; //this is possible
+        dp[0] = 1;
+        //if(n < 0) return 0; //we cannot accept such a solution
+        //if(dp[n] != -1) return dp[n];
 
-        int ans = INT_MAX;
-        int end = sqrt(n);
-        for(int i = 1; i <= end; i++){
-            int squareValue = i*i;
-            int OtherSquareValue = 1 + recursionMemo(n-squareValue, dp);
-            ans = min(ans, OtherSquareValue);
+        for(int i = 1; i <= n; i++){
+            int ans = INT_MAX;
+            int end = sqrt(i); //i
+            for(int ii = 1; ii <= end; ii++){ 
+                int squareValue = ii*ii;
+                int OtherSquareValue = 1 + dp[i-squareValue]; //i
+                ans = min(ans, OtherSquareValue);
+            }
+            dp[i] = ans;
         }
-        return dp[n] = ans;
+        return dp[n];
     }
+    
     int numSquares(int n) {
-        vector<int>dp(n+1, -1);
-        int ans = recursionMemo(n, dp) -1;
+        int ans = recursionTabu(n) -1;
         return ans;
     }
 };
