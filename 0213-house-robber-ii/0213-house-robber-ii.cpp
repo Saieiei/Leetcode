ef7565b685f51c3964c464b40077fed31bf9149f
Sairudra More <sairudra60@gmail.com>
2026-01-04 21:30:48 +0530

    Time: 0 ms (100%), Space: 10.7 MB (51.98%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,19 +1,23 @@
+//M3 Tabulation
+//this same as the prev 1, just that its in circle so we have 2 possibilities to opeate, then take max of these 2
 class Solution {
 public:
 //we will solve this using recussion but we will get TLE
 
-    int solve(vector<int>& nums, int s, int e, vector<int>& dp)
+    int solveTabu(vector<int>& nums, int s, int e)
     {
-        //base case
-        if(s>e) return 0;
-        if(dp[s] != -1) return dp[s];
 
-        //calculations
-        //recurssion
-        int option1=(nums[s]+solve(nums, s+2, e, dp));
-        int option2=(0+solve(nums, s+1, e, dp));
-        int finalAns=max(option1, option2);
-        dp[s] = finalAns;
+        vector<int>dp(nums.size() + 2, 0);
+        dp[e+1]  = 0;
+        
+        for(int i = e; i >= s; i--){
+            //calculations
+            //recurssion
+            int option1=(nums[i]+dp[i+2]);
+            int option2=(0+dp[i+1]);
+            int finalAns=max(option1, option2);
+            dp[i] = finalAns;
+        }
         return dp[s];
     }
 
@@ -24,12 +28,10 @@ public:
         //lets think that there r 2 worlds
             //in 1st world robber robs the 1st house and hence wont be able to access the last house
             //hence we r ablee to set the range (starting->0 and ending point->n-2)
-            vector<int>dp(nums.size(), -1);
-            int option1=solve(nums, 0, n-2, dp);
+            int option1=solveTabu(nums, 0, n-2);
             //in the 2nd world robber robs the 2nd house and hence will be able to access the last house
             //hence we r ablee to set the range (starting->1 and ending point->n-1)
-            dp.assign(nums.size(), -1);
-            int option2=solve(nums, 1, n-1, dp);
+            int option2=solveTabu(nums, 1, n-1);
         int ans=max(option1, option2);
         return ans;
     }
