dc48aa58c0a987f0c3eb0b44d8116215b547a59b
Sairudra More <sairudra60@gmail.com>
2026-01-05 21:30:42 +0530

    Time: 106 ms (58.63%), Space: 15.8 MB (6.75%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,21 +1,24 @@
-//M2 Memoization
+//M3 Tabulation
 //but bottom up
 class Solution {
 public:
 
-    int recursionTabu(vector<int>& nums, int start, int end, vector<vector<int>>& dp){
+    int recursionTabu(vector<int>& nums, int start, int end){
         //bc
-        if(start > end) return 0;
-        if(dp[start][end] != -1) return dp[start][end];
+        vector<vector<int>>dp(nums.size(), vector<int>(nums.size(), 0)); //dont have to do +1 idk y
+        int n = nums.size();
 
-        int ans = INT_MIN;
-        for(int i = start; i <= end; i++){ //since we r coming from bottom up we will have to do nums[start-1]*nums[i]*nums[end+1] 
-        //and not nums[i - 1] * nums[i] * nums[i + 1]
-            ans = max(ans, (nums[start-1]*nums[i]*nums[end+1] + recursionTabu(nums, start, i-1, dp) + recursionTabu(nums, i+1, end, dp)));
-            dp[start][end] = ans;
+        for(int start = n-2; start >= 1; start --){ //end should be 1->n; but the base case is end>start return 0; so end = start
+            for(int end = start; end <= n-2; end++){
+               int ans = INT_MIN;
+                for(int i = start; i <= end; i++){ //since we r coming from bottom up we will have to do nums[start-1]*nums[i]*nums[end+1] 
+                //and not nums[i - 1] * nums[i] * nums[i + 1]
+                    ans = max(ans, (nums[start-1]*nums[i]*nums[end+1] + dp[start][i-1] + dp[i+1][end]));
+                    dp[start][end] = ans;
+                } 
+            }
         }
-
-        return ans;
+        return dp[1][n-2]; //not n-2
     }
     int maxCoins(vector<int>& nums) {
         //before and after insert 1 in the array
@@ -23,6 +26,6 @@ public:
         nums.insert(nums.end(), 1);
         int start = 1, end = nums.size() - 2;
         vector<vector<int>>dp(nums.size(), vector<int>(nums.size(), -1));
-        return recursionTabu(nums, start, end, dp);
+        return recursionTabu(nums, start, end);
     }
 };
