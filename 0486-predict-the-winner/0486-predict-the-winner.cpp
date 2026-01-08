5e9410af5f5d9aba87d0f67a9619311fd8276d59
Sairudra More <sairudra60@gmail.com>
2026-01-08 09:53:27 +0530

    Time: 3 ms (39.73%), Space: 10.2 MB (30.24%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,18 +1,27 @@
-//M1 Recursion
+//M3 Tabulation
 //idea is to inrease the diff more such that P1 can win
 class Solution {
 public:
 
-    int recursion(vector<int>& nums, int start, int end){
+    int recursionTabu(vector<int>& nums, int start, int end){
         //bc
-        if(start == end)return nums[start];
+        vector<vector<int>>dp(nums.size()+1, vector<int>(nums.size()+1, -1)); //not 0
+        for(int i = 0; i <= nums.size()-1; i++){ //bc
+            dp[i][i] = nums[i];
+        }
 
-        int startDiff = nums[start] - recursion(nums, start + 1, end);
-        int endDiff = nums[end] - recursion(nums, start, end - 1);
-        return max(startDiff, endDiff);
+
+        for(int start = nums.size()-2; start >= 0; start--){ //nums.size()-1 already care
+            for(int end = start + 1; end <= nums.size()-1; end++){ // we will start from start because of the diagonal
+                int startDiff = nums[start] - dp[start + 1][end];     //but since [start][start] already taken care we will start from +1
+                int endDiff = nums[end] - dp[start][end - 1];
+                dp[start][end] = max(startDiff, endDiff);
+            }
+        }
+        return dp[0][nums.size()-1];
     }
     bool predictTheWinner(vector<int>& nums) {
-        int diffMax =  recursion(nums, 0, nums.size()-1);
+        int diffMax =  recursionTabu(nums, 0, nums.size()-1);
         return diffMax >= 0;
         
     }
