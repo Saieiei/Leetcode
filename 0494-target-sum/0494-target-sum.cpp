83da1484590ce255253012d054258d4adc42c990
Sairudra More <sairudra60@gmail.com>
2026-01-12 22:18:16 +0530

    Time: 295 ms (27.68%), Space: 62.1 MB (7.96%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,23 +1,27 @@
-//M1 Recursion
+//M2 Memoization
+//since target can go negative, we cannot use vector, rather map
 //Not intuitive rather typical
 //we will keep doing operations until target becaomes 0 and we have used all the indexes
 //2 options + or -
 //in the end add all those options 
 class Solution {
 public:
-    int recursion(vector<int>& nums, int target, int indx){
+    int recursionMemo(vector<int>& nums, int target, int indx, map<pair<int, int>, int>& dp){
         //bc
         if(indx >= nums.size()){
             if(target == 0) return 1;
             else return 0;
         }
+        if(dp.find({target, indx}) != dp.end()) return dp[{target, indx}];
 
-        int plus = recursion(nums, target - nums[indx], indx+1);
-        int mins = recursion(nums, target + nums[indx], indx+1);
-        return plus + mins;
+        int plus = recursionMemo(nums, target - nums[indx], indx+1, dp);
+        int mins = recursionMemo(nums, target + nums[indx], indx+1, dp);
+        dp[{target, indx}] = plus + mins;
+        return dp[{target, indx}];
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int indx = 0;
-        return recursion(nums, target, indx);
+        map<pair<int, int>, int>dp;
+        return recursionMemo(nums, target, indx, dp);
     }
 };
