c1f1edf5320a5f41fec11b259b27fffecbf5a02f
Sairudra More <sairudra60@gmail.com>
2026-01-14 18:26:45 +0530

    Time: 35 ms (30.32%), Space: 44.5 MB (9.96%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,4 +1,4 @@
-//M2 Memoization
+//M3 Memoization
 //not intuitive
 //its not include, exclude
 //2 pointers, i->s, j->t
@@ -6,24 +6,30 @@
 //if not same move only i by 1
 class Solution {
 public:
-    int recursionMemo(string& s, string& t, int i, int j, vector<vector<int>>& dp){
-        //bc, this order is imp
-        if(j >= t.size()) return 1;
-        if(i >= s.size()) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
+    int recursionTabu(string& s, string& t, int i, int j){
+        //if(i >= s.size()) return 0;
+        vector<vector<int>>dp(s.size() + 1, vector<int>(t.size() + 1, 0));
+        //if(j >= t.size()) return 1;
+        for(int i = 0; i <= s.size(); i++){
+            dp[i][t.size()] = 1;
+        }
 
-        int ans = 0;
-        if(s[i] == t[j]){ //same
-            ans = ans + recursionMemo(s, t, i + 1, j + 1, dp);
-        }//if not same
-        ans = ans + recursionMemo(s, t, i + 1, j, dp);
+        for(int I = s.size() - 1; I >= 0; I--){
+            for(int J = t.size() - 1; J >= 0; J--){
+                long long ans = 0;
+                if(s[I] == t[J]){ //same
+                    ans = ans + dp[I + 1][J + 1];
+                }//if not same
+                ans = ans + dp[I + 1][J];
 
-        dp[i][j] = ans;
-        return ans;
+                dp[I][J] = ans;
+            }
+        }
+        return dp[0][0];
     }
     int numDistinct(string s, string t) {
         int i = 0, j = 0;
         vector<vector<int>>dp(s.size() + 1, vector<int>(t.size() + 1, -1));
-        return recursionMemo(s, t, i, j, dp);
+        return recursionTabu(s, t, i, j);
     }
 };
