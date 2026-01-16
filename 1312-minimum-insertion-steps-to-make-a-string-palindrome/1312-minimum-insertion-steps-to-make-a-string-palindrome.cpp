d1c94d7094e467f21704ee13e6ba7fa898fecfe1
Sairudra More <sairudra60@gmail.com>
2026-01-16 16:13:37 +0530

    Time: 19 ms (89.09%), Space: 9.7 MB (90.48%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,58 @@
+//M3 Tabulation direct 
+//to solve this we have to know LCS -> LPS
+//after we get the length of LPS, we will sub it with the length of the string
+//the ans u get is r just the chars u need to add in the string
+//in LCS view, string1 is s string2 is reversed(s)
+//same code as LCS
+//https://leetcode.com/problems/longest-common-subsequence/submissions/1865161144/
+class Solution {
+public:
+
+    int recursionTabuSO(string& text1, string& text2, int index1, int index2){
+
+        //1. create dp
+        vector<int> curr(text1.length()+100);
+        vector<int> next(text1.length()+100);
+        //vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, 0)); //bc
+
+        //2.for-loop, reversed, copy-paste, rec-fun, index
+        for(int i=text1.size()-1; i>=0; i--){ //bc
+            for(int j = text2.size()-1; j>=0; j--){ //bc
+
+                int ans =0;
+                if(text1[i] == text2[j]){
+                    ans = 1 + next[j+1];
+                }
+                else{
+                    int pattern1 = curr[j+1];
+                    int pattern2 = next[j];
+                    ans = 0 + max(pattern1, pattern2);
+                }
+                //2. store the ans in dp
+                curr[j] = ans;
+            }
+            //shifting
+            next = curr; // its is goign upwards, reverded for loop
+        }
+
+        //3. return accordingly
+        return next[0];
+    }
+    int longestPalindromeSubseq(string s) {
+        string text1 = s;
+        string text2 = s;
+        reverse(text2.begin(), text2.end());
+
+        //copy-paste LCS
+        int index1=0, index2=0;
+        //1. create dp
+        //vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, -1));
+        return recursionTabuSO(text1, text2, index1, index2);
+
+        
+    }
+        int minInsertions(string s) {
+        int LPS_length = longestPalindromeSubseq(s);
+        return s.length() - LPS_length;
+    }
+};
