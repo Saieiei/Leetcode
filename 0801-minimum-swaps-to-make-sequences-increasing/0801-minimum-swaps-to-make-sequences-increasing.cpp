113873c4fb7422ce6b7b2ff7948f3ac7a108e850
Sairudra More <sairudra60@gmail.com>
2026-01-19 18:23:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,4 +1,6 @@
-//M2 Memoization
+//M3 Tabulation
+//we will have to go from size-1 to 1 as will insert -1 for p1 and p2 in the vectors
+//and also we will have to swap if isSwap
 //the dp is a little diff and we pass isSwap in recursion too now
 //This is sim to include exclude principle
 //we will have to return the min of it so inni to INT_MAX
@@ -7,25 +9,32 @@
 //swpa if th ecodition works
 class Solution {
 public:
-    int recursionMemo(vector<int>& nums1, vector<int>& nums2, int indx, int p1, int p2, vector<vector<int>>&dp, int isSwap){
+    int recursionTabu(vector<int>& nums1, vector<int>& nums2, int indx, int p1, int p2, int isSwap){
         //bc
+        vector<vector<int>>dp(nums1.size() + 1, vector<int>(2, 0));
         if(indx >= nums1.size()) return 0;
-        if(dp[indx][isSwap] != -1) return dp[indx][isSwap];
-        int swap = INT_MAX, NoSwap = INT_MAX;
-        
-        if(p1 < nums2[indx] && p2 < nums1[indx]){ //here swap p1 and p2
-            swap =  1 + recursionMemo(nums1, nums2, indx + 1, nums2[indx], nums1[indx], dp, 1);
+        //if(dp[indx][isSwap] != -1) return dp[indx][isSwap];
+
+        for(int I = nums1.size() - 1; I >= 1; I--){
+            for(int j = 1; j >= 0; j--){
+                if(j) swap(p1, p2); //idk y 
+                p1 = nums1[I - 1]; p2 = nums2[I - 1];
+                int swap = INT_MAX, NoSwap = INT_MAX;
+                if(p1 < nums2[I] && p2 < nums1[I]){ //here swap p1 and p2
+                    swap =  1 + dp[I + 1][1];
+                }
+                if(p1 < nums1[I] && p2 < nums2[I]){ //dont swap p1 and p2, but should be inc
+                    NoSwap = 0 + dp[I + 1][0];
+                }
+                dp[I][isSwap] =  min(swap, NoSwap);
+            }
         }
-        if(p1 < nums1[indx] && p2 < nums2[indx]){ //dont swap p1 and p2, but should be inc
-            NoSwap = 0 + recursionMemo(nums1, nums2, indx + 1, nums1[indx], nums2[indx], dp, 0);
-        }
-        dp[indx][isSwap] =  min(swap, NoSwap);
-        return dp[indx][isSwap];
+        return dp[1][0];
     }
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
         int indx = 0, p1 = -1, p2 = -1, isSwap = 0; // here p1  and p2 r not index
         //we pass -1 just beasue of the 1st if condition
-        vector<vector<int>>dp(nums1.size() + 1, vector<int>(2, -1)); //indx, swap/NoSwap
-        return recursionMemo(nums1, nums2, indx, p1, p2, dp, isSwap);
+        nums1.insert(nums1.begin(), -1); nums2.insert(nums2.begin(), -1);
+        return recursionTabu(nums1, nums2, indx, p1, p2, isSwap);
     }
 };
