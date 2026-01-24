22df06f4b5193242b292f801c41560ca9333e35f
Sairudra More <sairudra60@gmail.com>
2026-01-24 09:34:07 +0530

    Time: 125 ms (5.03%), Space: 101.2 MB (5.11%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,34 +1,35 @@
-//M1 recursion
+//M2 Memoization
 //this is same as https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/description/
-//just that when ur selling, u should sub the profit with trasaction fee
+//just that when ur selling, u should sub the profit with trasaction fee when ur selling it (1 complete transaction)
 class Solution {
 public:
-    int recursion(vector<int>& prices, int index, bool buy, int &fee){
+    int recursionMemo(vector<int>& prices, int index, bool buy, vector<vector<int>>& dp, int& fee){
         //bc
         if(index >= prices.size()) return 0;
+        if(dp[index][buy] != -1) return dp[index][buy];
         int maxProfit = INT_MIN;
         
 
         //we r going to buy
         if(buy){
-            int buyProfit = -prices[index] + recursion(prices, index+1, 0, fee); //u cant buy another1 when u bought 1 already
-            int holdingBuyProfit = 0 + recursion(prices, index+1, 1, fee); //u can buy 1 when u have nothing to hold
+            int buyProfit = -prices[index] + recursionMemo(prices, index+1, 0, dp, fee); //u cant buy another1 when u bought 1 already
+            int holdingBuyProfit = 0 + recursionMemo(prices, index+1, 1, dp, fee); //u can buy 1 when u have nothing to hold
             maxProfit = max(buyProfit, holdingBuyProfit);
         }
         else //we r going to sell
         {
-            //after complete trans apply trans fee
-            int SellProfit = prices[index] + recursion(prices, index+1, 1, fee) - fee; //u can buy when ur trying to sell,
-            int holdingSellProfit = 0 + recursion(prices, index+1, 0, fee); //u cant buy 1 when ur still trying to hold the share instead of selling it
+            int SellProfit = prices[index] + recursionMemo(prices, index+1, 1, dp, fee) - fee; //u can buy when ur trying to sell
+            int holdingSellProfit = 0 + recursionMemo(prices, index+1, 0, dp, fee); //u cant buy 1 when ur still trying to hold the share instead of selling it
             maxProfit = max(SellProfit, holdingSellProfit);
         }
 
+        dp[index][buy] = maxProfit;
         return maxProfit;
     }
 
     int maxProfit(vector<int>& prices, int fee) {
-        int index = 0, buy = 1;
-        return recursion(prices, index, buy, fee);
-        
+        int index = 0;
+        vector<vector<int>>dp(prices.size()+1, vector<int>(2, -1));
+        return recursionMemo(prices, index, true, dp, fee);
     }
 };
