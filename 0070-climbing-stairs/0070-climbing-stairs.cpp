98131a8b82b4d60f0f150adb0222a75f72432c1a
Sairudra More <sairudra60@gmail.com>
2026-01-26 18:41:46 +0530

    Time: 1 ms (5.61%), Space: 8.5 MB (34.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,16 +1,23 @@
+//M2 Memoization
+//we have 2 options
+//return the sum of the options
 class Solution {
 public:
-
+    int recursionMemo(int n, vector<int>& dp){
+        //bc
+        if(n == 0) return 1; //do nothing
+        if(dp[n] != -1) return dp[n];
+        int ways = 0;
+        //op1
+        ways = recursionMemo(n-1, dp);
+        //op2
+        if (n >= 2) ways += recursionMemo(n - 2, dp);
+        //return sum of options op1 + op2 (we already do it in ways)
+        dp[n] = ways; 
+        return ways;
+    }
     int climbStairs(int n) {
-        
-        int a =0;
-        int b=1;
-        int c=0;
-        for(int i=0;i<n;i++){
-            c = a+b;
-            a=b;
-            b=c;
-        }
-        return b;
+        vector<int>dp(n + 1, -1);
+        return recursionMemo(n, dp);
     }
 };
