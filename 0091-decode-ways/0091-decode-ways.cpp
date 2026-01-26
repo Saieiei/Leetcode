976697fbddf5b1c706e582efc29a53f18e5b7c92
Sairudra More <sairudra60@gmail.com>
2026-01-26 16:34:15 +0530

    Time: 0 ms (100%), Space: 8.7 MB (67.22%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,30 +1,38 @@
-//M2 Memoization
+//M3 Tabulation
 //either consume 1 char or 2 chars
 //even an empty string is considered (i==n)
 class Solution {
 public:
-    int recursion(string& s, int indx, vector<int>& dp){
+    int recursion(string& s, int indx){
+        vector<int>dp(s.size() + 1, 0);
         //bc
-        if(indx == s.size()) return 1; //empty
-        if(s[indx] == '0') return 0; //single
-        if(dp[indx] != -1) return dp[indx];
+        //if(indx == s.size()) return 1; //empty
+        dp[s.size()] = 1;
+        //if(s[indx] == '0') return 0; //single
+        //if(dp[indx] != -1) return dp[indx];
 
-        int ways = 0;
-        //op1
-        ways = recursion(s, indx + 1, dp); //not 1 + r
-        //op2
-        if(indx + 1 < s.size()){
-            int decoded  = (s[indx] - '0')*10 + (s[indx + 1] - '0');
-            if(decoded >= 10 && decoded <= 26 ) 
-                ways += recursion(s, indx + 2, dp);  //not 1 + r
+        for(int i = s.size() - 1; i >= 0; i--){
+            if(s[i] == '0'){ //bc
+                dp[i] = 0;
+                continue;
+            }
+            
+            int ways = 0;
+            //op1
+            ways = dp[i + 1]; //not 1 + r
+            //op2
+            if(i + 1 < s.size()){
+                int decoded  = (s[i] - '0')*10 + (s[i + 1] - '0');
+                if(decoded >= 10 && decoded <= 26 ) 
+                    ways += dp[i + 2];  //not 1 + r
+            }
+            dp[i] = ways;
         }
-        dp[indx] = ways;
-        return ways;
+        return dp[0];
     }
     int numDecodings(string s) {
         int indx = 0;
-        vector<int>dp(s.size() + 1, -1);
-        return recursion(s, indx, dp);
+        return recursion(s, indx);
         
     }
 };
