5a91ffa80d78b0ad52026d63138cb23428f25388
Sairudra More <sairudra60@gmail.com>
2026-02-16 17:30:06 +0530

    Time: 0 ms (100%), Space: 35.3 MB (86.02%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,24 +1,34 @@
-//simple dfs
+//simple bfs
 class Solution {
 public:
-    bool dfs(vector<int>& arr, int start, vector<bool>& visited){
-        //basecase
-        if(start >= arr.size() || start <= -1 || visited[start]){
-            return false;
+    bool bfs(vector<int>& arr, int start, vector<bool>& visited, queue<int>& q){
+        //keep doing until q is empty
+        while(!q.empty()){
+            //mark it as visited
+            int frontEle = q.front();
+            q.pop();
+            visited[frontEle] = true;
+            //check if 0
+            if(arr[frontEle] == 0){
+                return true;
+            }
+            //push the rest
+            int leftBFS = frontEle-arr[frontEle];
+            int rightBFS = frontEle+arr[frontEle];
+            if(leftBFS>=0 && !visited[leftBFS]){
+                q.push(leftBFS);
+            }
+            if(rightBFS<arr.size() && !visited[rightBFS]){
+                q.push(rightBFS);
+            }
         }
-        // Target found
-        if (arr[start] == 0) {
-            return true;
-        }
-        //mark it as visited
-        visited[start] = true;
-        //2 options
-        int leftDFS = dfs(arr, start-arr[start], visited);
-        int rightDFS = dfs(arr, start+arr[start], visited);
-        return (leftDFS || rightDFS);
+        //not found
+        return false;
     }
     bool canReach(vector<int>& arr, int start) {
         vector<bool> visited(arr.size(), false);
-        return dfs(arr, start, visited);
+        queue<int> q;
+        q.push(start);
+        return bfs(arr, start, visited, q);
     }
 };
