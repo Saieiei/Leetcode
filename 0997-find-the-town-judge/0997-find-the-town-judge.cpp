6aac4a277db77bea512c38646fb322b3c1b191d8
Sairudra More <sairudra60@gmail.com>
2026-03-06 17:02:15 +0530

    Time: 43 ms (13.66%), Space: 73.4 MB (15.82%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,25 +1,30 @@
-// the left element trusts some1
-// the right element is being trusted by ome1
-// so in the array left element can never be the Town Judge
-// the right elemtn is consider to the town judge only if all the elements are trusting the right elemt
-// https://www.youtube.com/watch?v=ZUP_tIs4VaE
+//the idea is that we will use incoming and outgoing edges
+//the node is considered as judge if it n-1 incoming edeges and 0 outgoing edges
+//so calc the incoming and outgoing edges for each node (travese through trust)
+//traverse through a loop and check for th ejudge condition
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        //int n=trust.size();
-        vector<pair<int,int>> arr(n+1, {0,0}); //we will be using pairs of array whose size is n+1 and all r innitialized to [0,0]
-        for(int i=0;i<trust.size();i++)
-        {
-            arr[trust[i][0]].first+=1; //trusting some1
-            arr[trust[i][1]].second+=1; //being trusted by some1
-        }
-        for(int i=1;i<=n;i++) //u dont have to start with 0 as it is always (0,0)
-        {
-            if(arr[i].first==0 && arr[i].second==n-1) //n-1 because teh town hall cannot trust himself
-                return i;
-        }
-        return -1;
+        //initialization, u dont have to do all this if ur suing unordered_map<int, int>
+        vector<int>incomingEdge(n+1, 0);
+        vector<int>outgoingEdge(n+1, 0);
 
-        
+        //find out incoming and outgoing edges of each node
+        for(vector<int> directions: trust){
+            int u = directions[0];
+            int v = directions[1];
+            incomingEdge[v]++;
+            outgoingEdge[u]++;
+        }
+
+        //traverese through each node and find out the judge
+        for(int i=1; i<=n; i++){
+            if(incomingEdge[i] == n-1 && outgoingEdge[i] == 0){
+                return i;
+            }
+        }
+
+        //else no judge
+        return -1;
     }
 };
