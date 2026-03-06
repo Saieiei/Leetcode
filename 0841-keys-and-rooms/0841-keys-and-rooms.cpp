6a86fdfd4288eb443f85fb23e0a0a95573ce5b3d
Sairudra More <sairudra60@gmail.com>
2026-03-06 19:38:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -6,7 +6,15 @@ class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         //in this case we dont have to create AdjList
-        //we will consider the rooms as AdjList itself
+        //but if u want to create a adjList
+        unordered_map<int, vector<int>> adjList;
+        int n = rooms.size();
+        for(int i=0; i<n; i++){
+            for(int j = 0; j<rooms[i].size(); j++){
+                adjList[i].push_back(rooms[i][j]);
+            }
+        }
+
         unordered_map<int, bool> isVisited;
         int countIsVisisted = 0;
         queue<int> q;
@@ -21,7 +29,7 @@ public:
             int frontNode = q.front();
             q.pop();
             //process its nbrs
-            for(int nbr: rooms[frontNode]){
+            for(int nbr: adjList[frontNode]){
                 if(!isVisited[nbr]){
                     q.push(nbr);
                     isVisited[nbr] = true;
