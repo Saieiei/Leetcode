7b07f565f1a2ddfa32f184be31eed74e7fd590f9
Sairudra More <sairudra60@gmail.com>
2026-03-14 14:14:16 +0530

    Time: 0 ms (100%), Space: 65 MB (81.71%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,30 +1,20 @@
-//the idea is that we will use incoming and outgoing edges
-//the node is considered as judge if it n-1 incoming edeges and 0 outgoing edges
-//so calc the incoming and outgoing edges for each node (travese through trust)
-//traverse through a loop and check for th ejudge condition
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        //initialization, u dont have to do all this if ur suing unordered_map<int, int>
-        vector<int>incomingEdge(n+1, 0);
-        vector<int>outgoingEdge(n+1, 0);
-
-        //find out incoming and outgoing edges of each node
-        for(vector<int>& directions: trust){
-            int u = directions[0];
-            int v = directions[1];
-            incomingEdge[v]++;
-            outgoingEdge[u]++;
+        if (n == 1) return 1; // If there's only one person, they're automatically the judge.
+        
+        vector<int> trustCounts(n + 1, 0);
+        
+        for (const auto& t : trust) {
+            trustCounts[t[0]]--; // Decrease for trusting others
+            trustCounts[t[1]]++; // Increase for being trusted
         }
-
-        //traverese through each node and find out the judge
-        for(int i=1; i<=n; i++){
-            if(incomingEdge[i] == n-1 && outgoingEdge[i] == 0){
+        
+        for (int i = 1; i <= n; ++i) {
+            if (trustCounts[i] == n - 1) {
                 return i;
             }
         }
-
-        //else no judge
-        return -1;
+        return -1; // No judge found
     }
 };
