49e1249d2d61472e57353881e54a20474d1e547b
Sairudra More <sairudra60@gmail.com>
2026-04-06 17:40:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0525-contiguous-array/0525-contiguous-array.cpp
+++ 0525-contiguous-array/0525-contiguous-array.cpp
@@ -1,29 +1,30 @@
-//brute force
+//optimised
+//prefix-sum, hashmaps
 class Solution {
 public:
     int findMaxLength(vector<int>& nums) {
         int n = nums.size();
-        int maxLengthSubarrya = INT_MIN;
-        int start = 0;
-        int end = 0;
+        int maxLength = 0;
+        unordered_map<int, int> mp;
         int sum = 0;
         for(int i = 0; i < n; i++){
-            sum = 0; //reset
-            start = nums[i];
-            for(int j = i; j < n; j++){
-                end = nums[j];
-                if(end == 0){
-                    sum += -1;
-                }
-                else{
-                    sum += 1;
-                }
-                if(sum == 0){
-                    int tempLenght = j - i + 1;
-                    maxLengthSubarrya = max(maxLengthSubarrya, tempLenght);
-                }
+            if(nums[i] == 0){
+                sum += -1;
+            }
+            else{
+                sum += 1;
+            }
+            if(sum == 0){
+                maxLength = i + 1;
+            }
+            else if(mp.find(sum) != mp.end()){ //found it
+                int tempLength = i - mp[sum];
+                maxLength = max(maxLength, tempLength);
+            }
+            else{ //not there in hash map, save the index
+                mp[sum] = i;
             }
         }
-        return maxLengthSubarrya;
+        return maxLength;
     }
 };
