298efa51ceae233218951dcfb1d9f65a9a91ff87
Sairudra More <sairudra60@gmail.com>
2026-04-09 00:25:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,70 +1,54 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <unordered_map>
-
-using namespace std;
-
+//normal dijstars algo
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        //build adjacency list
-        vector<vector<pair<int, int>>> adjList(n + 1);
-        for(auto i: times) {
-            //i -> vector<int>
-            //i[0] -> u
-            //i[1] -> v
-            //i[2] -> time
-            int u = i[0];
-            int v = i[1];
-            int wt = i[2];
-            adjList[u].push_back(make_pair(v, wt));
+       //adjList
+       //the size is n+1 cuz the numbering is from 1-n
+       vector<vector<pair<int, int>>> adjList(n + 1); //u -> w, v
+       for(const vector<int> time: times){
+            int u = time[0];
+            int v = time[1];
+            int w = time[2];
+            //directed
+            adjList[u].push_back({w, v});
         }
 
-        using P = pair<int, int>;
-        priority_queue<P, vector<P>, greater<P> > pq;
-        
-        //initial state -> {timesofar, node}
-        pq.push({0, k});
-        
-        int visitedNodeCount = 0;
-        unordered_map<int, bool> visited;
-        int timeAns = 0;
-        
-        //main logic
-        while(!pq.empty()) {
+       //so 0 will always be INT_MAX
+       vector<int> dist(n+1, INT_MAX);
+
+       //minheap-priority_queue //wt, node
+       priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+       dist[k] = 0;
+       pq.push({0, k});
+
+       //start processing
+       while(!pq.empty()){
             pair<int, int> topPair = pq.top();
             pq.pop();
-            
-            int currTime = topPair.first;
-            int currNode = topPair.second;
-            
-            //bhot important h
-            if(visited[currNode] == true) {
-                //ignore
-                continue;
-            }
-            
-            //if not visited
-            visited[currNode] = true;
-            visitedNodeCount++;
-            timeAns = max(timeAns, currTime);
-            
-            //process nbrs
-            for(pair<int, int> nbr: adjList[currNode]) {
-                //nbr.first -> int -> node
-                //nbr.second -> int -> time/weight
-                if(visited[nbr.first] == false) {
-                    pq.push({currTime + nbr.second, nbr.first});
+            int nodeWt = topPair.first;
+            int node = topPair.second;
+            //process its nbrs
+            for(const pair<int, int> nbr: adjList[node]){
+                int nbrWt = nbr.first;
+                int nbrNode = nbr.second;
+                if(nodeWt + nbrWt < dist[nbrNode]){
+                    //update the dist[nbrNode] and push in the pq
+                    dist[nbrNode] = nodeWt + nbrWt;
+                    pq.push({dist[nbrNode], nbrNode});
                 }
             }
         }
-        
-        // If we visited all nodes, return the timeAns. Otherwise, return -1.
-        if (visitedNodeCount == n) {
-            return timeAns;
-        } else {
-            return -1;
+
+        //return the max time from dist[] and ignore 0 index as it is always INT_MAX
+        int ansTime = 0;
+        for(int i=1; i<=n; i++){
+            //if any dist[node] from 1 - n is INT_MAX, then dijstars algo didnt work, so not possible
+            if(dist[i] == INT_MAX){
+                return -1;
+            }
+            ansTime = max(ansTime, dist[i]);
         }
+        return ansTime;
     }
 };
