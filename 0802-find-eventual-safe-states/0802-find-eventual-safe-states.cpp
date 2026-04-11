7d0c43762cea7b63e3eaf18aff220fbb83b20f9b
Sairudra More <sairudra60@gmail.com>
2026-04-11 15:01:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,64 +1,47 @@
-//DFS
+//BFS
+//reverse topologocal sorting
+//any nodes whos indegree is 0 is a safe node
+//finaly sort it
 class Solution {
 public:
-
-    bool dfs(int node, vector<vector<int>>& adjList, vector<bool>& isVisited, vector<bool>& isPathVisited, vector<bool>& isSafe){
-        //mark it as vivisted
-        isVisited[node] = true;
-        isPathVisited[node] = true;
-        //initially we will mark it as unsafe
-        isSafe[node] = false;
-
-        //explore its nbrs
-        for(const int nbrNode: adjList[node]){
-            if(!isVisited[nbrNode]){
-                if(dfs(nbrNode, adjList, isVisited, isPathVisited, isSafe)){
-                    //cycle found
-                    return true;
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int n = graph.size();
+        vector<int> indegree(n);
+        //reversed adjList
+        vector<vector<int>> adjList(n);
+        for(int node = 0; node<n; node++){
+            for(const int nbr: graph[node]){
+                //nbr->node
+                adjList[nbr].push_back(node);
+                indegree[node]++;
+            }
+        }
+        queue<int> q;
+        for(int node=0; node<n; node++){
+            if(indegree[node] == 0){
+                q.push(node);
+            }
+        }
+        vector<int> safeNodes;
+        while(!q.empty()){
+            int frontNode = q.front();
+            q.pop();
+            safeNodes.push_back(frontNode);
+            //explore nbrs
+            for(const int nbrNode: adjList[frontNode]){
+                indegree[nbrNode]--;
+                if(indegree[nbrNode] == 0){
+                    q.push(nbrNode);
                 }
             }
-            //cycle, cuz its in the path
-            else if(isPathVisited[nbrNode]){
-                return true;
-            }
+
         }
-        //else
-        isPathVisited[node] = false;
-        isSafe[node] = true;
-        return false;
-    }
+        //sort it
+        sort(safeNodes.begin(), safeNodes.end());
+        return safeNodes;
 
-    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        //isVisited, isPathVisited, check
-        int n = graph.size();
-        //vector<vector<int>> adjList(n);
-        //for(vector<int> connection: graph){
-        //    int u = connection[0];
-        //    int v = connection[1];
-        //    //directed
-        //    adjList[u].push_back(v);
-        //}
+        
 
-        vector<bool> isVisited(n, false);
-        vector<bool> isPathVisited(n, false);
-        vector<bool> isSafe(n, 0); 
 
-        //dfs
-        for(int i=0; i<n; i++){
-            //dont worry about the return value from dfs
-            int node = i;
-            dfs(node, graph, isVisited, isPathVisited, isSafe);
-        }
-
-        //process the ans vector in ascending order
-        vector<int> safeNodeAns;
-        for(int i=0; i<n; i++){
-            int node = i;
-            if(isSafe[node] == true){
-                safeNodeAns.push_back(node);
-            }
-        }
-
-        return safeNodeAns;
     }
 };
