fdbaf7bb9d43356ea9b4afeb18ce4458c8bcedc9
Sairudra More <sairudra60@gmail.com>
2026-04-20 18:12:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,48 +1,55 @@
-//BFS
-//so we have to follow a specific order -> topological sorting-BFS
-//to detect cycles, we will use TS and return based on number of nodes
-//prerequisites 1st, and then the rest
+//DFS
+//thinking it as a cycle detection
+//if there is a cycle then we can never complete the courses
 class Solution {
 public:
+
+    bool dfs(int node, vector<vector<int>>& adjList, vector<bool>& isVisited, vector<bool>& isPathVisited){
+        //visisted
+        isVisited[node] = true;
+        isPathVisited[node] = true;
+        //explore the nbrs
+        for(const int& nbr: adjList[node]){
+            if(!isVisited[nbr]){
+                if(dfs(nbr, adjList, isVisited, isPathVisited)){
+                    //cycle detected
+                    return true;
+                }
+            }
+            else{
+                if(isPathVisited[nbr]){
+                    //cycle detected
+                    return true;
+                }
+            }
+        }
+        isPathVisited[node] = false;
+        return false;
+    }
+
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        vector<int> indegrees(numCourses, 0);
+        
         //adjList
         vector<vector<int>> adjList(numCourses);
         for(const vector<int>& prerequisit: prerequisites){
             int u = prerequisit[1];
             int v = prerequisit[0];
-            //directef
+            //direcrted
             //u->v
             adjList[u].push_back(v);
-            indegrees[v]++;
         }
 
-        //no need to worry if disconnected components
-        //indegrees will take care of it
-        //while using indegree, we do not need isVisited[]
-        queue<int> q;
-        //topologocal sorting
-        //1st find the indegrees with 0 and push it in the q
-        for(int node=0; node<numCourses; node++){
-            if(indegrees[node] == 0){
-                q.push(node);
-            }
-        }
-
-        int countNodes = 0;
-        //process bfs
-        while(!q.empty()){
-            int frontNode = q.front();
-            q.pop();
-            countNodes++;
-            //explore the nbrs
-            for(const int& nbrNode: adjList[frontNode]){
-                indegrees[nbrNode]--;
-                if(indegrees[nbrNode] == 0){
-                    q.push(nbrNode);
+        vector<bool> isVisited(numCourses, false);
+        vector<bool> isPathVisited(numCourses, false);
+        //disconnected graph
+        for(int node = 0; node<numCourses; node++){
+            if(!isVisited[node]){
+                if(dfs(node, adjList, isVisited, isPathVisited)){
+                    //there is a cycle in the graph
+                    return false;
                 }
             }
         }
-        return (countNodes == numCourses);
+        return true; //no cycle
     }
 };
