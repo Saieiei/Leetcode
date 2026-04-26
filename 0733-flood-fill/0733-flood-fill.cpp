400c65cb014d02833e965ea5cf9f67e79b26b06f
Sairudra More <sairudra60@gmail.com>
2026-04-26 08:03:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,10 +1,7 @@
 //dfs
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, const int& color, const int& originalColor){
-        int m = image.size();
-        int n = image[0].size();
-
+    void dfs(vector<vector<int>>& image, int sr, int sc, const int color, const int m, const int n, const int originalColor){
         //1st check if the coordinates r safe
         if((sr<0 || sr>=m)||(sc<0 || sc>=n)||(image[sr][sc] != originalColor)){
             return;
@@ -14,18 +11,21 @@ public:
         image[sr][sc] = color;
 
         //spread the infection in 4 directions
-        dfs(image, sr-1, sc, color, originalColor);
-        dfs(image, sr+1, sc, color, originalColor);
-        dfs(image, sr, sc-1, color, originalColor);
-        dfs(image, sr, sc+1, color, originalColor);
+        dfs(image, sr-1, sc, color, m, n, originalColor);
+        dfs(image, sr+1, sc, color, m, n, originalColor);
+        dfs(image, sr, sc-1, color, m, n, originalColor);
+        dfs(image, sr, sc+1, color, m, n, originalColor);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size();
+        int n = image[0].size();
+    
         //you only want to spread the color to adjacent pixels that 
         //match the original color of the starting pixel.
         int originalColor = image[sr][sc];
         //check if the coordinates color is not the given color
         if(color != image[sr][sc]){
-            dfs(image, sr, sc, color, originalColor);
+            dfs(image, sr, sc, color, m, n, originalColor);
         }
         return image;
     }
