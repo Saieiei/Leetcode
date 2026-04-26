de931b2168b97b5ea4dda1c87a83038c822ddb53
Sairudra More <sairudra60@gmail.com>
2026-04-26 10:20:40 +0530

    Time: 3 ms (13.32%), Space: 18.1 MB (55.98%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,39 +1,48 @@
-//dfs
-// This block disables I/O synchronization to speed up execution time on LeetCode
-auto init = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    return 'c';
-}();
+//bfs
 class Solution {
 public:
-    void dfs(vector<vector<int>>& image, int sr, int sc, const int color, const int m, const int n, const int originalColor){
-        //1st check if the coordinates r safe
-        if((sr<0 || sr>=m)||(sc<0 || sc>=n)||(image[sr][sc] != originalColor)){
-            return;
+    bool isSafe(const int newX, const int newY, const int m, const int n, vector<vector<int>>& image, const int color, const int originalColor){
+        if((newX<0 || newX>=m)||(newY<0 || newY>=n)||(image[newX][newY] == color)
+        ||((image[newX][newY] != originalColor))){
+            return false;
         }
-
-        //its safe to color it
-        image[sr][sc] = color;
-
-        //spread the infection in 4 directions
-        dfs(image, sr-1, sc, color, m, n, originalColor);
-        dfs(image, sr+1, sc, color, m, n, originalColor);
-        dfs(image, sr, sc-1, color, m, n, originalColor);
-        dfs(image, sr, sc+1, color, m, n, originalColor);
+        return true;
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        //create isVisited
         int m = image.size();
         int n = image[0].size();
-    
-        //you only want to spread the color to adjacent pixels that 
-        //match the original color of the starting pixel.
-        int originalColor = image[sr][sc];
-        //check if the coordinates color is not the given color
-        if(color != image[sr][sc]){
-            dfs(image, sr, sc, color, m, n, originalColor);
+        //we will not create a isVisited as we will directly change the image
+        //in place optimization
+        //create a q
+        queue<pair<int, int>> q;
+        //1st mark it as visited
+        int originalColor; 
+        if(image[sr][sc] != color){
+            q.push({sr, sc});
+            originalColor = image[sr][sc];
+            image[sr][sc] = color;
+        }
+        //to explore its nbrs
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1};
+        while(!q.empty()){
+            pair<int, int> frontPair = q.front();
+            q.pop();
+            int x = frontPair.first;
+            int y = frontPair.second;
+            //explore its nbrs
+            for(int k=0; k<4; k++){
+                int newX = x + dx[k];
+                int newY = y + dy[k];
+                if(isSafe(newX, newY, m, n, image, color, originalColor)){
+                    //infection successful
+                    image[newX][newY] = color;
+                    q.push({newX, newY});
+                }
+            }
         }
         return image;
+
     }
 };
