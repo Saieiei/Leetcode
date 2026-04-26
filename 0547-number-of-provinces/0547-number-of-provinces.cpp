2c4a77e34026ba356cadc601382bad7b017b94f6
Sairudra More <sairudra60@gmail.com>
2026-04-26 17:27:15 +0530

    Time: 0 ms (100%), Space: 19.9 MB (47.91%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,43 +1,35 @@
-//we will work with adjList here
-//u can traverse through the matrix it self,
-//and not use adjList
-//the rest is simple DFS
 class Solution {
 public:
-    void dfs(int i, vector<bool>& isVisited, vector<vector<int>>& isConnected, vector<vector<int>>& adjList){
-        //1st mark it as visited
-        isVisited[i] = true;
-        //explore its nbrs
-        for(int nbr: adjList[i]){
-            if(!isVisited[nbr]){
-                dfs(nbr, isVisited, isConnected, adjList);
-            }
-        }
-    }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<vector<int>> adjList(n);
-        //traverse through the data
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                //get the the node
-                //not using nodeID
-                if(i != j && isConnected[i][j] == 1){
-                    adjList[i].push_back(j);
+        int countAns = 0;
+        vector<bool> isVisited(n, false);
+        
+        for (int i = 0; i < n; i++) {
+            // If the node hasn't been visited, it's a new component
+            if (!isVisited[i]) {
+                countAns++;
+                
+                // Initialize BFS for this component
+                queue<int> q;
+                q.push(i);
+                isVisited[i] = true;
+                
+                while (!q.empty()) {
+                    int curr = q.front();
+                    q.pop();
+                    
+                    // Explore neighbors directly through the matrix
+                    for (int j = 0; j < n; j++) {
+                        if (isConnected[curr][j] == 1 && !isVisited[j]) {
+                            isVisited[j] = true;
+                            q.push(j);
+                        }
+                    }
                 }
             }
         }
-
-        //dfs
-        int countAns = 0;
-        vector<bool> isVisited(n, false);
-        for(int i=0; i<n; i++){
-            if(!isVisited[i]){
-                dfs(i, isVisited, isConnected, adjList);
-                countAns++;
-            }
-        }
+        
         return countAns;
     }
 };
