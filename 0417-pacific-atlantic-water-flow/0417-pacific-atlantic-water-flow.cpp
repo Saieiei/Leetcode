a6aaecce981cebd14c0e9b94b7f215117db09535
Sairudra More <sairudra60@gmail.com>
2026-04-28 18:48:38 +0530

    Time: 7 ms (62.59%), Space: 22.1 MB (95.69%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,54 +1,80 @@
-//we will solve this q in other way
-//we will figure out which cells from the ocean can go deep into the map
-//so we will have to check if the cell height <= nbr cell to affect it
-//we will keep 2 visited vectors for each ocean
-//then take the intersection of them and return that ans
+//dfs
 class Solution {
 public:
-    void dfs(vector<vector<int>>& heights, vector<vector<bool>>& oceanVisted, int x, int y, int totalRows, int totalCols){
-        //mark it as visited
-        oceanVisted[x][y] = true;
-        //affect its nbrs
-        vector<int> dx = {1, -1, 0, 0};
-        vector<int> dy = {0, 0, -1, 1};
-        for(int i=0; i<4; i++){
-            int newX = dx[i] + x;
-            int newY = dy[i] + y;
-            if(newX>=0 && newX<totalRows && newY>=0 && newY<totalCols 
-                && !oceanVisted[newX][newY] && heights[x][y] <= heights[newX][newY]){
-                dfs(heights, oceanVisted, newX, newY, totalRows, totalCols);
-            }
+    void dfs(int x, int y, const int m, const int n, vector<vector<int>>& heights, vector<vector<bool>>& isVisitedOcean, int parentHeight){
+        //1st check if the coordinates r not safe
+        if((x<0 || x>=m)||(y<0 || y>=n)){
+            return;
         }
+        //check if already visited
+        if(isVisitedOcean[x][y] == true){
+            return;
+        }
+        //check if water cannot flow
+        if(parentHeight > heights[x][y]){
+            return;
+        }
+        
+        //its safe now, mark it as visited
+        isVisitedOcean[x][y] = true;
+        
+        //explore its nbrs
+        //up, down, left, right
+        dfs(x-1, y, m, n, heights, isVisitedOcean, heights[x][y]);
+        dfs(x+1, y, m, n, heights, isVisitedOcean, heights[x][y]);
+        dfs(x, y-1, m, n, heights, isVisitedOcean, heights[x][y]);
+        dfs(x, y+1, m, n, heights, isVisitedOcean, heights[x][y]);
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        int totalRows = heights.size();
-        int totalCols = heights[0].size();
-        int totalCells = totalRows * totalCols;
-        vector<vector<bool>> pacificVisited(totalRows, vector<bool>(totalCols, false));
-        vector<vector<bool>> atlanticVisited(totalRows, vector<bool>(totalCols, false));
-        //prcoess the pacificVisited
-        for(int col = 0; col < totalCols; col++){
-            dfs(heights, pacificVisited, 0, col, totalRows, totalCols);
-        }
-        for(int row = 0; row < totalRows; row++){
-            dfs(heights, pacificVisited, row, 0, totalRows, totalCols);
-        }
-        //process the atlanticVisited
-        for(int col = 0; col < totalCols; col++){
-            dfs(heights, atlanticVisited, totalRows-1, col, totalRows, totalCols);
-        }
-        for(int row = 0; row < totalRows; row++){
-            dfs(heights, atlanticVisited, row, totalCols -1, totalRows, totalCols);
-        }
-        //find the intersection
+        int m = heights.size();
+        int n = heights[0].size();
+        int totalNodes = m*n;
+        vector<vector<bool>> isVisitedPacific(m, vector<bool>(n, false));
+        vector<vector<bool>> isVisitedAtlantic(m, vector<bool>(n, false));
         vector<vector<int>> ans;
-        for(int i=0; i<totalRows; i++){
-            for(int j=0; j<totalCols; j++){
-                if(pacificVisited[i][j] == true && atlanticVisited[i][j] == true){
-                    vector<int>temp;
-                    temp.push_back(i);
-                    temp.push_back(j);
-                    ans.push_back(temp);
+
+        //dont have to visit through the matrix
+        //we have our 4 ways to traverse through
+        //pacific
+        for(int i=0; i<n; i++){
+            int y = i;
+            int x = 0;
+            int parentHeight = -1;
+            if(isVisitedPacific[x][y] == false){
+                dfs(x, y, m, n, heights, isVisitedPacific, parentHeight);
+            }
+        }
+        for(int i=0; i<m; i++){
+            int x = i;
+            int y = 0;
+            int parentHeight = -1;
+            if(isVisitedPacific[x][y] == false){
+                dfs(x, y, m, n, heights, isVisitedPacific, parentHeight);
+            }
+        }
+        //atlantic
+        for(int i=0; i<n; i++){
+            int y = i;
+            int x = m-1;
+            int parentHeight = -1;
+            if(isVisitedAtlantic[x][y] == false){
+                dfs(x, y, m, n, heights, isVisitedAtlantic, parentHeight);
+            }
+        }
+        for(int i=0; i<m; i++){
+            int x = i;
+            int y = n-1;
+            int parentHeight = -1;
+            if(isVisitedAtlantic[x][y] == false){
+                dfs(x, y, m, n, heights, isVisitedAtlantic, parentHeight);
+            }
+        }
+
+        //compute ans
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(isVisitedPacific[i][j] == true && isVisitedAtlantic[i][j] == true){
+                    ans.push_back({i, j});
                 }
             }
         }
