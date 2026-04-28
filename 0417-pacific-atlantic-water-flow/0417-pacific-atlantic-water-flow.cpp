62dff6748e7c494befd41d6b05f0341ac82aca41
Sairudra More <sairudra60@gmail.com>
2026-04-28 20:22:24 +0530

    Time: 4 ms (78.89%), Space: 23.1 MB (51.31%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,30 +1,11 @@
-//dfs
+//bfs
 class Solution {
 public:
-    void dfs(int x, int y, const int m, const int n, vector<vector<int>>& heights, 
-    vector<vector<bool>>& isVisitedOcean, int parentHeight){
-        //1st check if the coordinates r not safe
+    bool isSafe(int x, int y, const int m, const int n){
         if((x<0 || x>=m)||(y<0 || y>=n)){
-            return;
+            return false;
         }
-        //check if already visited
-        if(isVisitedOcean[x][y] == true){
-            return;
-        }
-        //check if water cannot flow
-        if(parentHeight > heights[x][y]){
-            return;
-        }
-        
-        //its safe now, mark it as visited
-        isVisitedOcean[x][y] = true;
-        
-        //explore its nbrs
-        //up, down, left, right
-        dfs(x-1, y, m, n, heights, isVisitedOcean, heights[x][y]);
-        dfs(x+1, y, m, n, heights, isVisitedOcean, heights[x][y]);
-        dfs(x, y-1, m, n, heights, isVisitedOcean, heights[x][y]);
-        dfs(x, y+1, m, n, heights, isVisitedOcean, heights[x][y]);
+        return true;
     }
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
         int m = heights.size();
@@ -33,45 +14,109 @@ public:
         vector<vector<bool>> isVisitedPacific(m, vector<bool>(n, false));
         vector<vector<bool>> isVisitedAtlantic(m, vector<bool>(n, false));
         vector<vector<int>> ans;
-
-        //dont have to visit through the matrix
-        //we have our 4 ways to traverse through
+        
+        queue<pair<pair<int, int>, int>> qPacific; //coordinates, prev Height
+        queue<pair<pair<int, int>, int>> qAtlantic; //coordinates, prev Height
+        //dont have to traverse through the matix
+        //we have our 4 starting areas
         //pacific
         for(int i=0; i<n; i++){
-            int y = i;
             int x = 0;
-            int parentHeight = -1;
-            if(isVisitedPacific[x][y] == false){
-                dfs(x, y, m, n, heights, isVisitedPacific, parentHeight);
-            }
+            int y = i;
+            int parentHeight = heights[x][y];
+            qPacific.push({{x, y}, parentHeight});
+            //as soon as u push it mark it as visited
+            isVisitedPacific[x][y] = true;
         }
         for(int i=0; i<m; i++){
             int x = i;
             int y = 0;
-            int parentHeight = -1;
-            if(isVisitedPacific[x][y] == false){
-                dfs(x, y, m, n, heights, isVisitedPacific, parentHeight);
+            int parentHeight = heights[x][y];
+            qPacific.push({{x, y}, parentHeight});
+            //as soon as u push it mark it as visited
+            isVisitedPacific[x][y] = true;
+        }
+
+        //start the process for pacific
+        //explore nbrs
+        //up, down, left, right
+        int dx[] = {-1, 1, 0, 0};
+        int dy[] = {0, 0, -1, 1}; 
+        while(!qPacific.empty()){
+            pair<pair<int, int>, int> frontData = qPacific.front();
+            qPacific.pop();
+            pair<int, int> coordinates = frontData.first;
+            int parentHeight = frontData.second;
+            int x = coordinates.first;
+            int y = coordinates.second;
+
+            //explore its nbrs
+            for(int i=0; i<4; i++){
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                //check if the new coordinates r safe
+                if(isSafe(newX, newY, m, n)){
+                    //check if not visited
+                    if(isVisitedPacific[newX][newY] == false){
+                        //check if flow is correct
+                        if(parentHeight <= heights[newX][newY]){
+                            //its safe now. u can push it in 
+                            qPacific.push({{newX, newY}, heights[newX][newY]});
+                            isVisitedPacific[newX][newY] = true;
+                        }
+                    }
+                }
             }
         }
-        //atlantic
+
         for(int i=0; i<n; i++){
-            int y = i;
             int x = m-1;
-            int parentHeight = -1;
-            if(isVisitedAtlantic[x][y] == false){
-                dfs(x, y, m, n, heights, isVisitedAtlantic, parentHeight);
-            }
+            int y = i;
+            int parentHeight = heights[x][y];
+            qAtlantic.push({{x, y}, parentHeight});
+            //as soon as u push it mark it as visited
+            isVisitedAtlantic[x][y] = true;
         }
         for(int i=0; i<m; i++){
             int x = i;
             int y = n-1;
-            int parentHeight = -1;
-            if(isVisitedAtlantic[x][y] == false){
-                dfs(x, y, m, n, heights, isVisitedAtlantic, parentHeight);
+            int parentHeight = heights[x][y];
+            qAtlantic.push({{x, y}, parentHeight});
+            //as soon as u push it mark it as visited
+            isVisitedAtlantic[x][y] = true;
+        }
+
+        //start the process for atlantic
+        //explore nbrs
+        //up, down, left, right
+        while(!qAtlantic.empty()){
+            pair<pair<int, int>, int> frontData = qAtlantic.front();
+            qAtlantic.pop();
+            pair<int, int> coordinates = frontData.first;
+            int parentHeight = frontData.second;
+            int x = coordinates.first;
+            int y = coordinates.second;
+
+            //explore its nbrs
+            for(int i=0; i<4; i++){
+                int newX = x + dx[i];
+                int newY = y + dy[i];
+                //check if the new coordinates r safe
+                if(isSafe(newX, newY, m, n)){
+                    //check if not visited
+                    if(isVisitedAtlantic[newX][newY] == false){
+                        //check if flow is correct
+                        if(parentHeight <= heights[newX][newY]){
+                            //its safe now. u can push it in 
+                            qAtlantic.push({{newX, newY}, heights[newX][newY]});
+                            isVisitedAtlantic[newX][newY] = true;
+                        }
+                    }
+                }
             }
         }
 
-        //compute ans
+        //ans
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(isVisitedPacific[i][j] == true && isVisitedAtlantic[i][j] == true){
