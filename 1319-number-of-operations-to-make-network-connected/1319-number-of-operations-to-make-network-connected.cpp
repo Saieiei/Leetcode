f62028f5bab992b438c6d4cca18a8305f2bf7a2e
Sairudra More <sairudra60@gmail.com>
2026-05-01 21:04:46 +0530

    Time: 15 ms (60.82%), Space: 46.7 MB (50.96%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,67 +1,75 @@
-//kruskals-algo - redundant conn
+//kruskals algo
+//nocc
 class Solution {
 public:
-
     int findParent(int node, vector<int>& parents){
+        //check if self then return
         if(parents[node] == node){
             return node;
         }
-        //else{ //path compression
-            return parents[node] = findParent(parents[node], parents);
-        //}
+        //else path compression
+        return parents[node] = findParent(parents[node], parents);
     }
+    void findUnion(vector<int>& parents, vector<int>&ranks, int uParent, int vParent){
+        //get the ranks
+        int uParentRank = ranks[uParentRank];
+        int vParentRank = ranks[vParentRank];
 
-    void unionOperation(int uParent, int vParent, vector<int>& parents, vector<int>& ranks){
-        int uRank = ranks[uParent];
-        int vRank = ranks[vParent];
-
-        if(uRank > vRank){
+        //now compare
+        if(uParentRank >= vParentRank){
+            ranks[uParentRank]++;
             parents[vParent] = uParent;
-            ranks[uParent]++;
         }
-        else if(vRank > uRank){
+        else{
+            ranks[vParentRank]++;
             parents[uParent] = vParent;
-            ranks[vParent]++;
-        }
-        else{ //both ranks are the same
-            parents[vParent] = uParent;
-            ranks[uParent]++;
         }
     }
-
     int makeConnected(int n, vector<vector<int>>& connections) {
-        vector<int>parents(n);
-        vector<int>ranks(n, 0);
+        //create trackers
+        vector<int> ranks(n, 0);
+        vector<int> parents(n);
+        //self parents
         for(int i=0; i<n; i++){
-            parents[i]=i;
+            parents[i] = i;
         }
+
         int redundantConnections = 0;
 
+        //start the process
+        //traverse though the edgelist
         for(vector<int> connection: connections){
             int u = connection[0];
             int v = connection[1];
-
+            //find their parents
             int uParent = findParent(u, parents);
             int vParent = findParent(v, parents);
 
+            //do the process if not same
             if(uParent != vParent){
-                unionOperation(uParent, vParent, parents, ranks);
+                findUnion(parents, ranks, uParent, vParent);
             }
-            else{//redundant connetion
+            else{
+                //this is a redundant connection
+                //we dont have to push u, v
+                //just increase count
                 redundantConnections++;
             }
         }
 
+        //now we have to find the actual proper connected edges
         //nocc
         int nocc = 0;
-        int noOfRequiredWires = 0;
         for(int i=0; i<n; i++){
             if(parents[i] == i){
                 nocc++;
             }
         }
-        noOfRequiredWires = nocc - 1;
 
-        return (redundantConnections >= noOfRequiredWires) ? noOfRequiredWires : -1;
+        //now we have to find the actual required connections
+        int actualRequiredConnections = nocc - 1;
+
+        //now based on the extra connections that u have return
+        return (redundantConnections >= actualRequiredConnections) ?  actualRequiredConnections : -1;
     }
 };
