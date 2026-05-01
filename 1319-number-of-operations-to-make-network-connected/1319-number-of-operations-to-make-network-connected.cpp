9c3ea3a6e162d6c06ef82c869f92aa2467463189
Sairudra More <sairudra60@gmail.com>
2026-05-01 21:07:09 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,5 +1,11 @@
 //kruskals algo
 //nocc
+auto init = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 'c';
+};
 class Solution {
 public:
     int findParent(int node, vector<int>& parents){
