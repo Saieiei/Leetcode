c25029dbd54830392d6e9142fb75e4b5ef63766d
Sairudra More <sairudra60@gmail.com>
2026-05-02 11:33:34 +0530

    Time: 0 ms (100%), Space: 13.6 MB (88.74%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,31 +1,49 @@
-//DFS
-//we will have a tempPath vector and a path vector<vector>
-//tempPath will be pushed in path
-//it will be pushed when src = dest
-//for backtracking we will pop inside the loop
-//the loop will traverse through the graph[node]
+//dfs
 class Solution {
 public:
-    void dfs(int src, int dest, vector<int>& tempPath, vector<vector<int>>& path, vector<vector<int>>& graph){
-        //1st just puth in the node in the tempPath
-        tempPath.push_back(src);
-        //u will return when src = dest
-        if(src == dest){
-            path.push_back(tempPath);
+    void dfs(int src, int n, vector<vector<int>>& graph, vector<int>& tempPath, vector<vector<int>>& ans, vector<bool>& isVisited){
+        //check if already visited
+        if(isVisited[src] == true){
             return;
         }
-        for(int nbr: graph[src]){
-            dfs(nbr, dest, tempPath, path, graph);
-            //backtracking
-            tempPath.pop_back();
+
+        //all safe now
+        //mark it as visited
+        isVisited[src] = true;
+        tempPath.push_back(src);
+
+        //check we have reached the end
+        if(src == n-1){
+            ans.push_back(tempPath);
         }
+
+        //explore its nbrs now
+        for(int nbr: graph[src]){
+            dfs(nbr, n, graph, tempPath, ans, isVisited);
+        }
+
+        //backtracking
+        //mark it as unvisited for others paths to visit it
+        isVisited[src] = false;
+        //pop it
+        tempPath.pop_back();
     }
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        int n = graph.size();
+
+        //no need of adjList
+        //the given data is already adjList
+
+        //isVisited is actually not needed
+        //because it is a DAG
+        vector<bool> isVisited(n, false);
+
         vector<int> tempPath;
-        vector<vector<int>> path;
+        vector<vector<int>> ans;
         int src = 0;
-        int dest = graph.size()-1;
-        dfs(src, dest, tempPath, path, graph);
-        return path;
+
+        //start the process
+        dfs(src, n, graph, tempPath, ans, isVisited);
+        return ans;
     }
 };
