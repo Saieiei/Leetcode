649ee68b1d4599ebbe3013126849bacc7ae1d992
Sairudra More <sairudra60@gmail.com>
2026-05-03 16:48:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,20 +1,38 @@
+auto init = [](){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 'c';
+}();
+//follows topo order concept
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        if (n == 1) return 1; // If there's only one person, they're automatically the judge.
-        
-        vector<int> trustCounts(n + 1, 0);
-        
-        for (const auto& t : trust) {
-            trustCounts[t[0]]--; // Decrease for trusting others
-            trustCounts[t[1]]++; // Increase for being trusted
+        //set up trackers. +1 because its starting from 1
+        vector<int> indegree(n+1, 0);
+        vector<int> outdegree(n+1, 0);
+
+        //we dont need a adjList here for the nbrs
+        //traverse through the given data
+        for(vector<int> data: trust){
+            int u = data[0];
+            int v = data[1];
+            //directed
+            //u->v
+            indegree[v]++;
+            outdegree[u]++;
         }
-        
-        for (int i = 1; i <= n; ++i) {
-            if (trustCounts[i] == n - 1) {
+
+        //now find the town judge
+        //should have no outgoing edges (trusts none)
+        //should have n-1 edges (all trusts, except himself)
+        for(int i=1; i<=n; i++){
+            if(outdegree[i] == 0 && indegree[i] == n-1){
+                //found him
                 return i;
             }
         }
-        return -1; // No judge found
+        //didint find
+        return -1;
     }
 };
