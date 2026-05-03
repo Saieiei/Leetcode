5a07bacf0a115002598daa2e334ae8e982c67c0e
Sairudra More <sairudra60@gmail.com>
2026-05-03 19:48:11 +0530

    Time: 0 ms (100%), Space: 14.5 MB (52.31%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,50 +1,37 @@
-//The idea about this is traversal
-//if u have the adjList and then u do travesal
-//and u do a DFS traversal (q, isVisited, countIsVisisted)
-//based on countIsVisisted u can tell if u can vist all the rooms
+//traversal
+//BFS
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        //in this case we dont have to create AdjList
-        //but if u want to create a adjList
-        unordered_map<int, vector<int>> adjList;
         int n = rooms.size();
-        for(int i=0; i<n; i++){
-            for(int j = 0; j<rooms[i].size(); j++){
-                adjList[i].push_back(rooms[i][j]);
-            }
-        }
+        //dont have to create adjList because rooms is same
+        vector<bool> isVisited(n, false);
 
-        unordered_map<int, bool> isVisited;
-        int countIsVisisted = 0;
         queue<int> q;
-        int src = 0;
-
-        //start the dfs process
-        q.push(src);
-        isVisited[src] = true;
-        countIsVisisted++;
+        q.push(0);
+        isVisited[0] = true;
 
         while(!q.empty()){
             int frontNode = q.front();
             q.pop();
-            //process its nbrs
-            for(int nbr: adjList[frontNode]){
-                if(!isVisited[nbr]){
+            //explore its nbrs
+            for(const int& nbr: rooms[frontNode]){
+                //check if not visitied
+                if(isVisited[nbr] == false){
+                    //mark it as visited and push it in 
                     q.push(nbr);
                     isVisited[nbr] = true;
-                    countIsVisisted++;
                 }
             }
         }
 
-        //check
-        if(countIsVisisted == rooms.size()){
-            return true;
+        //check if possible
+        for(int i=0; i<n; i++){
+            if(isVisited[i] == false){
+                return false;
+            }
         }
-        else{
-            return false;
-        }
-
+        //else possible
+        return true;
     }
 };
