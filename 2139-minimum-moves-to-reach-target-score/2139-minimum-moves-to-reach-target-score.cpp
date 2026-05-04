ec9db752fe85eb9b4920042a5feab50b27af7ace
Sairudra More <sairudra60@gmail.com>
2026-05-04 09:53:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -1,44 +1,48 @@
-//ulta q
-//bfs
+//ulta q = greedy
+//dfs
 class Solution {
 public:
+    void dfs(int target, int maxDoubles, int& moves){
+        //bc
+        if(target == 1){
+            //got the ans, now return
+            return;
+        }
+        if(maxDoubles == 0 && target > 1){
+            moves = moves + (target - 1);
+            target = 1;
+            //got the ans, now return
+            return;
+        }
+        else{ //maxDoubles > 0
+            //check if odd, make it even
+            if((target & 1) == 1){
+                moves++;
+                target--;
+            }
+            else{
+                //its even, use maxDoubles
+                target = target >> 1;
+                moves++;
+                maxDoubles--;
+            }
+        }
+
+        dfs(target, maxDoubles, moves);
+    }
     int minMoves(int target, int maxDoubles) {
         int moves = 0;
         //bc
-        if(maxDoubles == 0){
-            //-1 because the traget should reach 1 and not 0
-            moves = target - 1;
-            //now my target becomes 1
-            target = 1;
-            return moves;
-        }
         if(target == 1){
             return moves;
         }
-        while(true){
-            if(maxDoubles == 0 && target > 1){
-                //-1 because the traget should reach 1 and not 0
-                moves = moves + (target - 1);
-                //now my target becomes 1
-                target = 1;
-                return moves;
-            }
-            else{
-                //if target is odd, then make it even
-                if(target & 1 == 1){
-                    moves++;
-                    target --;
-                }
-                else{
-                    //if even then use maxDoubles
-                    maxDoubles--;
-                    moves++;
-                    target = target >> 1;
-                    if(target == 1){
-                        return moves;
-                    }
-                }
-            }
+        if(maxDoubles == 0 && target > 1){
+            //-1 because we have to reach 1 and not 0
+            moves = target - 1;
+            target = 1;
+            return moves;
         }
+        dfs(target, maxDoubles, moves);
+        return moves;
     }
 };
