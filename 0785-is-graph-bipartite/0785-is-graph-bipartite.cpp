34dfd200fe2f4a2b8431fd39f9cc51655f06c101
Sairudra More <sairudra60@gmail.com>
2026-05-04 19:57:47 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,8 +1,27 @@
-//bfs
+//dfs
 //2 colors
 //space optimized
 class Solution {
 public:
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& colors){
+        int nodeColor =  colors[node];
+
+        //explore its nbrs
+        for(int nbr: graph[node]){
+            //explore only if not visited
+            if(colors[nbr] == -1){
+                //mark it as visited
+                colors[nbr] = !nodeColor;
+                dfs(nbr, graph, colors);
+            }
+            else{ //marked a visited
+                if(nodeColor == colors[nbr]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
         //no need for adjList
@@ -23,36 +42,11 @@ public:
             //process those only if not visited
             int node = i;
             if(colors[node] == -1){
-                //start with bfs
-                queue<int> q;
-                q.push(node);
+                //start with dfs
                 //mark it as visited
                 //give it the color
                 colors[node] = color;
-
-                //start the process
-                while(!q.empty()){
-                    int frontNode = q.front();
-                    q.pop();
-                    int frontColor = colors[frontNode];
-                    //explore its nbrs if not visited
-                    for(int nbr: graph[frontNode]){
-                        if(colors[nbr] == -1){
-                            //give the color, mark it as visited
-                            //and push it
-                            int newColor = !frontColor;
-                            colors[nbr] = newColor;
-                            q.push(nbr);
-                        }
-                        else{ //colored, either 1 or 0
-                            //if already visited then color should not be the same
-                            int nbrColor = colors[nbr];
-                            if(nbrColor == frontColor){
-                                return false;
-                            }
-                        }
-                    }
-                }
+                return dfs(node, graph, colors);
             }
         }
         //no cases of false, then return true
