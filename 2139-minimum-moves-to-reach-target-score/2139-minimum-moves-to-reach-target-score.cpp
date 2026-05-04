80637246d54791b1de73858dcd25af458e6b3ad4
Sairudra More <sairudra60@gmail.com>
2026-05-04 14:07:42 +0530

    Time: 0 ms (100%), Space: 7.8 MB (63.89%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.cpp
@@ -1,48 +1,41 @@
-//ulta q = greedy
-//dfs
+//ulta
+//recursion - optimised - DFS
 class Solution {
 public:
-    void dfs(int target, int maxDoubles, int& moves){
-        //bc
+    int dfs(int target, int maxDoubles){
         if(target == 1){
-            //got the ans, now return
-            return;
+            int moves = 0;
+            return moves;
         }
-        if(maxDoubles == 0 && target > 1){
-            moves = moves + (target - 1);
+        if(maxDoubles == 0){
+            int moves = target - 1;
             target = 1;
-            //got the ans, now return
-            return;
-        }
-        else{ //maxDoubles > 0
-            //check if odd, make it even
-            if((target & 1) == 1){
-                moves++;
-                target--;
-            }
-            else{
-                //its even, use maxDoubles
-                target = target >> 1;
-                moves++;
-                maxDoubles--;
-            }
+            return moves; 
         }
 
-        dfs(target, maxDoubles, moves);
+        //explore 2 options
+        //check if odd
+        if(target & 1){
+            return 1 + dfs(target-1, maxDoubles);
+        }
+        else{
+            //target is even
+            return 1 + dfs(target>>1, maxDoubles-1);
+        }
     }
     int minMoves(int target, int maxDoubles) {
-        int moves = 0;
         //bc
+        int moves = 0;
         if(target == 1){
             return moves;
         }
-        if(maxDoubles == 0 && target > 1){
-            //-1 because we have to reach 1 and not 0
+        if(maxDoubles == 0){
+            //because it starts from 1 and not 0
             moves = target - 1;
             target = 1;
             return moves;
         }
-        dfs(target, maxDoubles, moves);
-        return moves;
+        //no need of moves here
+        return dfs(target, maxDoubles);
     }
 };
