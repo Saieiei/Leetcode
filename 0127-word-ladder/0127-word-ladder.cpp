55645ae2ccc61fd6b690f76a38c3cf4085ba3148
Sairudra More <sairudra60@gmail.com>
2026-05-05 15:27:15 +0530

    Time: 103 ms (35.97%), Space: 22.1 MB (24.25%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,63 @@
-//BFS
+//not like toposorting, alien dictionary
+//have to consider each word, each letter
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList.size();
+        //bc, already mentioned
+        ///if(beginWord == endWord){
+        ///    return 1;
+        ///}
+        //we have to keep the track of visited
+        unordered_map<string, bool> isVisited;
+        for(int i = 0; i < n; i++) {
+            isVisited[wordList[i]] = false; 
+        }
 
-        //bc
-        if(beginWord == endWord) return 0;
-        unordered_set<string> st(wordList.begin(), wordList.end());
-        queue<pair<string, int>> q;
-        q.push({beginWord, 1}); //word, iteration
+        int number = 1;
+        //now lets start the bfs
+        queue<pair<string, int>>q;
+        q.push({beginWord, number});
+        //mark it as visited
+        isVisited[beginWord] = true;
 
+        //start
         while(!q.empty()){
-            pair<string, int> frontNode = q.front();
+            //now we have to swap each letter with the alpha
+            //create the new word
+            //check if its there and push
+            pair<string, int> frontData = q.front();
             q.pop();
-            string currWord = frontNode.first;
-            int interation = frontNode.second;
-
-            //check
-            if(currWord == endWord) return interation;
-
-            //create new word
-            for(int index = 0; index<currWord.length(); index++){
-                string originalWordSave = currWord;
-                for(char ch = 'a'; ch <= 'z'; ch++){
-                    currWord[index] = ch; //curWord has been changed
-                    string newWord = currWord;
-                    if(st.find(newWord) != st.end()){ //found the word
-                        q.push({newWord, interation + 1});
-                        st.erase(newWord);
+            string frontWord = frontData.first;
+            int frontNumber = frontData.second;
+            //check if we have reached the end
+            if(frontWord == endWord){
+                return frontNumber;
+            }
+            //we have to create new words
+            int wordLength = frontWord.size();
+            for(int index = 0; index<wordLength; index++){
+                //for each index, replace the char with alphabets
+                for(char ch = 'a'; ch<='z'; ch++){
+                    string newWord = frontWord;
+                    newWord[index] = ch;
+                    //find this new word
+                    auto it = isVisited.find(newWord);
+                    if(it != isVisited.end()){
+                        //this word exits in the list
+                        //make sure its no visited
+                        if(isVisited[newWord] == false){
+                            //its not visited
+                            //mark it, push it in
+                            int newNumber = frontNumber + 1;
+                            q.push({newWord, newNumber});
+                            isVisited[newWord] = true;
+                        }
                     }
                 }
-                currWord = originalWordSave;
             }
         }
-        //not possible
+        //didnt find the endWord
         return 0;
     }
 };
