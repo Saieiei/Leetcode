bd13ce27b86acaaf76bd8fdb691f3407423be914
Sairudra More <sairudra60@gmail.com>
2026-05-08 19:40:47 +0530

    Time: 22 ms (83.59%), Space: 99.6 MB (85.72%) - LeetHub

--- 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
+++ 2360-longest-cycle-in-a-graph/2360-longest-cycle-in-a-graph.cpp
@@ -1,54 +1,83 @@
 //there is a cycle anyways -> topological sorting
-//and per 1 disconnected graph, there might be 1 ctcle
+//and per 1 disconnected graph, there might be 1 cycle
 //because it says that there is only atmost 1 outgoing edge
 //so we dont have to create adjList because given data is enough
-//we need to keep track of the dists, for each noce, so we need dist[]
-//DFS
+//BFS
+//We will 1st do normal BFS and execute all the nodes thats not in cycle
+//the rest of the nodes, thats not visited are basicially cycles of the disconnected graph
+//so we will have to travese through of these loops, keep the track of the largest size
 class Solution {
 public:
-    //in this DFS, we dont have to return anything
-    //if we the DFS return bool, then it will stop early
-    //because of which we will not be able to get the correct size
-    void dfs(int currLen, int& ans, vector<int>& edges, vector<bool>& isVisited, 
-    vector<bool>& isPathVisited, int node, vector<int>& dist){
-        //1st mark it as visited
-        isVisited[node] = true;
-        isPathVisited[node] = true;
-        currLen++;
-        dist[node] = currLen;
-        //explore nbrs
-        int nbr = edges[node];
-        //imp
-        if(nbr != -1){
-            if(!isVisited[nbr]){
-                //check for cycle, but dont stop early
-                dfs(currLen, ans, edges, isVisited, isPathVisited, nbr, dist);
-            }
-            else{
-                if(isPathVisited[nbr]){
-                    //cycle found
-                    int cycleLen = currLen - dist[nbr]  +1;
-                    ans = max(ans, cycleLen);
-                }
-            }
-        }
-        //backtracking
-        isPathVisited[node] = false;
-    }
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
         vector<bool> isVisited(n, false);
-        vector<bool> isPathVisited(n, false);
-        vector<int> dist(n, 0);
         int ans = -1;
         int currLen = 0;
-
-        //start DFS, disconnected
+        //process indegrees
+        vector<int> indegrees(n, 0);
         for(int i=0; i<n; i++){
             int node = i;
-            int currLen = 0;
+            //explore its nbrs
+            int nbr = edges[node];
+            //imp
+            if(nbr != -1){
+                indegrees[nbr]++;
+            }
+        }
+
+        //BFS
+        queue<int> q;
+        for(int i=0; i<n; i++){
+            //pushin nodes with 0 indegrees
+            //and mark it as visited
+            int node = i;
+            if(indegrees[node] == 0){
+                q.push(node);
+                isVisited[node] = true;
+            }
+        }
+        //start the process
+        while(!q.empty()){
+            int frontNode = q.front();
+            q.pop();
+            //explore its nbrs
+            int nbr = edges[frontNode];
+            //very imp
+            if(nbr != -1){
+                if(!isVisited[nbr]){
+                    indegrees[nbr]--;
+                    if(indegrees[nbr] == 0){
+                        q.push(nbr);
+                        isVisited[nbr] = true;
+                    }
+                }
+            }
+        }
+        //q is empty
+        //now all that we have not visited are the cycles
+        for(int i=0; i<n; i++){
+            int node = i;
             if(!isVisited[node]){
-                dfs(currLen, ans, edges, isVisited, isPathVisited, node, dist);
+                //cycle discovered
+                q.push(node);
+                isVisited[node] = true;
+
+                currLen = 0;
+                while(!q.empty()){
+                    int frontNode = q.front();
+                    q.pop();
+                    currLen++;
+                    //explore nbrs
+                    int nbr = edges[frontNode];
+                    // Just follow the cycle, no indegree checks needed here
+                    if(nbr != -1){
+                        if(!isVisited[nbr]){
+                            q.push(nbr);
+                            isVisited[nbr] = true;
+                        }
+                    }
+                }
+                ans = max(ans, currLen);
             }
         }
         return ans;
