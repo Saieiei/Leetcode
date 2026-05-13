94afa2678bfb9a8b2db52b9d288bf6a8bb5df083
Sairudra More <sairudra60@gmail.com>
2026-05-13 21:16:26 +0530

    Time: 58 ms (30.44%), Space: 53.7 MB (10.6%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.cpp
+++ 0721-accounts-merge/0721-accounts-merge.cpp
@@ -1,86 +1,83 @@
 //kruskals algo
 class Solution {
 public:
-    int findParent(vector<int>& parents, int node){
-        //check if same
+    int parentFind(int node, vector<int>& parents){
+        //check if self parents
         if(parents[node] == node){
             return node;
         }
         //path compression
-        return parents[node] = findParent(parents, parents[node]);
+        return parents[node] = parentFind(parents[node], parents);
     }
-    void findUnion(vector<int>& parents, vector<int>& ranks, int currentRowID, int alreadyRowID){
-        int parentCurrentRowID = findParent(parents, currentRowID);
-        int parentAlreadyRowID = findParent(parents, alreadyRowID);
-
-        //start the process
-        int rankCurrentRowID = ranks[parentCurrentRowID];
-        int rankAlreadyRowID = ranks[parentAlreadyRowID];
-        if(rankCurrentRowID >= rankAlreadyRowID){
-            ranks[parentCurrentRowID]++;
-            parents[parentAlreadyRowID] = parentCurrentRowID;
+    void unionFind(int node1, int node2, vector<int>& parents, vector<int>& ranks){
+        //find the parent
+        int node1Parent = parentFind(node1, parents);
+        int node2Parent = parentFind(node2, parents);
+        //ranks
+        int node1ParentRank = ranks[node1Parent];
+        int node2ParentRank = ranks[node2Parent];
+        //update
+        if(node1ParentRank >= node2ParentRank){
+            ranks[node1Parent]++;
+            parents[node2Parent] = node1Parent;
         }
         else{
-            ranks[parentAlreadyRowID]++;
-            parents[parentCurrentRowID] = parentAlreadyRowID;
+            ranks[node2Parent]++;
+            parents[node1Parent] = node2Parent;
         }
     }
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-        //1st create mp
-        unordered_map<string, int> mp;
         int n = accounts.size();
-        vector<int> ranks(n, 0);
         vector<int> parents(n);
+        vector<int> ranks(n, 0);
+        //self parents
         for(int i=0; i<n; i++){
             parents[i] = i;
         }
-        //process givenData -> mp
+
+        unordered_map<string, int> mp;
+        //traverse through the accounts
         int rowID = 0;
-        for(vector<string> account: accounts){
-            int colSize = account.size();
-            //we dont require the title here -> account[0]
-            for(int j=1; j<colSize; j++){
-                //check if the characteristics is already there in the mp
-                string characteristic = accounts[rowID][j];
+        for(vector<string> data: accounts){
+            int colSize = data.size();
+            string title = data[0];
+            //characteristics
+            for(int i=1; i<colSize; i++){
+                string characteristic = accounts[rowID][i];
+                //check if its already there in the mp
                 auto it = mp.find(characteristic);
-                if(it == mp.end()){
-                    //did not find it in the mp
-                    //push it in the mp
-                    mp[characteristic] = rowID;
+                if(it != mp.end()){
+                    //its already present
+                    //unionFind
+                    unionFind(rowID, it->second, parents, ranks);
                 }
                 else{
-                    //already there in the mp
-                    //do findUnion
-                    int currentRowID = rowID;
-                    int alreadyRowID = it->second;
-                    findUnion(parents, ranks, currentRowID, alreadyRowID);
+                    //did not find
+                    //push it in the mp
+                    mp[characteristic] = rowID;
                 }
             }
             rowID++;
         }
 
-        //create the preAns
         unordered_map<int, set<string>> preAns;
-        //traverse through the mp
+        //traverse through the map
         for(pair<const string, int> data: mp){
+            int rowId = data.second;
             string characteristic = data.first;
-            int rowID = data.second;
-            //forgot this step
-            int parentRowID = findParent(parents, rowID);
-            preAns[parentRowID].insert(characteristic);
+            int parentRowId = parentFind(rowId, parents);
+            preAns[parentRowId].insert(characteristic);
         }
 
-        //create ans
-        vector<vector<string>> ans;
-        //traverse through preAns
+        vector<vector<string>>ans;
         for(pair<const int, set<string>> data: preAns){
-            int parentRowID = data.first;
-            set<string> characteristics = data.second;
-            string title = accounts[parentRowID][0];
+            int rowID = data.first;
+            set<string> characteristics =  data.second;
+            string title = accounts[rowID][0];
             vector<string> temp;
             temp.push_back(title);
-            for(string characteristic: characteristics){
-                temp.push_back(characteristic);
+            for(string character: characteristics){
+                temp.push_back(character);
             }
             ans.push_back(temp);
         }
