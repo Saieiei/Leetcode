63a5ca1df91ec0ddb2d87ec04f3f81812675607d
Sairudra More <sairudra60@gmail.com>
2026-05-20 01:00:56 +0530

    Time: 3 ms (50.76%), Space: 18 MB (62.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,8 +1,5 @@
 class Solution {
 public:
-    static bool myCmp(const pair<int, int>a, const pair<int, int>b){
-        return a.first>b.first;
-    }
     vector<int> topKFrequent(vector<int>& nums, int k) {
        //M1 hashMap
        unordered_map<int, int> mp;
@@ -12,13 +9,15 @@ public:
        }
        //push all that to a vec
        vector<pair<int, int>> pairVec;
-       for(pair<const int, int> it: mp){
+       for(const pair<const int, int>& it: mp){
         int freq = it.second;
         int num = it.first;
         pairVec.push_back({freq, num});
        }
        //sort this vec with custom sort
-       sort(pairVec.begin(), pairVec.end(), myCmp);
+       sort(pairVec.begin(), pairVec.end(), [](const pair<int, int>& a, const pair<int, int>& b){
+        return a.first>b.first;
+       });
        //return the top k freq elements
        vector<int> ans;
        for(int i=0; i<k; i++){
