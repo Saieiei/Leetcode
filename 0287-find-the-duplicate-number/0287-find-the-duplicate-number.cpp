fb9ad8a4eecf33013a80aa5dc54d525314561d86
Sairudra More <sairudra60@gmail.com>
2026-05-19 17:33:48 +0530

    Time: 3 ms (67.69%), Space: 65 MB (68.7%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.cpp
@@ -3,16 +3,32 @@ public:
     int findDuplicate(vector<int>& nums) {
         int n = nums.size();
         //M1 brute force, sort
-        sort(nums.begin(), nums.end());
-        int i = 0;
-        int j = 0;
-        for(int i=0; i<n-1; i++){
-            j = i+1;
-            if(nums[i] == nums[j]){
-                return nums[i];
+        //sort(nums.begin(), nums.end());
+        //int i = 0;
+        //int j = 0;
+        //for(int i=0; i<n-1; i++){
+        //    j = i+1;
+        //    if(nums[i] == nums[j]){
+        //        return nums[i];
+        //    }
+        //}
+        ////else
+        //return 0;
+
+        //M2 in place, marking - as visisted
+        for(int i=0; i<n; i++){
+            //marked as visisted
+            int index = abs(nums[i]);
+            if(nums[index]<0){
+                return index;
+            }
+            else{
+                //mark it as visisted
+                //mark it -
+                //2s compliment
+                nums[index] = ~nums[index] + 1;
             }
         }
-        //else
         return 0;
         
     }
