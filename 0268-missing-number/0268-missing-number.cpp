0dc9254e3d5b49c43a57ba26d679eb4df4360fcb
Sairudra More <sairudra60@gmail.com>
2026-05-19 10:04:53 +0530

    Time: 0 ms (100%), Space: 21.9 MB (37.4%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,20 +1,15 @@
+//XOR
 class Solution {
 public:
-    int xorr(vector<int>&nums) {
-	int ans = 0;
-	// 1. xor all values of array
-	for(int i=0; i<nums.size(); ++i) {
-		ans = ans^nums[i];
-	}
-	//2. xor all range items [0, N]
-	int n = nums.size();
-	for(int i=0;i<=n;i++) {
-		ans ^= i;
-	}
-	return ans;
-}
     int missingNumber(vector<int>& nums) {
-        // return sortingMethod(nums);
-	    return xorr(nums);
+        int n = nums.size();
+        int ans = 0;
+        for(int num: nums){
+            ans = ans^num;
+        }
+        for(int i=0; i<=n; i++){
+            ans = ans^i;
+        }
+        return ans;
     }
 };
