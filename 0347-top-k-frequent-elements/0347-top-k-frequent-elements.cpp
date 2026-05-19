cce3161e190ca42f50456067c61e2f1c70a036cf
Sairudra More <sairudra60@gmail.com>
2026-05-20 01:07:13 +0530

    Time: 0 ms (100%), Space: 17.9 MB (62.01%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-       //M1 hashMap
+       //M3 hashMap + maxHeap
        unordered_map<int, int> mp;
        //populate the freq
        for(int num: nums){
         mp[num]++;
        }
-       //push all that to a vec
-       vector<pair<int, int>> pairVec;
+       //create maxHeap
+       priority_queue<pair<int, int>> pq;
+       //push all that to a pq
        for(const pair<const int, int>& it: mp){
         int freq = it.second;
         int num = it.first;
-        pairVec.push_back({freq, num});
+        pq.push({freq, num});
        }
-       //sort this vec with custom sort
-       sort(pairVec.begin(), pairVec.end(), [](const pair<int, int>& a, const pair<int, int>& b){
-        return a.first>b.first;
-       });
        //return the top k freq elements
        vector<int> ans;
        for(int i=0; i<k; i++){
-        int num = pairVec[i].second;
+        pair<int, int> frontData = pq.top();
+        int freq = frontData.first;
+        int num = frontData.second;
         ans.push_back(num);
+        pq.pop();
        }
        return ans;
     }
