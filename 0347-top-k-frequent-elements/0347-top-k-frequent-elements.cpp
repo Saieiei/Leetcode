ba1f5e62622313cd0adacdb7036ce2246fb95fab
Sairudra More <sairudra60@gmail.com>
2026-05-20 09:58:08 +0530

    Time: 2 ms (57.57%), Space: 20 MB (10.7%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,35 +1,33 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-       //M4 hashMap + minHeap -> space optimization
-       unordered_map<int, int> mp;
-       //populate the freq
-       for(int num: nums){
-        mp[num]++;
-       }
-       //create minHeap
-       using p = pair<int, int>;
-       priority_queue<p, vector<p>, greater<p>> pq;
-       //push all that to a pq
-       for(const pair<const int, int>& it: mp){
-        int freq = it.second;
-        int num = it.first;
-        pq.push({freq, num});
-        //pop if greater than size k
-        //we only need the last k elements
-        if(pq.size() > k){
-            pq.pop();
+        int n = nums.size();
+        //M5 bucket sorting
+        unordered_map<int, int> mp;
+        //hashing
+        for(int num: nums){
+            mp[num]++;
         }
-       }
-       //return the top k freq elements
-       vector<int> ans;
-       for(int i=0; i<k; i++){
-        pair<int, int> frontData = pq.top();
-        int freq = frontData.first;
-        int num = frontData.second;
-        ans.push_back(num);
-        pq.pop();
-       }
-       return ans;
+        //bucketing
+        vector<vector<int>> buckets(n+1); //<freq, num>
+        //traverse through the mp
+        for(pair<const int, int> it: mp){
+            int num = it.first;
+            int freq = it.second;
+            //push it in, in the correct buckets
+            buckets[freq].push_back(num);
+        }
+        //return the top k freq elements
+        vector<int> ans;
+        //we will start from the bucket which has highest freq
+        //freq = row
+        for(int freq=n; freq>=0; freq--){
+            for(const int& num: buckets[freq]){
+                if(ans.size()<k){
+                    ans.push_back(num);
+                }
+            }
+        }
+        return ans;
     }
 };
