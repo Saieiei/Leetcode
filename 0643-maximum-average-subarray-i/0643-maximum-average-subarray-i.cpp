7ec02fc7a1b70208e43b76c7233888c82ded91b1
Sairudra More <sairudra60@gmail.com>
2026-05-20 21:37:10 +0530

    Time: 0 ms (100%), Space: 113.7 MB (59.29%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.cpp
@@ -1,37 +1,27 @@
 class Solution {
 public:
-    double SlidingWindow(vector<int>& nums, int& k)
-    {
-        int i=0;
-        int j=k-1;
-
-        int sum=0;
-        int maxsum=INT_MIN;
-        for(int x=i;x<=j;x++)
-        {
-            sum=sum+nums[x];
-        }
-        maxsum=sum;
-
-        //catch part
-        j++;
-
-        while(j<nums.size())
-        {
-            sum=sum-nums[i++];
-            sum=sum+nums[j++];
-            maxsum=max(maxsum, sum);
-        }
-
-        double ans=maxsum/(double)k;
-        return ans;
-
-
-
-    }
     double findMaxAverage(vector<int>& nums, int k) {
-        return SlidingWindow(nums, k);
-
-        
+        int n = nums.size();
+        //sliding window - implimentation1
+        int i = 0; 
+        int j = k-1;
+        //1st find the sum of 1st k elements
+        int maxSum = INT_MIN;
+        int sum = 0;
+        for(int k = i; k<=j; k++){
+            sum = sum + nums[k];
+        }
+        maxSum = sum;
+        //now move j by 1 step front
+        j++;
+        //now keep processing until we reach the end
+        //from the sum sub i and add j pointer values
+        while(j<n){
+            sum = sum + nums[j] - nums[i];
+            j++;
+            i++;
+            maxSum = max(maxSum, sum);
+        }
+        return maxSum/(double)k;
     }
 };
