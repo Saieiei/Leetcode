0e105c2bfbe13d8b298dbd3dd58b1353d6c2f91c
Sairudra More <sairudra60@gmail.com>
2026-05-20 19:33:27 +0530

    Time: 0 ms (100%), Space: 24.5 MB (9.31%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -1,34 +1,40 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        //M1 hash maps
-        //create hash map
-        unordered_map<int, int> mp;
+        int n = nums.size();
+        //M2 bucket sorting
+        //1st hashing
+        unordered_map<int, int>mp;
         //populate the freq
         for(int num: nums){
             mp[num]++;
         }
 
-        //traverse through the mp and find the highest freq
-        int maxFreq = INT_MIN;
+        //2nd bucketing
+        vector<vector<int>> buckets(n+1);
+        //traverse through the mp and populate the buckets
         for(pair<const int, int>& it: mp){
             int num = it.first;
             int freq = it.second;
-            maxFreq = max(maxFreq, freq);
+            //populate
+            buckets[freq].push_back(num);
         }
-
-        //find out hpw many numbers have the max freq
-        int count = 0;
-        for(pair<const int, int>& it: mp){
-            int num = it.first;
-            int freq = it.second;
-            if(freq == maxFreq){
-                count++;
+        //find out the bucket which has the maximum freq
+        //should not be empty
+        int ans = 0;
+        for(int i=n; i>=0; i--){
+            //basically the row should have cols inside it
+            int freq = i;
+            if(!buckets[freq].empty()){
+                //found something
+                //traverse through this non-empty row
+                for(int num: buckets[freq]){
+                    ans = ans + freq;
+                }
+                break; // CRITICAL: Stop after processing the max frequency
             }
         }
-
-        //return ans
-        return count*maxFreq;
+        return ans;
 
     }
 };
