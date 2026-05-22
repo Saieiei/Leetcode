40d5c2369891f820078eeae387060f802cff4afc
Sairudra More <sairudra60@gmail.com>
2026-05-22 16:58:49 +0530

    Time: 0 ms (100%), Space: 20 MB (10.74%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,33 +1,41 @@
+//bucket sort
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         int n = nums.size();
-        //M5 bucket sorting
-        unordered_map<int, int> mp;
         //hashing
+        unordered_map<int, int>mp;
         for(int num: nums){
             mp[num]++;
         }
+
         //bucketing
-        vector<vector<int>> buckets(n+1); //<freq, num>
-        //traverse through the mp
-        for(pair<const int, int> it: mp){
+        vector<vector<int>> buckets(n+1);
+        vector<int> ans;
+        //traverse through the map
+        for(const pair<const int, int>& it: mp){
             int num = it.first;
             int freq = it.second;
-            //push it in, in the correct buckets
             buckets[freq].push_back(num);
         }
-        //return the top k freq elements
-        vector<int> ans;
-        //we will start from the bucket which has highest freq
-        //freq = row
-        for(int freq=n; freq>=0; freq--){
-            for(const int& num: buckets[freq]){
-                if(ans.size()<k){
+
+        //populate the ans
+        for(int i=n; i>=0; i--){
+            if(ans.size() == k){
+                break;
+            }
+            //check if that row is empty
+            if(!buckets[i].empty()){
+                //push the num in the ans
+                for(int num: buckets[i]){
                     ans.push_back(num);
+                    //check if we have reached the size
+                    if(ans.size() == k){
+                        return ans;
+                    }
                 }
             }
         }
-        return ans;
+        return {};
     }
 };
