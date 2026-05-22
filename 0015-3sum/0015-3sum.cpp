c3ad5ab3bcd74c69ea41004cc1480b68ed44d390
Sairudra More <sairudra60@gmail.com>
2026-05-22 16:27:59 +0530

    Time: 51 ms (43.74%), Space: 29 MB (91.6%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,44 +1,40 @@
-//we will 1st sort, 2nd pivot 1 and use 2 pointers
-//removing the duplicates is the main task here
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n = nums.size();
+        //sort it 1st
         sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
-        //size cannot be less than 3
-        //index is the pivot
-        for(int index=0; index<n; index++){
-            // skip duplicates
-            if (index > 0 && nums[index] == nums[index - 1]){
+        //traverse through the array now
+        for(int i=0; i<n; i++){
+            //remove duplicates
+            if(i>0 && nums[i] == nums[i-1]){
                 continue;
             }
-            int leftIndex = index+1;
+            //now we have a new num
+            int leftIndex = i+1;
             int rightIndex = n-1;
             while(leftIndex < rightIndex){
-                int sum = nums[leftIndex] + nums[index] + nums[rightIndex];
-                //if sum is < 0 we need to increase the leftIndex
-                if(sum < 0){
-                    leftIndex = leftIndex + 1;
+                int sum = nums[i] + nums[leftIndex] + nums[rightIndex];
+                if(sum == 0){
+                    //found it
+                    ans.push_back({nums[i], nums[leftIndex], nums[rightIndex]});
+                    while (leftIndex < rightIndex && nums[leftIndex] == nums[leftIndex + 1]){
+                        leftIndex++;
+                    }
+                    while (leftIndex < rightIndex && nums[rightIndex] == nums[rightIndex - 1]){
+                        rightIndex--;
+                    }
+                    leftIndex++;
+                    rightIndex--;
                 }
-                //if sum is > 0 we need to decrease the rightIndex
                 else if(sum > 0){
+                    //move right index inside
                     rightIndex = rightIndex - 1;
                 }
                 else{
-                    //we got a match
-                    vector<int> temp{nums[leftIndex], nums[index], nums[rightIndex]};
-                    ans.push_back(temp);
-                    //process until we remove upcoming duplicates
-                    while(leftIndex < rightIndex && nums[leftIndex] == nums[leftIndex + 1]){
-                        leftIndex++;
-                    }
-                    while(leftIndex < rightIndex && nums[rightIndex] == nums[rightIndex - 1]){
-                        rightIndex--;
-                    }
-                    //after u have pushed, update the pointers
-                    leftIndex++;
-                    rightIndex--;
+                    //move left index outside
+                    leftIndex = leftIndex + 1;
                 }
             }
         }
