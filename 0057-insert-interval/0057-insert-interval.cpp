5c2a2d71fe8bf2c449368c1efe664085f37832c2
Sairudra More <sairudra60@gmail.com>
2026-05-23 22:11:19 +0530

    Time: 3 ms (29.95%), Space: 21.8 MB (45.41%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        vector<vector<int>> result;
-            for (int i = 0; i < intervals.size(); i++) {
-                if (intervals[i][1] < newInterval[0]) {
-                    result.push_back(intervals[i]);
-                }
-
-                else if (intervals[i][0] > newInterval[1]) {
-                    result.push_back(newInterval);
-                    newInterval = intervals[i];
-                } else {
-                    newInterval[0] = min(intervals[i][0], newInterval[0]);
-                    newInterval[1] = max(newInterval[1], intervals[i][1]);
-                }
+        //this is similar to merge intervals qs
+        //naive method - insert newInterval in intervals
+        //NlogN instead of N
+        intervals.push_back(newInterval);
+        //continue with the rest of the merge interval process
+        //it was already sorted, but then since we pushed, 
+        //we have to sort it again
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> ans;
+        //push the 1st row as it is in the ans
+        ans.push_back(intervals[0]);
+        int n = intervals.size();
+        //star the loop till n
+        for(int i=1; i<n; i++){
+            //check if they r overlapping
+            if(ans.back()[1] >= intervals[i][0]){
+                //if so merge them
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            result.push_back(newInterval);
-            return result;
+            else{
+                //simply push
+                ans.push_back(intervals[i]);
+            }
         }
-    };
+        return ans;
+    }
+};
