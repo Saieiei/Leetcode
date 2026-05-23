bb698842d9fdb515d17ede66889ea204365e7b0f
Sairudra More <sairudra60@gmail.com>
2026-05-23 10:18:36 +0530

    Time: 4 ms (20.83%), Space: 42.2 MB (16.39%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,28 +1,27 @@
-// https://www.youtube.com/watch?v=4DIvrJHnfOc
+//PREFIX SUM
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        
-        vector<int> prefix(nums.size());
-        vector<int> suffix(nums.size());
-        vector<int> ans(nums.size());
-
-        prefix[0]=1;
-        suffix[nums.size()-1]=1;
-
-        for(int i=1;i<nums.size();i++)
-        {
-            prefix[i]=prefix[i-1]*nums[i-1];
+        int n = nums.size();
+        //just like pivot sum
+        vector<int> prefixMul(n);
+        vector<int> postfixMul(n);
+        vector<int> ans(n);
+        //inital cases
+        prefixMul[0] = 1;
+        postfixMul[n-1] = 1;
+        //start the for loop for prefix
+        for(int i=1; i<n; i++){
+            prefixMul[i] = prefixMul[i-1] * nums[i-1];
         }
-
-        for(int i=nums.size()-2;i>=0;i--)
-        {
-            suffix[i]=suffix[i+1]*nums[i+1];
+        //start the for loop for postfix
+        for(int i=n-2; i>=0; i--){
+            postfixMul[i] = postfixMul[i+1] * nums[i+1];
         }
-
-        for(int i=0;i<nums.size();i++)
-        {
-            ans[i]=prefix[i]*suffix[i];
+        //get ur final answer
+        //formula
+        for(int i=0; i<n; i++){
+            ans[i] = prefixMul[i] * postfixMul[i];
         }
 
         return ans;
