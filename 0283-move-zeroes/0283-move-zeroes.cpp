bd805315d8b7928e7ead36d0ca2d83103772ea05
Sairudra More <sairudra60@gmail.com>
2026-05-24 23:46:31 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.72%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //optimal way 
+        //Optimal Method N
         //2 pointers
         //move 1stIndex only when 2nd Index found
         //and swap then 1st
         int n = nums.size();
         int i=0;
         //traverse through th rest
-        for(int i=0; i<n; i++){
-            for(int j=1; j<n; j++){
-                if(nums[i] == 0 && nums[j] != 0 && j>i){
-                    //swap them and update the index
-                    swap(nums[i], nums[j]);
-                    i++;
-                }
+        for(int j=0; j<n; j++){
+            if(nums[j] != 0){
+                //swap them and update the index
+                swap(nums[i], nums[j]);
+                i++;
             }
         }
         return;
