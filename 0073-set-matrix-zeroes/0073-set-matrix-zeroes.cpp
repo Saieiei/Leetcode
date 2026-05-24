5673ad27ce973f2c1bc6c42045a4bbc3fa1efa1b
Sairudra More <sairudra60@gmail.com>
2026-05-24 15:30:17 +0530

    Time: 0 ms (100%), Space: 20.8 MB (48.99%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,29 +3,46 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        //brute force method, N^2
-        //this is like ur setting up lazers on the wall
-        //where u get the value as 0
-        //1st traverse through it
-        vector<bool> rows(m, false);
-        vector<bool> cols(n, false);
+        //this 1 takes N^2 too but 0 space
+        //this is again like magnets
+        //if we find 0 we will setup magnets on the wall
+        //we have to consider [0][0] separetly as it is a corner piece
+        //and it can influence both the row and the col
+        bool col0 = false;
+
+        //traverse through the matrix and set up the magnets
         for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
+            //check if the corner prience is 0
+            if(matrix[i][0] == 0){
+                col0 = true;
+                //later on, in the last part, we will mark teh whole row as 0
+            }
+
+            //traverse through the rest of the col
+            for(int j=1; j<n; j++){
                 if(matrix[i][j] == 0){
-                    rows[i] = true;
-                    cols[j] = true;
+                    //set up the magents on the walls
+                    //col
+                    matrix[0][j] = 0;
+                    //row
+                    matrix[i][0] = 0;
                 }
             }
         }
-
-        //now while traversing again
-        //check if its row or col was marked
-        //if so change that cell to 0
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(rows[i] || cols[j]){
+        //now we enter phase 2 where we traverse from bottom right to top left
+        //but we skip the 1st col
+        for(int i=m-1; i>=0; i--){
+            for(int j=n-1; j>=1; j--){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0){
+                    //mark its as 0
                     matrix[i][j] = 0;
                 }
+            }
+        }
+        //in the end process the 1st col
+        if(col0 == true){
+            for(int i=0; i<m; i++){
+                matrix[i][0] = 0;
             }
         }
         return;
