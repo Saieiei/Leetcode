511e54f0961038210fab041139c4efe230237637
Sairudra More <sairudra60@gmail.com>
2026-05-25 09:25:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (47.39%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,7 +1,39 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //brute force NlogN
-        sort(nums.begin(), nums.end());        
+        //since only 3 elements we could rather count them
+        //N
+        int n = nums.size();
+        int zeros = 0;
+        int ones = 0;
+        int twos = 0;
+        //traverse through the vector
+        for(int num: nums){
+            if(num == 0){
+                zeros++;
+            }
+            else if(num == 1){
+                ones++;
+            }
+            else{
+                twos;
+            }
+        }    
+        //rearrange now
+        for(int i=0; i<n; i++){
+            if(zeros > 0){
+                nums[i] = 0;
+                zeros--;
+            }
+            else if(ones > 0){
+                nums[i] = 1;
+                ones--;
+            }
+            else{
+                nums[i] = 2;
+                twos--;
+            }
+        }
+        return;    
     }
 };
