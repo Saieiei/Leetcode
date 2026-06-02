ee9c845739c67511db43bd054466d555ab93ad56
Sairudra More <sairudra60@gmail.com>
2026-06-02 23:01:58 +0530

    Time: 1 ms (2.25%), Space: 14.1 MB (7.03%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,29 +3,22 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        //Binary search logMN
-        int low = 0;
-        int high = m*n-1;
-        //start the process
-        while(low <= high){
-            int mid = low + (high - low)/2;
-            //check if this is the target
-            int row = mid/n;
-            int col = mid%n;
-            int midValue = matrix[row][col];
-            if(midValue == target){
-                return true;
-            }
-            else{
-                if(midValue > target){
-                    high = mid -1;
-                }
-                else{
-                    low = mid +1;
-                }
+        //MN
+        vector<int> flatMatrix(m*n);
+
+        //populate flatMatrix
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                flatMatrix[i*n +j] = matrix[i][j];  
             }
         }
-        //not possible
+
+        //now travese and find out
+        for(int i=0; i<m*n; i++){
+            if(flatMatrix[i] == target){
+                return true;
+            }
+        }
         return false;
     }
 };
