4259b7d0735b2da325bf8b046a8b732c8399a009
Sairudra More <sairudra60@gmail.com>
2026-06-02 22:47:50 +0530

    Time: 0 ms (100%), Space: 13.3 MB (77.94%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -3,14 +3,29 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
-        //brute force N2
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++){
-                if(matrix[i][j] == target){
-                    return true;
+        //Binary search logMN
+        int low = 0;
+        int high = m*n-1;
+        //start the process
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            //check if this is the target
+            int row = mid/n;
+            int col = mid%n;
+            int midValue = matrix[row][col];
+            if(midValue == target){
+                return true;
+            }
+            else{
+                if(midValue > target){
+                    high = mid -1;
+                }
+                else{
+                    low = mid +1;
                 }
             }
         }
+        //not possible
         return false;
     }
 };
