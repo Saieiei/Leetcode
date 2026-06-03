57ef291ce41cea3bddc4ac79a6ad5931ad5de7c2
Sairudra More <sairudra60@gmail.com>
2026-06-03 14:58:28 +0530

    Time: 0 ms (100%), Space: 63.6 MB (47.96%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -2,16 +2,28 @@ class Solution {
 public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n = arr.size();
-        //brute force N
-        //space optimised O(1)
-        int max = 0;
-        int ans = 0;
-        for(int i=0; i<n; i++){
-            if(arr[i] > max){
-                ans = i;
-                max = arr[i];
+        //Binmary search N
+        //byheart
+        //once we get the mid ele, check if its next ele is big
+        //if so shift low to mid+1
+        //if not shift high top mid
+        //finally return low
+        int low = 0;
+        int high = n-1;
+
+        while(low < high){ //=
+            int mid = low + (high - low)/2;
+            int midEle = arr[mid];
+            //now check if the next ele to mid is greater than mid
+            if(arr[mid]<arr[mid+1]){
+                //if so, shit low to mid+1
+                low = mid+1;
+            }
+            else{
+                //move high to mid
+                high = mid;
             }
         }
-        return ans;
+        return low;
     }
 };
