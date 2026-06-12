47c9a175b389aa5cb9d08eb91d40e4be445a0df4
Sairudra More <sairudra60@gmail.com>
2026-06-12 09:23:12 +0530

    Time: 12 ms (39.13%), Space: 35.8 MB (92.21%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-
-        //method 2: using 2 pointers, l(low) and h(high), since it is sorted
-
-        int l=0, h=arr.size()-1;
-        while(h-l>=k) //this gives u the required elements range,  that is k 
-        {
-            if(x-arr[l]>arr[h]-x) // dont put >= sign
-//the diffence made by lower elemnt of the sorted array is more, this means the element on the right side(higher element) has a low difference, which means we should not touch the higher element of the array as we need only lower differences element, so we should move the lower element to get more low diference
-            {
-                l++;
-            }
-            else
-            {
-                h--;
-            }
-        }
-        vector<int> ans;
-        for(int i=l;i<=h;i++)
-        {
-            ans.push_back(arr[i]);
-        }
-
+        //custom sort class
+        auto myCmp = [&](int a, int b){
+            int diffa = abs(x-a);
+            int diffb = abs(x-b);
+            return (diffa < diffb) || (diffa == diffb && a<b);
+        };
+        //method 1 custom sorting NlogN
+        //1st lets apply the custom sorting
+        sort(arr.begin(), arr.end(), myCmp);
+        //now take the 1st k elemnts from the sorted arr
+        vector<int>ans(arr.begin(), arr.begin() + k);
+        //sort the ans again because the orginal array was in sorted condition
+        sort(ans.begin(), ans.end());
         return ans;
     }
 };
