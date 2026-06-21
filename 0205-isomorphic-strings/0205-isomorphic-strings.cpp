74377401c057ae694bf3525c861e0ccad06db001
Sairudra More <sairudra60@gmail.com>
2026-06-21 20:54:32 +0530

    Time: 0 ms (100%), Space: 9.4 MB (55.99%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,45 +1,30 @@
-//week5_ass_5
-//mapping
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        //we will use mapping method
-        //if u do not know what is mapping, it is basically a array of numbers all(256) initialized to 0
-            // ok but how do we map 2 strings with a array?
-                //the 1st string characters will act as the index of the arry
-                //the 2nd string charcters will act as the values that will be mapped to that index with the help of charcters
-        
-        //ok so now uk what is mapping
-        //After mapping the charcters we also have to make sure that we dont map the characters to some new charcters if its alreay mapped
-            //how do we do that?
-                //this will also use the concept of mapping
-                //we have to create a new array[256], the characters of 2nd string will act as index ans the value will be updated to 1 if already done
-        
-        //in the end we will use the mapping that we had created and check if we r able to get the characters of t using the mapping
-            //if so then true, else false
-        
-        int mapping[256]={0};
-        int checker[256]={0};
-
-        for(int i=0;i<s.size();i++)
-        {
-            if(checker[t[i]]==0 && mapping[s[i]]==0  ) //not mapped yet
-            {
-                mapping[s[i]]=t[i]; //update the mapping
-                checker[t[i]]=1; //update the checker
-            }
+        //brute force bijection mapping
+        //N, U
+        //bc
+        if(s.length() != t.length()){
+            return false;
         }
-        
-        //testing using mapping
-        for(int i=0;i<s.size();i++)
-        {
-            char ch=mapping[s[i]];
-            if(ch!=t[i])
-            {
+        //create 2 hash maps
+        unordered_map<char, char> StoT;
+        unordered_map<char, char> TtoS;
+        //if that letter is present in the map, 
+        //then check if its mapped correctly or not
+        for(int i=0; i<s.length(); i++){
+            char Sch = s[i];
+            char Tch = t[i];
+            if(StoT.count(Sch) == true && StoT[Sch] != Tch){
                 return false;
             }
+            if(TtoS.count(Tch) == true && TtoS[Tch] != Sch){
+                return false;
+            }
+            //not mapped yet?, map it
+            StoT[Sch] = Tch;
+            TtoS[Tch] = Sch;
         }
         return true;
-        
     }
 };
