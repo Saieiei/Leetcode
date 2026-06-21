0da6ad2f0d3c27eaaf880eaa03658674d23726d7
Sairudra More <sairudra60@gmail.com>
2026-06-21 17:26:22 +0530

    Time: 0 ms (100%), Space: 9.8 MB (49.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //Brite force - sorting
-        //NlogN, 
+        //hashmap method
+        //N, N
         //bc
         if(s.length() != t.length()){
             return false;
         }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        //return (s==t);
+        unordered_map<char, int>mp;
+        //populate the freq and depopulate the freq
         for(int i=0; i<s.length(); i++){
-            if(s[i] != t[i]){
+            mp[s[i]]++;
+            mp[t[i]]--;
+        }
+        //in the end we should all the freq as 0 only
+        for(const pair<const char, const int>& it: mp){
+            if(it.second != 0){
                 return false;
             }
         }
