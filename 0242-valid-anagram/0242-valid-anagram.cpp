d71a5b458e6ee8fbc709e90566b2be0266ed3c4e
Sairudra More <sairudra60@gmail.com>
2026-06-21 17:20:24 +0530

    Time: 7 ms (19.11%), Space: 9.7 MB (75.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -2,12 +2,14 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         //Brite force - sorting
+        //NlogN, 
         //bc
         if(s.length() != t.length()){
             return false;
         }
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
+        //return (s==t);
         for(int i=0; i<s.length(); i++){
             if(s[i] != t[i]){
                 return false;
