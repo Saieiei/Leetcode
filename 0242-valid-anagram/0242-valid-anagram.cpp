075a125896f8b8da21196654c21b453effb57c58
Sairudra More <sairudra60@gmail.com>
2026-06-21 17:30:02 +0530

    Time: 8 ms (7.07%), Space: 9.6 MB (92.39%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        //hashmap method
+        //vector method - space optimized
         //N, N
         //bc
         if(s.length() != t.length()){
             return false;
         }
-        unordered_map<char, int>mp;
+        //since it all lowercase we can use vectors of size 26
+        vector<int>freq(26, 0);
         //populate the freq and depopulate the freq
         for(int i=0; i<s.length(); i++){
-            mp[s[i]]++;
-            mp[t[i]]--;
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
         }
         //in the end we should all the freq as 0 only
-        for(const pair<const char, const int>& it: mp){
-            if(it.second != 0){
+        for(const int& frequencyNo: freq){
+            if(frequencyNo != 0){
                 return false;
             }
         }
