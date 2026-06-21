bc8a02f9d1d7e3e5f7308ae4dccc643f7a29954d
Sairudra More <sairudra60@gmail.com>
2026-06-21 21:08:10 +0530

    Time: 0 ms (100%), Space: 9.3 MB (55.99%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        //brute force bijection mapping
+        //bijection mapping - space optimized
+        //last seen index approach
         //N, U
         //bc
         if(s.length() != t.length()){
             return false;
         }
-        //create 2 hash maps
-        unordered_map<char, char> StoT;
-        unordered_map<char, char> TtoS;
+        //create 2 vectors, unicode
+        vector<int> Svec(256, 0);
+        vector<int> Tvec(256, 0);
         //if that letter is present in the map, 
         //then check if its mapped correctly or not
         for(int i=0; i<s.length(); i++){
             char Sch = s[i];
             char Tch = t[i];
-            if(StoT.count(Sch) == true && StoT[Sch] != Tch){
+            //if th evlaues r not same, then it doesnt follow the pattern
+            if(Svec[Sch] != Tvec[Tch]){
                 return false;
             }
-            if(TtoS.count(Tch) == true && TtoS[Tch] != Sch){
-                return false;
-            }
-            //not mapped yet?, map it
-            StoT[Sch] = Tch;
-            TtoS[Tch] = Sch;
+            //record the last visited index
+            //we do + 1 because we diff it from innitial 0
+            Svec[Sch] = i + 1;
+            Tvec[Tch] = i + 1;
         }
         return true;
     }
