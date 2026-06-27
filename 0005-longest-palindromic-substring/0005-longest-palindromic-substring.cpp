8e20e6500b68ec643ef8a1b829c68c9a40f16ec0
Sairudra More <sairudra60@gmail.com>
2026-06-27 21:32:39 +0530

    Time: 71 ms (42.47%), Space: 165 MB (17.01%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,33 +1,32 @@
-//M1 recursion
-//we will find each possible substring
-//then we will use recursion to find out if its palindrome or not
-
 class Solution {
 public:
-    int start = 0;
-    int maxLen = 1;
-    bool recursion(string& s, int i, int j){
-        //bc
-        if(i >= j) return true;
-
-        bool flag = false;
-        if(s[i] == s[j]) {
-            flag = recursion(s, i + 1, j - 1);
+    int expandFromCentre(string s, int left, int right){
+        while(left >= 0 && right < s.length() && s[left] == s[right]){
+            left--;
+            right++;
         }
-
-        if(flag){
-            int currLen = j - i + 1;
-            if(currLen > maxLen){
-                maxLen = currLen;
-                start = i;
-            }
-        }
-        return flag;
+        return right - left - 1;
     }
     string longestPalindrome(string s) {
-        for(int i = 0; i <= s.length() - 1; i++){
-            for(int j = i; j <= s.length() - 1; j++){
-                recursion(s, i, j);     
+        int n = s.length();
+        //expland from centre
+        //N
+        //traverse through the chars in s
+        //try to expand from the centre
+        //we have to find len for both even and odd
+        //after we get the maxLen and the startting point
+        //we can get the substr
+        int start = 0;
+        int maxLen = INT_MIN;
+        int currMaxLen = INT_MIN;
+        for(int i=0; i<n; i++){
+            int len1 = expandFromCentre(s, i, i);
+            int len2 = expandFromCentre(s, i, i+1);
+            currMaxLen = max(len1, len2);
+            if(currMaxLen > maxLen){
+                maxLen = currMaxLen;
+                //its somewhat like a formula
+                start = i - (currMaxLen -1)/2;
             }
         }
         return s.substr(start, maxLen);
