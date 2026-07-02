ed319cc42343a198cda059fb8a9a74fa1e459035
Sairudra More <sairudra60@gmail.com>
2026-07-02 22:12:21 +0530

    Time: 4 ms (77.8%), Space: 14 MB (75.17%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.cpp
@@ -1,40 +1,29 @@
 class Solution {
 public:
     string removeDuplicates(string s, int k) {
-        //vectors
+        int n = s.size();
+        //2 pointers, in place but we will use a extra space
         //N, N
-        vector<pair<char, int>> countVec; //char, freq
+        int slow = 0;
+        int fast = 0;
+        vector<int> freq(n, 0);
         //traverse through the string
-        for(const char& ch: s){
-            //check is it matched with the previous 1
-            if(!countVec.empty() && countVec.back().first == ch){
-                //increase the freq
-                countVec.back().second++;
-
-                //since freq has increased, check if we met the condition
-                if(countVec.back().second == k){
-                    //pop it
-                    countVec.pop_back();
+        for(fast = 0; fast<n; fast++, slow++){
+            //make the fast char to slow char
+            s[slow] = s[fast];
+            //check if we have to updat ethe freq
+            if(slow > 0 && s[slow] == s[slow -1]){
+                freq[slow] = freq[slow - 1] + 1;
+                //if the freq is meant we have to decrease slow
+                if(freq[slow] == k){
+                    slow = slow - k;
                 }
             }
             else{
-                //empty or not matching
-                countVec.push_back({ch, 1});
+                //new streak
+                freq[slow] = 1;
             }
         }
-        //make the string now
-        string ans = "";
-        //pop it 1 by 1 then reverse it
-        for(const pair<char, int>& it: countVec){
-            char ch = it.first;
-            int freq = it.second;
-            while(freq){
-                //very imp, do not do ans = ans + ch
-                //u will go into memory issues
-                ans += ch;
-                freq--;
-            }
-        }
-        return ans;
+        return s.substr(0, slow);
     }
 };
