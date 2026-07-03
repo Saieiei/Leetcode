b03a1b2414b0089c632497764cbd7a2b84041354
Sairudra More <sairudra60@gmail.com>
2026-07-03 19:57:41 +0530

    Time: 0 ms (100%), Space: 8.5 MB (55.55%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -2,50 +2,63 @@ class Solution {
 public:
     string reorganizeString(string s) {
         int n = s.size();
-        //even odd method
-        //N
-        //take teh char with the largest freq
-        //place it all in the even index
-        //fill th erest with other chars
-        //if it excees the s size, then fill up the odd indexes
-        //the matimatical catch: for a string of len 5, max freq should be 3
-        //X freq > n+1/2
+        //priority queue (maxHeap)
+        //take the top 2 freq chars
+        //put them adjacent
+        //decrease  their freq
+        //push back into the priorty queue
+        //repeat until its empty
+        //N N
         //populate the freq
         vector<int> freq(26, 0);
-        int maxFreq = 0;
-        int maxFreqChar = 0;
-        //traverse through the string
+        //travese through the string
         for(char ch: s){
             freq[ch - 'a']++;
-            if(freq[ch - 'a'] > maxFreq){
-                maxFreq = freq[ch - 'a'];
-                maxFreqChar = ch - 'a';
+        }
+        //poplate the pq
+        priority_queue<pair<int, char>> pq; //freq, char
+        //becase, it will sort based on .first
+        for(int i=0; i<26; i++){
+            if(freq[i]>0){
+                //push it in pq
+                pq.push({freq[i], i+'a'});
             }
         }
-        //bc, check if it satisfies mathematical prop
+        //mathematical property
+        //in a 5 chars, only 3 can be distinct
+        //take the max freq
+        int maxFreq = pq.top().first;
         if(maxFreq > (n+1)/2){
             //not possible
             return "";
         }
-        //now take the max char freq and put it all in the even index
+        //start filling now
+        //always make sure 2 remains
         int index = 0;
-        while(freq[maxFreqChar]>0){
-            s[index] = maxFreqChar + 'a';
-            index += 2;
-            freq[maxFreqChar]--;
-        }
-        //it has to get over before reached the end or might have reached the end
-        //fill up the remaining with the rest of the freq
-        for(int i=0; i<26; i++){
-            while(freq[i] > 0){
-                //if it has reached the end, then continue fro index 1
-                if(index >= n){
-                    index = 1;
-                }
-                s[index] = i + 'a';
-                index += 2;
-                freq[i]--;
+        while(pq.size()>=2){
+            pair<int, char> p1 = pq.top();
+            pq.pop();
+            pair<int, char> p2 = pq.top();
+            pq.pop();
+            //place them
+            s[index] = p1.second;
+            p1.first--;
+            index++;
+            s[index] = p2.second;
+            p2.first--;
+            index++;
+            //push back in only if more freq remain
+            if(p1.first > 0){
+                pq.push(p1);
             }
+            if(p2.first > 0){
+                pq.push(p2);
+            }
+        }
+        //if something remains because of odd
+        if(!pq.empty()){
+            s[index] = pq.top().second;
+            pq.pop();
         }
         return s;
     }
