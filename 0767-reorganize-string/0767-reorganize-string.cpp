a28217de38b505b5c51492371a6ba741c78257f0
Sairudra More <sairudra60@gmail.com>
2026-07-03 17:45:28 +0530

    Time: 0 ms (100%), Space: 8.4 MB (79.45%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,57 +1,50 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        //for this to work we need a hash
-        //hash will store all the number of times a charcter has occured
-        //then will find the most occuring element
-        //we will place that in alternate places and if it is exceeding the length of the string(s) then it is not possible return null ("")
-        //if it was possble to store the most repeating elements in alternate places then we will continue to place the rest of the chars in the 
-            //available positions
-
-        //creating hash
-        int hash[26]={0};
-
-        for(auto ch:s) //updating the frequency of each char using hash table
-        {
-            int position=ch-'a';
-            hash[position]=hash[position]+1;
-        }
-
-        //fiding the maxium frequenct charcter
-        int max_freq=INT_MIN;
-        char max_freq_char;
-        for(int i=0;i<26;i++) //for every element in hash
-        {
-            if(hash[i]>max_freq)
-            {
-                max_freq=hash[i];
-                max_freq_char=i+'a';
+        int n = s.size();
+        //even odd method
+        //N
+        //take teh char with the largest freq
+        //place it all in the even index
+        //fill th erest with other chars
+        //if it excees the s size, then fill up the odd indexes
+        //the matimatical catch: for a string of len 5, max freq should be 3
+        //X freq > n+1/2
+        //populate the freq
+        vector<int> freq(26, 0);
+        int maxFreq = 0;
+        int maxFreqChar = 0;
+        //traverse through the string
+        for(char ch: s){
+            freq[ch - 'a']++;
+            if(freq[ch - 'a'] > maxFreq){
+                maxFreq = freq[ch - 'a'];
+                maxFreqChar = ch - 'a';
             }
         }
-
-        //now lets allocate the max_freq_char in alternate position
-        int index=0;
-        while(index<s.size() && max_freq>0)
-        {
-            s[index]=max_freq_char;
-            index=index+2;
-            max_freq--;
+        //bc, check if it satisfies mathematical prop
+        if(maxFreq > (n+1)/2){
+            //not possible
+            return "";
         }
-
-        //if we were not able to allocate the max_freq in alternate positions then its not possible
-        if(max_freq>0) return "";
-        else //all have been succesfully allocated, now we can procede allocating the rest
-        {
-             hash[max_freq_char-'a']=0;
-            for(int i=0;i<26;i++)
-            {
-                while(hash[i]>0)
-                {
-                    index=index>=s.size()?1:index;
-                    s[index]=i+'a';
-                    hash[i]--;
-                    index=index+2;
+        //now take the max char freq and put it all in the even index
+        int index = 0;
+        while(freq[maxFreqChar]>0){
+            s[index] = maxFreqChar + 'a';
+            index += 2;
+            freq[maxFreqChar]--;
+        }
+        //it has to get over before reached the end or might have reached the end
+        //fill up the remaining with the rest of the freq
+        for(int i=0; i<26; i++){
+            while(freq[i] > 0){
+                //if it has reached the end, then continue fro index 1
+                if(index >= n){
+                    index = 1;
                 }
+                s[index] = i + 'a';
+                index += 2;
+                freq[i]--;
             }
         }
         return s;
