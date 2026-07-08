66315802be1c7f9cffa3b7a15120e7cd03318773
Sairudra More <sairudra60@gmail.com>
2026-07-08 09:45:13 +0530

    Time: 0 ms (100%), Space: 8.4 MB (43.15%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -1,23 +1,47 @@
 class Solution {
 public:
     string customSortString(string order, string s) {
-        //NlogM
-        //use custome sorting lambda function
-        //there will be no statiuc problem
-        //this is better than global static fun because
-        //it avoids the race condition cuzed by the global static var
-        //creat the ranking system for the custom lambda sorting
-        //every1 ranked the lowest 0th postion = high, 25th postion = low
-        vector<int> ranks(26, 26);
-        //give them the ranks based on the order
-        for(int i=0; i<order.size(); i++){
-            char chOrder = order[i];
-            ranks[chOrder - 'a'] = i;
+        //N 
+        //we will append on a new string based on the order
+        //let scollect all the chars thats there in s
+        vector<int> freq(26, 0);
+        //update the freq
+        for(char ch: s){
+            freq[ch - 'a']++;
         }
-        //custom lambda sorting
-        sort(s.begin(), s.end(), [&](const char& a, const char& b){
-            return ranks[a - 'a'] < ranks[b - 'a'];
-        });
-        return s;
+        string ans = "";
+        //just aa small optimization on space alloc
+        ans.reserve(s.size());
+        //now we will append the s based on order
+        for(char ch: order){
+            //check if that frq > 0
+            if(freq[ch - 'a'] > 0){
+                //if so append it 
+                ans.append(freq[ch - 'a'], ch);
+                //or
+                //for(int i=0; i<freq[ch - 'a']; i++){
+                    //ans += ch;
+                //}
+            }
+            //after using all the freq of the ch, mark it to 0
+            freq[ch - 'a'] = 0;
+        }
+
+        //now after we have arranged the chars of order, 
+        //we have to also append the chars of s
+        for(char ch: s){
+            //check if that frq > 0
+            if(freq[ch - 'a'] > 0){
+                //if so append it 
+                ans.append(freq[ch - 'a'], ch);
+                //or
+                //for(int i=0; i<freq[ch - 'a']; i++){
+                    //ans += ch;
+                //}
+            }
+            //after using all the freq of the ch, mark it to 0
+            freq[ch - 'a'] = 0;
+        }
+        return ans;
     }
 };
