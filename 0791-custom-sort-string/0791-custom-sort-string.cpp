e5c7cd110a7d60cf86f60abe0c452cd0ca4281fd
Sairudra More <sairudra60@gmail.com>
2026-07-08 09:16:21 +0530

    Time: 0 ms (100%), Space: 8.3 MB (69.87%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.cpp
+++ 0791-custom-sort-string/0791-custom-sort-string.cpp
@@ -6,13 +6,22 @@ public:
     }
     string customSortString(string order, string s) {
         //NlogM
-        //use custome sorting function
-        //make sure of the static problem
-        //but if u use a globa variable and a static function
-        //this is bad because it will keep chnaging and may cause race cnditions
-        globalOrder = order;
-        //custome sorting
-        sort(s.begin(), s.end(), mycmp);
+        //use custome sorting lambda function
+        //there will be no statiuc problem
+        //this is better than global static fun because
+        //it avoids the race condition cuzed by the global static var
+        //creat the ranking system for the custom lambda sorting
+        //every1 ranked the lowest 0th postion = high, 25th postion = low
+        vector<int> ranks(26, 26);
+        //give them the ranks based on the order
+        for(int i=0; i<order.size(); i++){
+            char chOrder = order[i];
+            ranks[chOrder - 'a'] = i;
+        }
+        //custom lambda sorting
+        sort(s.begin(), s.end(), [&](const char& a, const char& b){
+            return ranks[a - 'a'] < ranks[b - 'a'];
+        });
         return s;
     }
 };
