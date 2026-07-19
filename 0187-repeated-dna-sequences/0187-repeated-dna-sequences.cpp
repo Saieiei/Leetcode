c2bc49e892c0ef89280bd26790488697f68a9be9
Sairudra More <sairudra60@gmail.com>
2026-07-19 18:11:14 +0530

    Time: 35 ms (87.14%), Space: 26.7 MB (5.82%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -3,44 +3,32 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         const int n = s.size();
         const int windowSize = 10;
-        //M2 - sorting 
-        //nlogn
-        //if we take each block of 10 and reorder thenm in sorted format
-        //then the duplicates will be adjacent to each other
+        //M3 hashing (freq)
+        //N, N
+        //we will push in the ans only if the freq >=2
         //bc
-        //if length is less than 10
-        if(n<10){
+        if(n <= windowSize){
             //not possible
             return {};
         }
-        //if the legtn is equal to 10
-        if(n==10){
-            //still not possible
-            return {};
+        unordered_map<string, int> freqs;
+        //make every possible substring and update the freq
+        for(int i=0; i <= (n - windowSize); i++){
+            string substring = s.substr(i, windowSize);
+            //update the freq
+            freqs[substring]++;
         }
-        vector<string>possibleSubStringsSorted;
-        //push all the possible substrings
-        for(int i=0; i<=(n-windowSize); i++){
-            string SubString = s.substr(i, 10);
-            possibleSubStringsSorted.push_back(SubString);
-        }
-        //now sort the entier vector
-        sort(possibleSubStringsSorted.begin(), possibleSubStringsSorted.end());
-
         vector<string> ans;
-        //now traverse through this sorting string and find out the dup adjacent once
-        //if they r not inserted in the ans already, then push it in 
-        for(int i=1; i<possibleSubStringsSorted.size(); i++){
-            //check if the prev1 is duplicate
-            if(possibleSubStringsSorted[i] == possibleSubStringsSorted[i-1]){
-                //we found a matching 1 but pefore pushing it in ans, check its back once
-                //for example ther can be 3 duplicates instead of 2
-                if(ans.empty() || ans.back() != possibleSubStringsSorted[i]){
-                    //its safe to push it in now
-                    ans.push_back(possibleSubStringsSorted[i]);
-                }
-            }
+        //now traverse through the freq of the unordered_map 
+        //and push in the substrings whos frequency is >=2
+        for(const pair<const string, const int>& it: freqs){
+            string subString = it.first;
+            int freq = it.second;
+            //check if freq >=2
+            if(freq >= 2){
+                ans.push_back(subString);
+            } 
         }
         return ans;
-    }
+    }   
 };
