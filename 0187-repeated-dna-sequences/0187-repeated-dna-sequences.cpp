25364a00f708d68d2aa5ad0a126265051006e27b
Sairudra More <sairudra60@gmail.com>
2026-07-19 18:31:12 +0530

    Time: 39 ms (76.56%), Space: 22.4 MB (83%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -3,31 +3,40 @@ public:
     vector<string> findRepeatedDnaSequences(string s) {
         const int n = s.size();
         const int windowSize = 10;
-        //M3 hashing (freq)
-        //N, N
-        //we will push in the ans only if the freq >=2
+        //M4 2 sets 
+        //N, N (time and space optimized)
+        //1 set is for seeing for teh 1st time
+        //another set if for repeated
         //bc
         if(n <= windowSize){
             //not possible
             return {};
         }
-        unordered_map<string, int> freqs;
-        //make every possible substring and update the freq
-        for(int i=0; i <= (n - windowSize); i++){
-            string substring = s.substr(i, windowSize);
-            //update the freq
-            freqs[substring]++;
-        }
+        //2 sets
+        unordered_set<string> seen;
+        unordered_set<string> reported;
         vector<string> ans;
-        //now traverse through the freq of the unordered_map 
-        //and push in the substrings whos frequency is >=2
-        for(const pair<const string, const int>& it: freqs){
-            string subString = it.first;
-            int freq = it.second;
-            //check if freq >=2
-            if(freq >= 2){
-                ans.push_back(subString);
-            } 
+        //create every possible sub arrays
+        for(int i=0; i<=(n - windowSize); i++){
+            string subString = s.substr(i, windowSize);
+            //check if we r seeing this substring for the 1st time ever
+            if(seen.count(subString) == 0){
+                //simply push it in as visisted
+                seen.insert(subString);
+            }
+            else{
+                //it was already seen, so we have to check if this is a duplicate
+                //or too many duplicates
+                if(reported.count(subString) == 0){
+                    //1st duplicate only. we can push it in ans and mark it as visited
+                    ans.push_back(subString);
+                    reported.insert(subString);
+                }
+                else{
+                    //its freq is more than 2, we can simply ignore it
+                    continue;
+                }
+            }
         }
         return ans;
     }   
