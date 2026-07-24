66c436886bd924244c503866765e4558f03386ad
Sairudra More <sairudra60@gmail.com>
2026-07-24 19:39:50 +0530

    Time: 0 ms (100%), Space: 17.9 MB (5.02%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,58 +1,46 @@
 class Solution {
 public:
-    string updatePrefix(const string& prefix, const string& currentString){
-        //traverse through the string until the 2 chars r not the same anymore
-        //a smart move will be traversing though the string whoes length is the smallest
-        //because we cannot have a prefix whose length is greater than the smallest string
-        int prefixLength = static_cast<int>(prefix.size());
-        int currentStringLength = static_cast<int>(currentString.size());
-        int minLength = min(prefixLength, currentStringLength);
-        //initially lets keep the updated prefix as the string whose length is smaller
-        //u can use ternary operator if needed
-        string updatedPrefix = "";
-        if(prefixLength > currentStringLength){
-            updatedPrefix = currentString;
-        }
-        else{
-            updatedPrefix = prefix;
-        }
-        for(int i = 0; i<minLength; i++){
-            if(prefix[i] != currentString[i]){
-                updatedPrefix = updatedPrefix.substr(0, i);
-                break;
-            }
-        }
-        return updatedPrefix;
-    }
     string longestCommonPrefix(vector<string>& strs) {
         int n = static_cast<int>(strs.size());
-        //horizontal scalling
-        //u take the 1st string and superimpose on another string
-        //u get the prefix and take that prefix and super impose on the string
-        //keep doing tuntil u all used upp all the strings in the strs vector
-        //return the prefix
+        //vertical scalling
+        //we take the 1st string as the prefix
+        //then take the 1st letter and compare it every other string
+        //if that letter is present in all we take the next letter of the prefix
+        //and compare it with every other strings at the same position of the letter
+        //if 1 of them breaks either because the length of the string is short 
+        //(teh whole string is the prefix, return it)
+        //or the letters didint match, then return the substring based on the index so far
 
         //bc
         if(strs.empty()){
             return "";
         }
 
+        //take the 1st strign as the prefix
         string prefix = strs[0];
-        //traverse through all the strings
-        for(int i=1; i<n; i++){
-            string currentString = strs[i];
-            //bc
-            //prefix of an empty string will be empty only
-            if(currentString == ""){
-                return "";
-            }
-            prefix = updatePrefix(prefix, currentString);
-            //if ur prefix is empty after superimposing, 
-            //then no point comparing with other strings
-            if(prefix == ""){
-                return "";
+        //now we have to go through its chars and compare it with every string, 
+        //until something break
+        int prefixLength = static_cast<int>(prefix.size());
+        for(int prefixStringIndex = 0; prefixStringIndex < prefixLength; prefixStringIndex++){
+            int prefixChar = prefix[prefixStringIndex];
+            //now we have to compare it with other strings until something breaks
+            for(int row = 1; row<n; row++){
+                string currentString = strs[row];
+                //if the currentString length is less than the prefix index
+                //then the currentString is thre prefix to be returned
+                int currentStringLength = static_cast<int>(currentString.size());
+                if(currentStringLength < prefixStringIndex){
+                    return currentString;
+                }
+                //if the chars dont match, then return the substring
+                char currentStringChar = currentString[prefixStringIndex];
+                if(currentStringChar != prefixChar){
+                    return prefix.substr(0, prefixStringIndex);
+                }
             }
         }
-        return prefix;
+        //if nothing breaks till now, which means all the strings in the str
+            //are the same strings or there is only 1 string in the vec
+            return prefix;
     }
 };
