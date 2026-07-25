d509022a71ca8167ca678dad1cad00470db06425
Sairudra More <sairudra60@gmail.com>
2026-07-25 19:59:03 +0530

    Time: 3 ms (33.94%), Space: 10.8 MB (22.11%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.cpp
@@ -1,42 +1,30 @@
 class Solution {
 public:
-bool fun(char c){
-    c=tolower(c);
-    return (c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
-}
     string reverseVowels(string s) {
-        int start=0;
-        int end=s.length()-1;
-        string st;
-
-
-        while(start<end){
-            if(!fun(s[start])) //false false is true
-            {
-                start++;
-                continue;
-                
-                  }
-           if(!fun(s[end])) //false false is true
-            {  
-                end--;
-                continue;
-                
-                
+        int n = static_cast<int>(s.length());
+        //we can do this with simply 2 pointers
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int leftPointer = 0;
+        int rightPointer = n-1;
+        //we have to keep on swapping until they become the same index
+        while(leftPointer<rightPointer){
+            //keep on moving left until we get a proper vowel
+            while(leftPointer<n && (vowels.find(s[leftPointer]) == vowels.end())){
+                leftPointer++;
             }
-           else{ //both vowels
-            
-                swap(s[start],s[end]);
-                start++;
-                end--;
-           }
-
-            
+            //keep on moving right until we get a proper vowel
+            while(rightPointer>0 && (vowels.find(s[rightPointer]) == vowels.end())){
+                rightPointer--;
+            }
+            //its time to swap
+            if(leftPointer<rightPointer){
+                char temp = s[rightPointer];
+                s[rightPointer] = s[leftPointer];
+                s[leftPointer] = temp;
+                leftPointer++;
+                rightPointer--;
+            }
         }
-
-
-return s;
-        
-        
+        return s;
     }
 };
