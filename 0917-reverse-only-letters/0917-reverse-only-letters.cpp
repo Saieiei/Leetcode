a945fe5bf64182505aa3433466bef0de25e3e072
Sairudra More <sairudra60@gmail.com>
2026-07-25 20:45:39 +0530

    Time: 0 ms (100%), Space: 8.2 MB (3.45%) - LeetHub

--- 0917-reverse-only-letters/0917-reverse-only-letters.cpp
+++ 0917-reverse-only-letters/0917-reverse-only-letters.cpp
@@ -1,27 +1,28 @@
-//week5_ass_2
-//we will be using 2 pointers approach
-//l will be in the begining and h will be in the end
 class Solution {
 public:
     string reverseOnlyLetters(string s) {
-        int len=s.size();
-        int l=0;
-        int h=len-1;
-        while(l<h)
-        {
-            if(isalpha(s[l]) && isalpha(s[h]))
-            {
-                swap(s[l], s[h]);
-                l++;
-                h--;
+        int n = static_cast<int>(s.length());
+        //simply 2 pointers
+        int leftPointer = 0;
+        int rightPointer = n-1;
+        //we have to keep swapping until they r at the same index;
+        while(leftPointer < rightPointer){
+            //we have to keep moving front if its not a alpha
+            while(leftPointer<n && (!isalpha(s[leftPointer]))){
+                leftPointer++;
             }
-            else if(!isalpha(s[l]))
-            {
-                l++;
+             //we have to keep moving back if its not a alpha
+            while(rightPointer>0 && (!isalpha(s[rightPointer]))){
+                rightPointer--;
             }
-            else
-            {
-                h--;
+            //its time to swap now
+            if(leftPointer < rightPointer){
+                char temp = s[leftPointer];
+                s[leftPointer] = s[rightPointer];
+                s[rightPointer] = temp;
+                //update the indexes
+                leftPointer++;
+                rightPointer--;
             }
         }
         return s;
