d21b67126dbecab14a4b01234bebe8603b02aeb7
Sairudra More <sairudra60@gmail.com>
2026-08-02 10:57:58 +0530

    Time: 0 ms (100%), Space: 10.7 MB (47.38%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,33 +1,31 @@
-//M3
-//1st we will try to solve this using recursion+Tabu
-//we will solve it using include and exclude method
 class Solution {
 public:
-
-    int solveUsingRecurssionTabu(vector<int>& nums, int index){
-        //basecase and create dp
-        int n = nums.size();
-        vector<int>dp(n+1, -1);
-        //if(index >= nums.size()) return 0;
-        dp[n] = 0; 
-
-        //bottom up and convertion
-        for(int i=n-1; i>=0; i--){
-            
-            int temp = 0;
-            if(i == n-1) temp = 0; //will go out of bounds or else
-            else temp = dp[i+2];
-
-            int include = nums[i] + temp;
-            int exclude = 0 + dp[i+1];
-            dp[i] = max(include, exclude);
+    int recursion(vector<int>& nums, int index, const int n, vector<int>& dp){
+        //bc
+        if(index >= n){
+            return 0;
         }
-        
-        return dp[0];
+        //return if already found
+        if(dp[index] != -1){
+            return dp[index];
+        }
+        //we have 2 options, either include the house and follow the rest
+        //else exclude the house and follow the rest
+        //include
+        int include = nums[index] + recursion(nums, index+2, n, dp);
+        //exclude
+        int exclude = 0 + recursion(nums, index+1, n, dp);
+        //returning dp
+        dp[index] = max(include, exclude);
+        return dp[index];
     }
-
     int rob(vector<int>& nums) {
-        int index=0;
-        return solveUsingRecurssionTabu(nums, index);
+        int n = nums.size();
+        //Memoization (check if already found, returning dp)
+        //include exclude principal
+
+        int index = 0;
+        vector<int>dp(n+5, -1);
+        return recursion(nums, index, n, dp);
     }
 };
