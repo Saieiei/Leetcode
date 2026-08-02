33e08c0a825f60b6a6c8e828c5c99595e3b2810c
Sairudra More <sairudra60@gmail.com>
2026-08-02 11:55:48 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.34%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,26 +2,34 @@ class Solution {
 public:
     int recursion(vector<int>& nums, int index, const int n){
         //move dp inside, do not intialize this to -1
-        vector<int> dp(n+5, 0);
+        //vector<int> dp(n+5, 0);
         //bc, but anyways, we have it defined already
-        dp[n] = 0;
+        //dp[n] = 0;
+        int curr = 0, next1 = 0, next2 = 0;
         //create the loop
         for(int index = n-1; index>=0; index--){
             //we have 2 options, either include the house and follow the rest
             //else exclude the house and follow the rest
             //include
-            int include = nums[index] + dp[index+2];
+            int include = nums[index] + next2;
             //exclude
-            int exclude = 0 + dp[index+1];
+            int exclude = 0 + next1;
             //returning dp
-            dp[index] = max(include, exclude);
+            curr = max(include, exclude);
+
+            //exchange, imp
+            next2 = next1;
+            next1 = curr;
         }
-        return dp[0];
+        return curr;
     }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        //Tabulation 
-        //(move dp inside, change BC, create a loop in the oppo dir, remove recursion, return opo extreme index)
+        //SO
+        ////to get the value of a dp[index] we require, dp[index+1] and dp[index+2]
+        //we, do not need the rest
+        //so simply curr=dp[index], next1=dp[index+1], next2=dp[index+2], 
+        //finally exchange as we pass through the for loop
         //include exclude principal
 
         int index = 0;
