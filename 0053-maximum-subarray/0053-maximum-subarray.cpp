522c7b9e30394077f33424714a1d4c71c73c1dab
Sairudra More <sairudra60@gmail.com>
2026-08-12 19:15:21 +0530

    Time: 31 ms (4.44%), Space: 73 MB (15.17%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,23 +1,50 @@
+//M1 recursion (DnC)
+//This is the most easiest method
+//find the left sum, find the right sum, find the crossum, then do recursion with the mid
+//return the max 
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int n = nums.size();
-        //kadane algo O(N)
-        //main: if sum < 0 , reset it to 0
-        //alsways maSum in the begining should be nums[0]
-        int maxSum = nums[0];
+
+    int crossMaxFun(vector<int>& nums, int start, int mid, int end){
+        //we need to get the best leftSum and best rightSum from mid
+        //                         <----|---->
         int currSum = 0;
-        //traverse
-        for(int i=0; i<n; i++){
+        int bestLeftSum = INT_MIN;
+        for(int i = mid; i >= start; i--){
             currSum += nums[i];
-            maxSum = max(maxSum, currSum);
-            //negavtive check
-            if(currSum < 0){
-                //reset it to 0
-                currSum = 0;
-            }
+            bestLeftSum = max(bestLeftSum, currSum);
         }
-        return maxSum;
-        
+        currSum = 0;
+        int bestRightSum = INT_MIN;
+        for(int i = mid+1; i <= end; i++){
+            currSum += nums[i];
+            bestRightSum = max(bestRightSum, currSum);
+        }
+        int crossSum = bestLeftSum + bestRightSum;
+        return crossSum;
+    }
+
+    int recursion(vector<int>& nums, int start, int end){
+        //the ans will bre the only element left out (max value)
+        //bc, 
+        if(start == end){
+            return nums[start];
+        }
+        //we need mid to divide
+        int mid = start + (end-start)/2;
+        //I trust the recursion to get the left max
+        int leftMax = recursion(nums, start, mid);
+        //I trust the recursion to get the right max
+        int rightMax = recursion(nums, mid+1, end);
+        //we have to get the crossMax as well
+        int crossMax = crossMaxFun(nums, start, mid, end);
+        //get the max ans return it
+        return max({leftMax, rightMax, crossMax});
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        //recursion
+        int n = nums.size()-1;
+        return recursion(nums, 0, n);
     }
 };
