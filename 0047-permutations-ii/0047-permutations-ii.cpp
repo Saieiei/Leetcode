1320882213b1e2042864163939d207645ffb805a
Sairudra More <sairudra60@gmail.com>
2026-08-15 16:03:57 +0530

    Time: 10 ms (24.33%), Space: 15 MB (20.78%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,35 +1,44 @@
 class Solution {
 public:
-    void recursion(vector<int>nums, int index, vector<vector<int>>& ans){
+    void recursion(vector<int>nums, vector<int> currectAns, 
+                    vector<vector<int>>& ans, vector<bool>& isVisited){
         //bc
-        if(index >= nums.size()){
-            ans.push_back(nums);
+        if(currectAns.size() == nums.size()){
+            ans.push_back(currectAns);
+            return;
         }
-        //traverse through the nums
-        for(int j=index; j<nums.size(); j++){
-            //use it
-            swap(nums[index], nums[j]);
+        for(int i=0; i<nums.size(); i++){
+            //1st check if that ele was used
+            if(isVisited[i]){
+                continue;
+            }
+            //is not used but we have to skip duplicates as well
+            if(i>0 && nums[i] == nums[i-1] && isVisited[i-1] == false){
+                continue;
+            }
+
+            //use it and mark it as visisted
+            currectAns.push_back(nums[i]);
+            isVisited[i] = true;
             //recursion
-            recursion(nums, index+1, ans);
-            //undo
-            swap(nums[index], nums[j]);
+            recursion(nums, currectAns, ans,  isVisited);
+            //undo and mark it as not visited
+            currectAns.pop_back();
+            isVisited[i] = false;
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         //simplest way is to use sets to avoid duplication
         //but its a bad approach and the uncessary branches r also calculated
-        //the same approach as to solve permutations but with sets
-        //we will be swaping in nums itself, so we need index which we will use in a for loop
-        //we will store all the permutations in an ans
-        //which we will later convert it to a set to delete the dups
-        vector<vector<int>> ans;
-        int index = 0;
-        recursion(nums, index, ans);
-        set<vector<int>>st (ans.begin(), ans.end());
-        ans.clear();
-        for(vector<int> it: st){
-            ans.push_back(it);
-        }
+        //so, we will not be swapping here, we will create a new vector
+        //we will also have to check if we have used the ele or not, 
+        //isVisisted[], so we will use a loop here
+        //make sure to sort it because teh duplicates will be adjacent
+        sort(nums.begin(), nums.end());
+        vector<vector<int>>ans;
+        vector<int> currectAns;
+        vector<bool> isVisited(nums.size(), false);
+        recursion(nums, currectAns, ans, isVisited);
         return ans;
     }
 };
