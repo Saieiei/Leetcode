2a82425e7269a8dce55d0d4e184c3216a47eb746
Sairudra More <sairudra60@gmail.com>
2026-08-15 15:29:45 +0530

    Time: 197 ms (5.03%), Space: 68.8 MB (5.01%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,78 +1,35 @@
-//class Solution {
-//public:
-//
-//    void HelperFunction(vector<int>& nums, vector<vector<int>> &ans, int start)
-//    {   
-//        //base condition
-//        if(start>=nums.size())
-//        {
-//            ans.push_back(nums); //since we directly manupulating nums vector
-//            return;
-//        }
-//
-//        //calculation
-//        for(int i=start; i<nums.size();i++)
-//        {
-//            //1st we will swap with with i and start, then increase start
-//            swap(nums[i], nums[start]);
-//            //RECURSSION
-//            HelperFunction(nums, ans, start+1);
-//            //BACKTRACKING
-//            swap(nums[i], nums[start]);
-//        }
-//    }
-//
-//    //we will solve this using recurssion and Backtracking
-//    vector<vector<int>> permuteUnique(vector<int>& nums) {
-//        vector<vector<int>> ans;
-//        int start = 0;
-//        //we will directly update ans and nums vector, so need for return, hence call by reference
-//        HelperFunction(nums, ans, start);
-//        //now we dont need duplicates, so we can make use of sets
-//        set<vector<int>> st(ans.begin(), ans.end());
-//        //now we will put all the ans from set to ans again (no duplicates)
-//        ans.clear();
-//        for(auto i: st) ans.push_back(i);
-//        return ans;
-//    }
-//
-//    //below 1, we r using a code where we will not make use osets as it takes time, we will try to make this code a little better
-//};
-
 class Solution {
 public:
-
-    void HelperFunction(vector<int>& nums, vector<vector<int>> &ans, int start)
-    {   
-        //base condition
-        if(start>=nums.size())
-        {
-            ans.push_back(nums); //since we directly manupulating nums vector
-            return;
+    void recursion(vector<int>nums, int index, vector<vector<int>>& ans){
+        //bc
+        if(index >= nums.size()){
+            ans.push_back(nums);
         }
-
-        //calculation
-        unordered_map<int, bool> visited;
-        for(int i=start; i<nums.size();i++)
-        {
-            if(visited.find(nums[i])!= visited.end()) continue;
-            visited[nums[i]] = true;
-            //1st we will swap with with i and start, then increase start
-            swap(nums[i], nums[start]);
-            //RECURSSION
-            HelperFunction(nums, ans, start+1);
-            //BACKTRACKING
-            swap(nums[i], nums[start]);
+        //traverse through the nums
+        for(int j=index; j<nums.size(); j++){
+            //use it
+            swap(nums[index], nums[j]);
+            //recursion
+            recursion(nums, index+1, ans);
+            //undo
+            swap(nums[index], nums[j]);
         }
     }
-
-    //we will solve this using recurssion and Backtracking
     vector<vector<int>> permuteUnique(vector<int>& nums) {
+        //simplest way is to use sets to avoid duplication
+        //but its a bad approach and the uncessary branches r also calculated
+        //the same approach as to solve permutations but with sets
+        //we will be swaping in nums itself, so we need index which we will use in a for loop
+        //we will store all the permutations in an ans
+        //which we will later convert it to a set to delete the dups
         vector<vector<int>> ans;
-        int start = 0;
-        //we will directly update ans and nums vector, so need for return, hence call by reference
-        HelperFunction(nums, ans, start);
+        int index = 0;
+        recursion(nums, index, ans);
+        set<vector<int>>st (ans.begin(), ans.end());
+        ans.clear();
+        for(vector<int> it: st){
+            ans.push_back(it);
+        }
         return ans;
     }
-
 };
