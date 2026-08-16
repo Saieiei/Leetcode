485168fc66d81d8baa74909348dda496b8bf0cbd
Sairudra More <sairudra60@gmail.com>
2026-08-16 14:27:55 +0530

    Time: 3 ms (61.42%), Space: 11.2 MB (24.79%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,97 +1,72 @@
 class Solution {
 public:
-    unordered_map<int, bool> leftHorizontalCheck;
-    unordered_map<int, bool> upperDiagonalCheck;
-    unordered_map<int, bool> lowerDiagonalCheck;
-    void storeSolution(vector<vector<char>> &board, vector<vector<string>> &ans, int n)
-    {
-        //1st we need to create the string
-        vector<string> temp;
-        for(int row=0; row<n; row++)
-        {
-            string output = "";
-            for(int col=0; col<n; col++)
-            {
-                char ch = board[row][col];
-                output.push_back(ch);
+    void saveBoardinAns(int n, vector<vector<char>>& board, vector<vector<string>>& ans){
+        vector<string> tempAns;
+        for(int i=0; i<n; i++){
+            string rowOutput = "";
+            for(int j = 0; j<n; j++){
+                rowOutput += board[i][j];
             }
-            //1 string is ready
-            temp.push_back(output);
+            tempAns.push_back(rowOutput);
         }
-        ans.push_back(temp);
+        ans.push_back(tempAns);
     }
-    bool isSafe(vector<vector<char>> &board, int row, int col)
-    {
-        //int n = board.size();
-        ////i need to check 3 directions only, not all 8
-        ////left horizontal
-        //for(int i=0; i<=col; i++)
-        //{
-        //    if(board[row][i] == 'Q') return false;
-        //}
-        ////left upper dia
-        //int i = row - 1;
-        //int j = col - 1;
-        //while(i>=0 && j>=0)
-        //{
-        //    if(board[i][j] == 'Q') return false;
-        //    i--;
-        //    j--;
-        //}
-        ////left lower dia
-        //int x = row +1;
-        //int y = col -1;
-        //while(x<n && y>=0)
-        //{
-        //    if(board[x][y] == 'Q') return false;
-        //    x++;
-        //    y--;
-        //}
-
-        //leftHorizontal
-        if(leftHorizontalCheck[row] == true) return false;
-        //upperDiagonal
-        if(upperDiagonalCheck[row-col] == true) return false;
-        //lowerDiagonal
-        if(lowerDiagonalCheck[row+col] == true) return false;
-        //there is no attack
+    bool isSafe(int n, vector<vector<char>>& board, vector<vector<string>>& ans, int i, int j){
+        //to check if its safe, we have to explore the 3 directions
+        //explore the left horizontal 1st
+        int row = i, col = j;
+        for(col = j; col>=0; col--){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+        }
+        //explore the left top diagonal
+        row = i, col = j;
+        while(row >= 0 && col >= 0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            row--;
+            col--;
+        }   
+        //explore the left bottom diagonal
+        row = i, col = j;
+        while(row < n && col >= 0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            row++;
+            col--;
+        }
         return true;
-    } 
-    void solve(vector<vector<char>> &board, vector<vector<string>> &ans, int n, int col)
-    {
-        //base case
-        if(col >=n)
-        {
-            //this means 1 case is ready and we will store it
-            storeSolution(board, ans, n);
+    }
+    void solve(int n, vector<vector<char>>& board, vector<vector<string>>& ans, int i, int j){
+        //check we have reached the end of the col, if so we can save teh board
+        if(j>=n){
+            saveBoardinAns(n, board, ans);
             return;
         }
-
-        //1 case we will solve, the rest recurssion will see
-        for(int row=0; row<n; row++)
-        {
-            if(isSafe(board, row, col))
-            {
-                board[row][col] = 'Q';
-                leftHorizontalCheck[row]=true;
-                upperDiagonalCheck[row-col]=true;
-                lowerDiagonalCheck[row+col]=true;
-                //the rest recurssion will take care
-                solve(board, ans, n, col+1);
-                //backtracking
-                board[row][col] = '.';
-                leftHorizontalCheck[row]=false;
-                upperDiagonalCheck[row-col]=false;
-                lowerDiagonalCheck[row+col]=false;
+        //traverse through the row and figure out where to place the Q in the row from 0 not i
+        for(int row = 0; row<n; row++){
+            //1st check if its safe at this position
+            if(isSafe(n, board, ans, row, j)){
+                //put the quene here, use it
+                board[row][j] = 'Q';
+                //recurse, move to the next col
+                solve(n, board, ans, row, j+1);
+                //undo, we have reached the end of the board
+                board[row][j] = '.';
             }
-            
         }
     }
     vector<vector<string>> solveNQueens(int n) {
-        //n*n board
+        //we will solve it using recursion
+        //create the board of nXn
+        vector<vector<char>>board (n, vector<char>(n, '.'));
         vector<vector<string>> ans;
-        vector<vector<char>> board (n, vector<char>(n, '.'));
-        solve(board, ans, n, 0);
+        //ui really dont need i here, but still
+        int i=0, j=0; 
+        solve(n, board, ans, i, j);
         return ans;
     }
 };
