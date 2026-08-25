108c867e5185d5b425996838ef6289a548a59c95
Sairudra More <sairudra60@gmail.com>
2026-08-25 19:42:15 +0530

    Time: 0 ms (100%), Space: 19.6 MB (27.75%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,30 +11,34 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        //we will create a new LL which will connect the nodes of the existing LL
-
-        ListNode* ll = new ListNode();
-        ListNode* tail = ll;
-
-        while(list1 && list2)
-        {
-            if(list1->val < list2->val)
-            {
+        //we will have to start with dummy and tail
+        //dummy will stay as the head and tail will figure the path between these 2 lists
+        //in the end move dummy->next is the head and return head
+        //the tail will find its way until any1 of the list reaches NULL
+        //if 1 of them reached the end we have to check if the other 1 has also reached the end
+        //so in the end the tail has successfully consumed both the lists
+        ListNode* dummy = new ListNode(0);
+        ListNode* tail = dummy;
+        while(list1 != NULL && list2 != NULL){
+            if(list1->val <= list2->val){
                 tail->next = list1;
                 list1 = list1->next;
             }
-            else
-            {
+            else{
                 tail->next = list2;
                 list2 = list2->next;
             }
             tail = tail->next;
         }
-
-        //incase somethign is left out
-        if(list1) tail->next = list1;
-        if(list2) tail->next = list2;
-
-        return ll->next;
+        if(list1 != NULL){
+            tail->next = list1;
+        }
+        if(list2 != NULL){
+            tail->next = list2;
+        }
+        ListNode* head = dummy;
+        head = dummy->next;
+        delete dummy;
+        return head;
     }
 };
