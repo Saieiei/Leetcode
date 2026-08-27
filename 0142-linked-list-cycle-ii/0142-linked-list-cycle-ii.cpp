9db2e886f95fc3e06b7e638eea5cee9419ce2ebd
Sairudra More <sairudra60@gmail.com>
2026-08-27 22:25:36 +0530

    Time: 7 ms (65.93%), Space: 11.3 MB (85.89%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,24 +9,31 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        //here we will use extra space <unordered_map>
-        //we will iterate through the list and update the map as visited, until we reach true or temp->next = NULL return the node
-        //else return false (NULL)
-
-        if(!head || !head->next) return NULL;
-
-        unordered_map<ListNode*, bool> mp;
-        ListNode* temp = head;
-
-        while(temp)
-        {
-            if(mp[temp] == true) return temp;
-            else
-            {
-                mp[temp] =  true;
-                temp = temp->next;
+        //this work like magic
+        //1st we have to find the cycle thereor not
+        //use flyods algo
+        //2nd if there then keep 1 pointer at the head 
+        //and move both togther
+        //where ever they r same, is the starting point of the cycle
+        //return the node which becomes same
+        ListNode* slow = head;
+        ListNode* fast = head;
+        //1st check if there is a cycle
+        while(fast!=NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                //cycle found, now we have to find teh starting point of the cycle
+                ListNode* curr = head;
+                while(curr != slow){
+                    slow = slow->next;
+                    curr = curr->next;
+                }
+                //now they r the same, so return any1
+                return slow;
             }
         }
+        //no cycle means not possible
         return NULL;
     }
 };
