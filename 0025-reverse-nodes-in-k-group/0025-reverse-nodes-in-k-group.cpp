d7a18c96506b40e9cd5599fd5ffe23d157c48682
Sairudra More <sairudra60@gmail.com>
2026-08-30 14:43:51 +0530

    Time: 0 ms (100%), Space: 16.5 MB (70.53%) - LeetHub

--- 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.cpp
@@ -10,56 +10,63 @@
  */
 class Solution {
 public:
-//week 9, MSFT
-
-    //finding the length of the node
-    int getLength(ListNode* head)
-    {
-        ListNode* temp = head;
-        int length = 0;
-        while(temp != NULL)
-        {
-            length++;
-            temp = temp->next;
+    ListNode* getKthNode(ListNode* curr, int k){
+        while(curr && k>0){
+            curr = curr->next;
+            k--;
         }
-        return length;
+        return curr;
     }
     ListNode* reverseKGroup(ListNode* head, int k) {
-        //let 1t check the base cases
-        if(head == NULL) return head;
-        if(head->next == NULL) return head;
-
-        //now we will try to solve this using recurssion (revering)
-        //so 1st we will try to solve 1 
-        ListNode* prev = NULL;
-        ListNode* curr = head;
-        ListNode* nextNode = NULL;
-        int pos = 0;
-
-        //now we will check if there is space for k at a time
-        int length=getLength(head);
-        if(length<k) return head;
-
-        //we will reverse the k elements
-        while(pos<k)
-        {
-            nextNode = curr->next;
-            curr->next = prev;
-            prev = curr;
-            curr = nextNode;
-            pos++;
+        //this 1 is a little hard
+        //so we require all these pointers
+        //curr, prev and forwd for reversing
+        //groupPrev, oldGroupStart for linking old group and new group
+        //kthNode, groupNext for the next group purposes
+        //IMP STUFF
+        //while(true)
+        //while starting reversal, link prev to groupNext
+        //dummy node will be before head
+        //the Main concept is that, we know where the group starts
+        //we move k nodes forward to get the kth Node
+        //we also have know where the next group starts which is kthNode->next
+        //reverse that curr group
+        //then we link the group to the next group and consider the group as old
+        //if we r not able to get the kthNode (becomes NULL)
+        //then no need to do anything, just delete the dummy node and return NewHead
+        //NewHead is the dummy->next, we never touch head
+        //1st create dummyNode
+        ListNode* dummyNode = new ListNode(0);
+        dummyNode->next = head;
+        ListNode* groupPrev = dummyNode;
+        //V IMP
+        while(true){
+            ListNode* kthNode = getKthNode(groupPrev, k);
+            //1st check if the group is there or not
+            if(kthNode == NULL){
+                break;
+            }
+            //now we have to reverse this group and also link it to the next group
+            ListNode* groupNext = kthNode->next;
+            //reverse it
+            //V IMP
+            ListNode* prev = groupNext;
+            ListNode* curr = groupPrev->next;
+            while(curr != groupNext){
+                ListNode* forwd = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = forwd;
+            }
+            //now we have to make this group old to move forward to the next group
+            //VIMP
+            ListNode* oldGroupStart = groupPrev->next;
+            groupPrev->next = kthNode;
+            groupPrev = oldGroupStart;
         }
-
-        //recurssion, trust the recurssion process
-        ListNode* recurssionAns = NULL;
-        //now do the recurssion 
-        if(nextNode != NULL)
-        {
-            recurssionAns = reverseKGroup(nextNode, k); //pass nextNode and not head
-            //now we willlik the parts of the recurssion (vey imp, common  mistake)
-            head->next = recurssionAns;
-        }
-
-        return prev; //sarting point of the LL
+        //since we coudnt form the group (kthNode = NULL), simply return 
+        ListNode* newHead = dummyNode->next;
+        delete dummyNode;
+        return newHead;
     }
 };
