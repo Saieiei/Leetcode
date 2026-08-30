ad9c3ce97a507a8e61e144b21e87558f7d20a1af
Sairudra More <sairudra60@gmail.com>
2026-08-30 10:03:51 +0530

    Time: 0 ms (100%), Space: 18.2 MB (93%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,49 +8,52 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-
 class compare{
 public:
-    bool operator()(ListNode* a , ListNode* b){
+    //greater<int>
+    bool operator()(ListNode* a, ListNode* b){
         return a->val > b->val;
     }
 };
-
-//for this we will follow how we did this https://www.geeksforgeeks.org/problems/merge-k-sorted-arrays/1
-//since we are dealing with LL we have to have a head and a tail (ans)
-// we should take in any null pointers into the ans
-//its kinda easy than array
 class Solution {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        
+        //we will not use bruteforce, thats NxM
+        //we will use min-Heap NlogM
+        //take the starting nodes of the LL and push it in min-Heap
+        //take the top ele from min-Heap and push it in new LL
+        //keepdoing until the minHeap gets empty
+        //keep pushing in minhead also (topNode->next)
+        //minHeap, requires a cutome gereater<int>
         priority_queue<ListNode*, vector<ListNode*>, compare> pq;
-        ListNode* head = NULL; ListNode* tail = NULL;
-
-        //1st nodes of each vector lists
-        for(int i=0; i<lists.size(); i++){
-            ListNode* temp = lists[i];
-            //check if its empty, if not then only push
-            if(temp != NULL)pq.push(temp);
-        }
-
-        //just like level order traversAL TECHNIQUE
-        while(!pq.empty()){
-            //take and pop the top elemnt
-            ListNode* front = pq.top(); pq.pop();
-
-            if(front){
-                //then either this element can the begining of the ans or next next
-                if(head == NULL && tail == NULL){ //this is the 1st element
-                    head = front; tail = front;
-                }
-                else{ //next next
-                    tail->next = front; tail = front;
-                }
+        //push the 1st nodes in the minHeap
+        for(ListNode* &it: lists){
+            if(it != NULL){
+                pq.push(it);
             }
-
-            if(front->next) pq.push(front->next);
+        }
+        //keep processing until the minHeap is not empty
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+        while(!pq.empty()){
+            //get the top and pop it an push it
+            ListNode* topNode = pq.top();
+            pq.pop();
+            //check if this is the 1st node ever,
+            if(head == NULL){
+                head = topNode;
+                tail = topNode;
+            }
+            else{
+                //if not, update tail and move it
+                tail->next = topNode;
+                tail = tail->next;
+            }
+            //push in the next node in the minHeap
+            if(topNode->next){
+                pq.push(topNode->next);
+            }
         }
         return head;
     }
-};    
+};
