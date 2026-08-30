e4e3463a3dfff76514a8930448baaebb18e2d8dd
Sairudra More <sairudra60@gmail.com>
2026-08-30 16:44:17 +0530

    Time: 53 ms (76.9%), Space: 24 MB (55.77%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,29 +9,13 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        //this is simple only, 2 pointer approach, O(N+M) O(1)
-        //we should match the node and not the value
-        //we will keep on trying to match the LLs
-        //if they come to a NULL then we will make it go as a head to the other LL
+        //compact apprach
         ListNode* a = headA;
         ListNode* b = headB;
         while(a != b){
-            //before moving it ahead, check if its NULL
-            if(a == NULL){
-                a = headB;
-            }
-            else{
-                a = a->next;
-            }
-            //the same goes for b as well
-            if(b == NULL){
-                b = headA;
-            }
-            else{
-                b = b->next;
-            }
+            a = (a == NULL)? headB : a->next;
+            b = (b == NULL)? headA : b->next;
         }
-        //return any1, since they r now at the same node
         return a;
     }
 };
